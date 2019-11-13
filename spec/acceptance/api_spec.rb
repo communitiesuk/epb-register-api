aa610396fea1d5a065e7b7dcba32860af4ceca99
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2019-11-13 12:17:25 +0000

    Add acceptance test for api app

---
+++ spec/acceptance/api_spec.rb
@@ -0,0 +1,9 @@
+describe AssessorService do
+  context "GET to /healthcheck" do
+    let(:response) { get "/healthcheck" }
+
+    it "returns status 200 OK" do
+      expect(response.status).to eq 200
+    end 
+  end
+end 
