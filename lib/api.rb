68b1cc483f7d6f00fbc36a720ddc8a13fc07ec4a
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-14 16:05:37 +0000

    Dependency inversion for Sinatra initializer

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,13 @@
 require 'sinatra/base'
-require 'helpers/unleash'
+require 'helpers/toggles'
 
 class AssessorService < Sinatra::Base
+  def initialize(toggles = false)
+    super
+
+    @toggles = toggles || Toggles.new
+  end
+
   get '/' do
     'Hello world!'
   end
