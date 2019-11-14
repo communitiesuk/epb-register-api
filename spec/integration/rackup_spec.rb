c5bb029c19534909fae895ce73e79fa8926473c0
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-14 17:28:47 +0000

    Removes rackup...

--- spec/integration/rackup_spec.rb
+++ spec/integration/rackup_spec.rb
@@ -4,33 +4,33 @@ require 'pry'
 
 $process = 0
 
-describe 'starts server outside of ruby'  do
-  describe 'the server running live' do
-    before(:all) do
-      $stdout = StringIO.new
-      process = IO.popen('rackup 2>&1')
-      $process = process.pid
-      sleep 1
-    end
-
-    after(:all) do
-      Process.kill('KILL', $process)
-    end
-
-    let(:request) { Net::HTTP.new('localhost', 9292) }
- 
-    context 'it is running' do
-      it 'returns status 200' do
-        req = Net::HTTP::Get.new('/healthcheck')
-        response = request.request(req)
-        expect(response.code).to eq('200')
-      end
-
-      it 'returns status 404' do
-        req = Net::HTTP::Get.new('/error-message')
-        response = request.request(req)
-        expect(response.code).to eq('404')
-      end
-    end
-  end 
-end 
+# describe 'starts server outside of ruby'  do
+#   describe 'the server running live' do
+#     before(:all) do
+#       $stdout = StringIO.new
+#       process = IO.popen('rackup 2>&1')
+#       $process = process.pid
+#       sleep 1
+#     end
+#
+#     after(:all) do
+#       Process.kill('KILL', $process)
+#     end
+#
+#     let(:request) { Net::HTTP.new('localhost', 9292) }
+#
+#     context 'it is running' do
+#       it 'returns status 200' do
+#         req = Net::HTTP::Get.new('/healthcheck')
+#         response = request.request(req)
+#         expect(response.code).to eq('200')
+#       end
+#
+#       it 'returns status 404' do
+#         req = Net::HTTP::Get.new('/error-message')
+#         response = request.request(req)
+#         expect(response.code).to eq('404')
+#       end
+#     end
+#   end
+# end
