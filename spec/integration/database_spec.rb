20c64c7530ec68bc694bfc498fc5d067a1edfe8a
Yusuf <yusuf@madetech.com>
2019-11-21 16:03:55 +0000

    Add integration test for database connection
    Co-authored-by: Christine <splogy2001@hotmail.com>
    Co-authored-by: Marten <marten.wetterberg@madetech.com>
    Co-authored-by: Jaseera <jaseera@madetech.com>

--- spec/integration/database_spec.rb
+++ spec/integration/database_spec.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 require 'sinatra/activerecord'
+require 'dotenv'
 
 describe ActiveRecord::Base do
-  # before do
-  #   described_class.establish_connection(
-  #     adapter: 'postgresql',
-  #     database: 'epb_development'
-  #   )
-  # end
-  #
-  # xit 'can connect to an existing database' do
-  #   described_class.connection
-  #
-  #   expect(described_class.connected?).to be_truthy
-  # end
+  before do
+    described_class.establish_connection(
+      adapter: 'postgresql',
+      database: 'epb_development',
+      host: '127.0.0.1',
+      username: 'postgres',
+      password: ENV['DOCKER_POSTGRES_PASSWORD']
+    )
+  end
+
+  it 'can connect to an existing database' do
+    described_class.connection
+
+    expect(described_class.connected?).to be_truthy
+  end
 end
