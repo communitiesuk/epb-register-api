990ea4e737112d2961ce423b0f7b650c9e95ffac
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-21 17:35:25 +0000

    Remove connection details for db integration test
    Co-authored-by: Christine <splogy2001@hotmail.com>
    Co-authored-by: Yusuf <yusuf@madetech.com>
    Co-authored-by: Jaseera <jaseera@madetech.com>

--- spec/integration/database_spec.rb
+++ spec/integration/database_spec.rb
@@ -5,18 +5,17 @@ require 'dotenv'
 
 describe ActiveRecord::Base do
   before do
-    described_class.establish_connection(
-      adapter: 'postgresql',
-      database: 'epb_development',
-      host: '127.0.0.1',
-      username: 'postgres',
-      password: ENV['DOCKER_POSTGRES_PASSWORD']
-    )
+    described_class.establish_connection
   end
 
   it 'can connect to an existing database' do
     described_class.connection
 
     expect(described_class.connected?).to be_truthy
+  end
+
+  it 'can find the schemes table' do
+    ENV['RACK_ENV'] = 'development'
+    described_class.connection
   end
 end
