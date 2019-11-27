340b3c872dea8eb137a5c7de3d2bd5fb3fd18426
Yusuf <yusuf@madetech.com>
2019-11-27 17:18:40 +0000

    Add integration tests for schemes endpoint
    Co-authored-by: Marten <marten.wetterberg@madetech.com>

--- spec/integration/scheme_spec.rb
+++ spec/integration/scheme_spec.rb
@@ -12,15 +12,25 @@ describe AssessorService do
     end
 
     context 'responses from schemes after posting' do
-      let(:response) do 
+      let(:response) do
         post '/schemes', :schemes => {:name => "Scheme name"}
-        get '/schemes' 
-      end 
+        get '/schemes'
+      end
 
       it 'returns scheme object' do
         parsed_response = JSON.parse(response.body)
         expect(parsed_response["schemes"].count).to eq(1)
-      end 
-    end 
-  end 
-end 
+      end
+    end
+
+    context 'responses from schemes after posting' do
+      let(:response) do
+        post '/schemes', :schemes => {:name => "Scheme name"}
+      end
+
+      it 'returns a status code of 200' do
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
