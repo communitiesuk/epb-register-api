f3394cc5a814a5252fe63efe00079fe96f161f96
Tito Sarrionandia <tito@madetech.com>
2019-11-28 15:59:03 +0000

    Refactor, move all of the posting tests to one context in schemes acceptance test

--- spec/acceptance/schemes_spec.rb
+++ spec/acceptance/schemes_spec.rb
@@ -18,52 +18,24 @@ describe AssessorService do
     end
 
     context 'posting to the schemes api' do
-      let(:response) { post '/api/schemes', '{"name": "Scheme name"}' }
+      response = false
+      before(:each) do
+        response = post('/api/schemes', '{"name": "XYMZALERO"}')
+      end
 
       it 'returns status 201' do
         expect(response.status).to eq(201)
       end
-    end
-
-    context 'responses from schemes after posting' do
-      let(:response) do
-        post '/api/schemes', '{"name": "Scheme name"}'
-        get '/api/schemes'
-      end
-
-      it 'returns scheme object' do
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response['schemes'].count).to eq(1)
-      end
-    end
-
-    context 'when adding a scheme' do
-      let(:post_request) do
-        post '/api/schemes', '{"name": "XYMZALERO"}'
-      end
-
-      it 'returns a status code of 200' do
-        expect(post_request.status).to eq(201)
-      end
 
       it 'is visible in the list of schemes' do
-        post_request
-
         get_response = get '/api/schemes'
-
         expect(get_response.body).to include('XYMZALERO')
       end
 
       it 'cannot have the same name twice' do
-        post_response = false
-
-        2.times do
-          post_response = post '/api/schemes', '{"name": "XYMZALERO"}'
-        end
-
+        post_response = post '/api/schemes', '{"name": "XYMZALERO"}'
         expect(post_response.status).to eq(400)
       end
     end
-
   end
 end
