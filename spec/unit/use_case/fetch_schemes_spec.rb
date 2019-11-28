6c1b84656d18e676b5a64f31920d232b097be7a9
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-28 16:17:33 +0000

    Add tests for fetch schemes use case
    Co-authored-by: Yusuf <yusuf@madetech.com>

---
+++ spec/unit/use_case/fetch_schemes_spec.rb
@@ -0,0 +1,33 @@
+describe UseCase::FetchSchemes do
+  class SchemesGatewayFake
+    attr_writer :schemes
+
+    def initialize
+      @schemes = {}
+    end
+
+    def all
+      @schemes
+    end
+  end
+
+  let(:schemes_gateway) { SchemesGatewayFake.new }
+
+  let(:response) do
+    described_class.new(schemes_gateway).execute
+  end
+
+  context 'when there are no schemes' do
+    it 'displays an empty hash' do
+      expect(response).to eq(schemes: {})
+    end
+  end
+
+  context 'when there are schemes' do
+    it 'displays the schemes in a hash' do
+      schemes_gateway.schemes = {name: 'hello'}
+
+      expect(response).to eq(schemes: {name: 'hello'})
+    end
+  end
+end
