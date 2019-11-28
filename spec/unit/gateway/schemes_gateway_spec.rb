2e69bdc099d7c74532bcaeafae4138a698b32645
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-28 15:57:06 +0000

    Add tests for schemes gateway
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- spec/unit/gateway/schemes_gateway_spec.rb
+++ spec/unit/gateway/schemes_gateway_spec.rb
@@ -1,5 +1,29 @@
 describe Gateway::SchemesGateway do
-  it 'does pass' do
-    expect(1).to eq(1)
+  context 'when adding a scheme' do
+    it 'can add a scheme' do
+      expect(Gateway::SchemesGateway::Scheme).to receive(:create)
+
+      subject = described_class.new
+      subject.add_scheme('test')
+    end
+  end
+
+  context 'when there are no schemes' do
+    it 'can show an empty hash' do
+      allow(Gateway::SchemesGateway::Scheme).to receive(:all).and_return({})
+
+      expect(Gateway::SchemesGateway::Scheme).to receive(:all)
+
+      subject = described_class.new
+      subject.all_schemes
+    end
+  end
+
+  context 'when there are schemes' do
+    it 'can show results' do
+      allow(Gateway::SchemesGateway::Scheme).to receive(:all).and_return([{ id: 1, name: 'hello' }])
+
+      expect(described_class.new.all_schemes).to eq([{ scheme_id: 1, name: 'hello' }])
+    end
   end
 end
