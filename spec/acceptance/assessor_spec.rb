78b6a0ac3d0c474b2ac72f52fff6daca9df7c863
Yusuf <yusuf@madetech.com>
2019-12-02 11:15:52 +0000

    Add test for returning status 404 for when a scheme doesnt exist
    Co-authored-by: Tito <tito@madetech.com>

--- spec/acceptance/assessor_spec.rb
+++ spec/acceptance/assessor_spec.rb
@@ -1,15 +1,27 @@
 describe AssessorService do
   describe 'The Assessor API' do
     context 'When a scheme doesnt exist' do
-      let(:response) { get '/api/schemes/20/assessors/SCHEME4532' }
+      it 'returns status 404 for a get' do
+        response = get '/api/schemes/20/assessors/SCHEME4532'
+        expect(response.status).to eq(404)
+      end
 
-      it 'returns status 404' do
+      it 'returns status 404 for a PUT' do
+        request_body =
+          {
+            firstName: 'Some',
+            middleNames: 'middle',
+            lastName: 'Person',
+            dateOfBirth: '1991-02-25'
+          }
+        response = put('/api/schemes/20/assessors/SCHEME4532', request_body.to_json)
+
         expect(response.status).to eq(404)
       end
     end
 
     context 'when an assessor doesnt exist' do
-      let (:post_response) { post('/api/schemes', { name: 'scheme245'}.to_json) }
+      let(:post_response) { post('/api/schemes', { name: 'scheme245'}.to_json) }
 
       it 'returns status 404' do
         schemeid = JSON.parse(post_response.body)['schemeId']
