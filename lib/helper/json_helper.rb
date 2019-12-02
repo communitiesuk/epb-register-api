1ce7c898ba68fa2338015b095d13fba6c00ea083
Yusuf <yusuf@madetech.com>
2019-12-02 17:10:35 +0000

    Refactor: Remove local variable

--- lib/helper/json_helper.rb
+++ lib/helper/json_helper.rb
@@ -1,8 +1,7 @@
 module Helper
   class JsonHelper
     def convert_to_ruby_hash(json)
-      symbolized_keys_hash = JSON.parse(json)
-      symbolized_keys_hash.deep_transform_keys { |k| k.to_s.underscore.to_sym }
+      JSON.parse(json).deep_transform_keys { |k| k.to_s.underscore.to_sym }
     end
   end
 end
