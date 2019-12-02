eec7aa9141693d602693f97091be53b959a9e9c9
Yusuf <yusuf@madetech.com>
2019-12-02 11:26:35 +0000

    Refactor: Extract constant for valid assessor
    Co-authored-by: Tito <tito@madetech.com>

--- spec/acceptance/assessor_spec.rb
+++ spec/acceptance/assessor_spec.rb
@@ -1,5 +1,12 @@
 describe AssessorService do
   describe 'The Assessor API' do
+    VALID_ASSESSOR_REQUEST_BODY = {
+        firstName: 'Some',
+        middleNames: 'middle',
+        lastName: 'Person',
+        dateOfBirth: '1991-02-25'
+    }
+
     context 'When a scheme doesnt exist' do
       it 'returns status 404 for a get' do
         response = get '/api/schemes/20/assessors/SCHEME4532'
@@ -7,14 +14,8 @@ describe AssessorService do
       end
 
       it 'returns status 404 for a PUT' do
-        request_body =
-          {
-            firstName: 'Some',
-            middleNames: 'middle',
-            lastName: 'Person',
-            dateOfBirth: '1991-02-25'
-          }
-        response = put('/api/schemes/20/assessors/SCHEME4532', request_body.to_json)
+
+        response = put('/api/schemes/20/assessors/SCHEME4532', VALID_ASSESSOR_REQUEST_BODY.to_json)
 
         expect(response.status).to eq(404)
       end
@@ -37,15 +38,8 @@ describe AssessorService do
       it 'returns status 404' do
         schemeid = JSON.parse(post_response.body)['schemeId']
         second_schemeid = JSON.parse(post('/api/schemes', { name: 'scheme987'}.to_json).body)['schemeId']
-        request_body =
-            {
-                firstName: 'Some',
-                middleNames: 'middle',
-                lastName: 'Person',
-                dateOfBirth: '1991-02-25'
-            }
 
-        put("/api/schemes/#{second_schemeid}/assessors/SCHE987654", request_body)
+        put("/api/schemes/#{second_schemeid}/assessors/SCHE987654", VALID_ASSESSOR_REQUEST_BODY)
 
         get_response = get "/api/schemes/#{schemeid}/assessors/SCHE987654"
 
