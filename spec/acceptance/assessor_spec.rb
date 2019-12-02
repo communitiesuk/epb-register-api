ab7dde4c2d0fb6aaf5d609955ff5f9e279e51054
Yusuf <yusuf@madetech.com>
2019-12-02 11:23:55 +0000

    Add test for returning status 404 for when getting an assessor on the wrong scheme
    Co-authored-by: Tito <tito@madetech.com>

--- spec/acceptance/assessor_spec.rb
+++ spec/acceptance/assessor_spec.rb
@@ -30,5 +30,27 @@ describe AssessorService do
         expect(get_response.status).to eq(404)
       end
     end
+
+    context 'when getting an assessor on the wrong scheme' do
+      let(:post_response) { post('/api/schemes', { name: 'scheme245'}.to_json) }
+
+      it 'returns status 404' do
+        schemeid = JSON.parse(post_response.body)['schemeId']
+        second_schemeid = JSON.parse(post('/api/schemes', { name: 'scheme987'}.to_json).body)['schemeId']
+        request_body =
+            {
+                firstName: 'Some',
+                middleNames: 'middle',
+                lastName: 'Person',
+                dateOfBirth: '1991-02-25'
+            }
+
+        put("/api/schemes/#{second_schemeid}/assessors/SCHE987654", request_body)
+
+        get_response = get "/api/schemes/#{schemeid}/assessors/SCHE987654"
+
+        expect(get_response.status).to eq(404)
+      end
+    end
   end
 end
