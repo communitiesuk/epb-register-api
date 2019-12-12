13c3e875e1dac072db230dfe9d383f6823f93bf8
Tito Sarrionandia <tito@madetech.com>
2019-12-12 16:11:46 +0000

    Log when assessors are created

--- lib/controller/assessor_controller.rb
+++ lib/controller/assessor_controller.rb
@@ -42,8 +42,10 @@ module Controller
           assessor_details
         )
       if create_assessor_response[:assessor_was_newly_created]
+        @logger.info("New assessor registered")
         json_response(201, create_assessor_response[:assessor])
       else
+        @logger.info("Assessor details updated")
         json_response(200, create_assessor_response[:assessor])
       end
     rescue Exception => e
