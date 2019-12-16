1b6a6e6048900eb65816f88876cbc5c1aea009b9
Tito Sarrionandia <tito@madetech.com>
2019-12-16 14:06:49 +0000

    Remove manual print log statements

--- lib/controller/assessor_controller.rb
+++ lib/controller/assessor_controller.rb
@@ -50,11 +50,9 @@ module Controller
         )
       if create_assessor_response[:assessor_was_newly_created]
         @logger.info("New assessor registered")
-        p "Print new assessor registered"
         json_response(201, create_assessor_response[:assessor])
       else
         @logger.info("Assessor details updated")
-        p "print new assessor registered"
         json_response(200, create_assessor_response[:assessor])
       end
     rescue Exception => e
