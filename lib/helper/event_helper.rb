d0dacdab2fe4aaae494603c627a8fdece748bab3
Tito Sarrionandia <tito@madetech.com>
2019-12-16 14:48:21 +0000

    Custom event logging format

--- lib/helper/event_helper.rb
+++ lib/helper/event_helper.rb
@@ -2,10 +2,14 @@ module Helper
   class EventHelper
     def initialize
       @logger = Logger.new(STDOUT)
+      @logger.formatter = proc do |severity, datetime, progname, msg|
+        "[#{datetime}] #{severity} EVENT:#{msg}\n"
+      end
+
     end
 
     def event(event_code)
-      @logger.info("EVENT:#{event_code.to_s.upcase}")
+      @logger.info(event_code.to_s.upcase)
     end
   end
 end
