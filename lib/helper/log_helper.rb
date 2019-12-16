f3aaff412107cd5fce8316ffb5e90765e6abeb28
Tito Sarrionandia <tito@madetech.com>
2019-12-16 15:47:16 +0000

    Remove newline from output because print adds one anyway

--- lib/helper/log_helper.rb
+++ lib/helper/log_helper.rb
@@ -1,16 +1,7 @@
 module Helper
   class LogHelper
-    def initialize
-      $stdout.sync = true
-      @logger = Logger.new($stdout)
-      @logger.formatter = proc do |severity, datetime, progname, msg|
-        "#{{timestamp: datetime}.merge(msg).to_json}\n"
-      end
-
-    end
-
     def event(event_code)
-      p "#{{timestamp: Time.now.to_s, event_type: event_code}.to_json}\n"
+      p "#{{timestamp: Time.now.to_s, event_type: event_code}.to_json}"
     end
   end
 end
