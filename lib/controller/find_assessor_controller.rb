09c8e2e5a068992b009b9cec73436919555606b2
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-01-07 10:22:36 +0000

    Remove redundant controllers

--- lib/controller/find_assessor_controller.rb
+++
@@ -1,7 +0,0 @@
-module Controller
-  class FindAssessorController < Controller::BaseController
-    get '/api/findassessor/:postcode', jwt_auth: [] do
-      body '1'
-    end
-  end
-end
--- spec/unit/controller/find_assessor_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Controller::FindAssessorController do
-  context 'when findassessor receives GET request' do
-    it 'returns an object' do
-      response = authenticate_and { get '/api/findassessor/E2+0SZ' }
-
-      expect(response.body).to eq('1')
-    end
-  end
-end
