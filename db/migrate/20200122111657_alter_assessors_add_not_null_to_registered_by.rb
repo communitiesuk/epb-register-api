f1936e5cfa919397b4d88fc1c1178942edc92fb9
Christine-horrocks <splogy2001@hotmail.com>
2020-01-22 15:04:14 +0000

    Update migration to backfil nil values
    Co-authored-by: Yusuf <yusuf@madeteach.com>

--- db/migrate/20200122111657_alter_assessors_add_not_null_to_registered_by.rb
+++ db/migrate/20200122111657_alter_assessors_add_not_null_to_registered_by.rb
@@ -1,5 +1,5 @@
 class AlterAssessorsAddNotNullToRegisteredBy < ActiveRecord::Migration[6.0]
   def change
-     change_column_null :assessors, :registered_by, false
+     change_column_null :assessors, :registered_by, false, 1
   end
 end
