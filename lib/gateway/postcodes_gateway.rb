1e8d7a41b44381b51627df609427ebf9c6f235f5
Yusuf <yusuf@madetech.com>
2020-01-23 13:45:16 +0000

    Refactor: Create private output method for response mapping
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- lib/gateway/postcodes_gateway.rb
+++ lib/gateway/postcodes_gateway.rb
@@ -3,43 +3,41 @@ module Gateway
     def fetch(postcode)
       response = db_response(postcode)
       result = []
-      response.map do |row|
-        result.push(
-          {
-            'postcode': row['postcode'],
-            'latitude': row['latitude'].to_f,
-            'longitude': row['longitude'].to_f
-          }
-        )
-      end
+
+      output(response, result)
+
       if result.empty?
         outcode_array = postcode.split(' ')
         outcode = outcode_array[0]
         response =
           db_response('outcode', 'postcode_outcode_geolocations', outcode)
 
-        response.map do |row|
-          result.push(
-            {
-              'outcode': row['outcode'],
-              'latitude': row['latitude'].to_f,
-              'longitude': row['longitude'].to_f
-            }
-          )
-        end
+        output(response, result, 'outcode')
       end
+
       result
     end
 
     private
 
     def db_response(code = 'postcode', table = 'postcode_geolocation', postcode)
-      response =
-        ActiveRecord::Base.connection.execute(
-          "SELECT #{code}, latitude, longitude FROM #{table} WHERE #{code} = '#{
-            ActiveRecord::Base.sanitize_sql(postcode)
-          }'"
+      ActiveRecord::Base.connection.execute(
+        "SELECT #{code}, latitude, longitude FROM #{table} WHERE #{code} = '#{
+          ActiveRecord::Base.sanitize_sql(postcode)
+        }'"
+      )
+    end
+
+    def output(response, result, postcode = 'postcode')
+      response.map do |row|
+        result.push(
+          {
+            "#{postcode}": row[postcode],
+            'latitude': row['latitude'].to_f,
+            'longitude': row['longitude'].to_f
+          }
         )
+      end
     end
   end
 end
