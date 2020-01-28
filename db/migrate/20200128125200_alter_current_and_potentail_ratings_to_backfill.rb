54d6296e01a758dfef9d0ffdef526135b7f3383f
Christine-horrocks <splogy2001@hotmail.com>
2020-01-28 16:03:39 +0000

    Format
    Co-authored-by: Jaseera <jaseera@madeteach.com>

--- db/migrate/20200128125200_alter_current_and_potentail_ratings_to_backfill.rb
+++ db/migrate/20200128125200_alter_current_and_potentail_ratings_to_backfill.rb
@@ -1,6 +1,12 @@
 class AlterCurrentAndPotentailRatingsToBackfill < ActiveRecord::Migration[6.0]
   def change
-    change_column :domestic_energy_assessments, :current_energy_efficiency_rating, :smallint, null: false, default: 1
-    change_column :domestic_energy_assessments, :potential_energy_efficiency_rating, :smallint, null: false, default: 2
+    change_column :domestic_energy_assessments,
+                  :current_energy_efficiency_rating,
+                  :smallint,
+                  null: false, default: 1
+    change_column :domestic_energy_assessments,
+                  :potential_energy_efficiency_rating,
+                  :smallint,
+                  null: false, default: 2
   end
 end
