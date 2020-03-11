ee8b61641037b3f4130d1e848c4de6fb4f19ac35
Tito Sarrionandia <tito@madetech.com>
2020-03-11 10:46:51 +0000

    Refactor: Split the search for assessment tests to a different file because the file was way too big

--- db/migrate/20200304104905_alter_assessors_rename_column_domestic_ep_qualification.rb
+++ db/migrate/20200304104905_alter_assessors_rename_column_domestic_ep_qualification.rb
@@ -1,5 +1,9 @@
-class AlterAssessorsRenameColumnDomesticEpQualification < ActiveRecord::Migration[6.0]
+class AlterAssessorsRenameColumnDomesticEpQualification < ActiveRecord::Migration[
+  6.0
+]
   def change
-    rename_column :assessors, :domestic_energy_performance_qualification, :domestic_rd_sap_qualification
+    rename_column :assessors,
+                  :domestic_energy_performance_qualification,
+                  :domestic_rd_sap_qualification
   end
 end
