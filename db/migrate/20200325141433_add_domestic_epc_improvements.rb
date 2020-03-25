46490573a2027f8d60bec6e17580ffaad39ac5fd
Tito Sarrionandia <tito@madetech.com>
2020-03-25 14:55:13 +0000

    Add migration for EPC improvements

---
+++ db/migrate/20200325141433_add_domestic_epc_improvements.rb
@@ -0,0 +1,12 @@
+class AddDomesticEpcImprovements < ActiveRecord::Migration[6.0]
+  def change
+    create_table :domestic_epc_energy_improvements, { id: false } do |t|
+      t.string :assessment_id
+      t.integer :sequence
+    end
+
+    add_foreign_key :domestic_epc_energy_improvements,
+                    :domestic_energy_assessments,
+                    column: :assessment_id, primary_key: :assessment_id
+  end
+end
