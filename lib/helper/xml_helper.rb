9eb553fb0685d7d67a8f323d7974c8aa6e152c79
Yusuf <yusuf@madetech.com>
2020-04-02 12:20:50 +0100

    Refactor: Add exception message when raising error for invalid xml
    Co-authored-by: Marten <marten@madetech.com>
    Co-authored-by: Jiv <jiv.dhaliwal@madetech.com>

--- lib/helper/xml_helper.rb
+++ lib/helper/xml_helper.rb
@@ -9,10 +9,11 @@ module Helper
     def convert_to_hash(xml, schema)
       xsd = Nokogiri::XML.Schema(schema)
       file = Nokogiri.XML(xml)
+      errors = xsd.validate(file)
 
-      raise InvalidXml unless xsd.validate(file).empty?
+      raise InvalidXml, errors.map(&:message).join(', ') if errors.any?
 
-      Hash.from_xml(file.to_s).deep_symbolize_keys if xsd.validate(file).empty?
+      Hash.from_xml(file.to_s).deep_symbolize_keys if errors.empty?
     end
   end
 end
