86902c2077a3590ff980ad73a2dfda18cf4e9c43
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-04-08 10:56:53 +0100

    Add migration to change energy performance rating type to integer

---
+++ db/migrate/20200408093743_change_energy_performance_rating_to_integer_in_domestic_epc_energy_improvements.rb
@@ -0,0 +1,5 @@
+class ChangeEnergyPerformanceRatingToIntegerInDomesticEpcEnergyImprovements < ActiveRecord::Migration[6.0]
+  def change
+    change_column :domestic_epc_energy_improvements, :energy_performance_rating,  'integer USING CAST(energy_performance_rating AS integer)'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_084606) do
+ActiveRecord::Schema.define(version: 2020_04_08_093743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_084606) do
     t.decimal "typical_saving"
     t.string "improvement_category"
     t.string "improvement_type"
-    t.string "energy_performance_rating"
+    t.integer "energy_performance_rating"
     t.string "environmental_impact_rating"
     t.string "green_deal_category_code"
   end
