034fb9f277876402453905304cd3685260cf0e65
Yusuf <yusuf@madetech.com>
2020-04-15 11:16:05 +0100

    Refactor: Add empty line after guard clause

--- lib/use_case/check_assessor_belongs_to_scheme.rb
+++ lib/use_case/check_assessor_belongs_to_scheme.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module UseCase
   class CheckAssessorBelongsToScheme
     class AssessorNotFoundException < StandardError; end
@@ -7,9 +9,11 @@ module UseCase
     end
 
     def execute(scheme_assessor_id, scheme_ids)
-      assessor = @assessor_gateway.fetch(scheme_assessor_id)
+      assessor = @assessor_gateway.fetch scheme_assessor_id
+
       raise AssessorNotFoundException unless assessor
-      scheme_ids.include?(assessor.registered_by_id)
+
+      scheme_ids.include? assessor.registered_by_id
     end
   end
 end
