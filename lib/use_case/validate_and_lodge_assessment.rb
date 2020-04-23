dd4ff75357083d7c3129cb7270267c9b3a26198d
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-04-23 16:55:00 +0200

    Make lodgement orchestrator decode XML to hash
    Co-authored-by: George <george@madetech.com>

--- lib/use_case/validate_and_lodge_assessment.rb
+++ lib/use_case/validate_and_lodge_assessment.rb
@@ -1,13 +1,28 @@
+require 'active_support/core_ext/hash/conversions'
+
 module UseCase
   class ValidateAndLodgeAssessment
+    class ValidationError < StandardError; end
+
     def initialize(validate_lodgement_use_case, lodge_assessment_use_case)
       @validate_lodgement_use_case = validate_lodgement_use_case
       @lodge_assessment_use_case = lodge_assessment_use_case
     end
 
-    def execute
-      @validate_lodgement_use_case.execute
-      @lodge_assessment_use_case.execute
+    def execute(assessment_id, xml, content_type)
+      unless @validate_lodgement_use_case.execute(xml, content_type)
+        raise ValidationError
+      end
+
+      hash = xml_to_hash(xml)
+
+      @lodge_assessment_use_case.execute(hash, assessment_id)
+    end
+
+    private
+
+    def xml_to_hash(xml)
+      Hash.from_xml(xml).deep_symbolize_keys
     end
   end
 end
--- spec/unit/use_case/validate_and_lodge_assessment_spec.rb
+++ spec/unit/use_case/validate_and_lodge_assessment_spec.rb
@@ -19,13 +19,17 @@ describe UseCase::ValidateAndLodgeAssessment do
     end
   end
 
+  let(:valid_xml) do
+    File.read File.join Dir.pwd, 'api/schemas/xml/examples/RdSAP-19.01.xml'
+  end
+
   context 'when validating a valid RdSAP assessment' do
     it 'will call the two use cases' do
       validate_lodgement_use_case = FakeValidateLodgementUseCase.new
       lodge_assessment_use_case = FakeLodgeAssessmentUseCase.new
 
       use_case = described_class.new(validate_lodgement_use_case, lodge_assessment_use_case)
-      use_case.execute()
+      use_case.execute("0000-0000-0000-0000-0000", valid_xml, "application/xml+RdSAP-Schema-19.0")
 
       expect(validate_lodgement_use_case.is_called).to eq(true)
       expect(lodge_assessment_use_case.is_called).to eq(true)
