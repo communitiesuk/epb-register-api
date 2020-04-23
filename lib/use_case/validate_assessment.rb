afc445147ec3ef2fa6210f82c50982a9f5fa9687
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-04-23 15:30:18 +0200

    Add validate assessment use case to validate XML against a schema
    Co-authored-by: George <george@madetech.com>

---
+++ lib/use_case/validate_assessment.rb
@@ -0,0 +1,24 @@
+module UseCase
+  class ValidateAssessment
+    class InvalidXml < StandardError; end
+
+    def execute(xml, content_type)
+      schema =
+        'api/schemas/xml/RdSAP-Schema-19.0/RdSAP/Templates/RdSAP-Report.xsd'
+      assessment_body = validate_xml(xml, schema)
+    end
+
+    private
+
+    def validate_xml(xml, schema)
+      xsddoc = Nokogiri.XML(File.read(schema), schema)
+      xsd = Nokogiri::XML::Schema.from_document(xsddoc)
+      file = Nokogiri.XML(xml)
+      errors = xsd.validate(file)
+
+      raise InvalidXml, errors.map(&:message).join(', ') if errors.any?
+
+      true
+    end
+  end
+end
---
+++ spec/unit/use_case/validate_assessment_spec.rb
@@ -0,0 +1,14 @@
+describe UseCase::ValidateAssessment do
+  context 'given an existing RdSAP schema name' do
+    let(:valid_xml) do
+      File.read File.join Dir.pwd, 'api/schemas/xml/examples/RdSAP-19.01.xml'
+    end
+
+    it 'will return a boolean true when the XML is valid' do
+      validate_assessment = described_class.new
+      valid_xml_response = validate_assessment.execute(valid_xml, 'application/xml+RdSAP-Schema-19.0')
+
+      expect(valid_xml_response).to eq(true)
+    end
+  end
+end
