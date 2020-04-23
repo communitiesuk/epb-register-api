cb4db88fc81a04cb4452bc92a7fc0754c1468f1b
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-04-23 17:07:01 +0200

    Validate assessor belongs to lodging scheme in orchestrator
    Co-authored-by: George <george@madetech.com>

--- lib/use_case/validate_and_lodge_assessment.rb
+++ lib/use_case/validate_and_lodge_assessment.rb
@@ -4,17 +4,20 @@ module UseCase
   class ValidateAndLodgeAssessment
     class ValidationError < StandardError; end
 
-    def initialize(validate_lodgement_use_case, lodge_assessment_use_case)
+    def initialize(validate_lodgement_use_case, lodge_assessment_use_case, check_assessor_belongs_to_scheme)
       @validate_lodgement_use_case = validate_lodgement_use_case
       @lodge_assessment_use_case = lodge_assessment_use_case
+      @check_assessor_belongs_to_scheme = check_assessor_belongs_to_scheme
     end
 
-    def execute(assessment_id, xml, content_type)
+    def execute(assessment_id, xml, content_type, scheme_ids)
       unless @validate_lodgement_use_case.execute(xml, content_type)
         raise ValidationError
       end
 
       hash = xml_to_hash(xml)
+
+      validate_assessor_can_lodge(hash, scheme_ids)
 
       @lodge_assessment_use_case.execute(hash, assessment_id)
     end
@@ -23,6 +26,16 @@ module UseCase
 
     def xml_to_hash(xml)
       Hash.from_xml(xml).deep_symbolize_keys
+    end
+
+    def validate_assessor_can_lodge(hash, scheme_ids)
+      scheme_assessor_id =
+        hash[:RdSAP_Report][:Report_Header][:Energy_Assessor][
+          :Identification_Number
+        ][
+          :Membership_Number
+        ]
+      @check_assessor_belongs_to_scheme.execute(scheme_assessor_id, scheme_ids)
     end
   end
 end
--- spec/unit/use_case/validate_and_lodge_assessment_spec.rb
+++ spec/unit/use_case/validate_and_lodge_assessment_spec.rb
@@ -19,6 +19,16 @@ describe UseCase::ValidateAndLodgeAssessment do
     end
   end
 
+  class FakeCheckAssessorBelongsToScheme
+    def execute(*)
+      @called = true
+    end
+
+    def is_called
+      @called
+    end
+  end
+
   let(:valid_xml) do
     File.read File.join Dir.pwd, 'api/schemas/xml/examples/RdSAP-19.01.xml'
   end
@@ -27,9 +37,10 @@ describe UseCase::ValidateAndLodgeAssessment do
     it 'will call the two use cases' do
       validate_lodgement_use_case = FakeValidateLodgementUseCase.new
       lodge_assessment_use_case = FakeLodgeAssessmentUseCase.new
+      check_assessor_belongs_to_scheme = FakeCheckAssessorBelongsToScheme.new
 
-      use_case = described_class.new(validate_lodgement_use_case, lodge_assessment_use_case)
-      use_case.execute("0000-0000-0000-0000-0000", valid_xml, "application/xml+RdSAP-Schema-19.0")
+      use_case = described_class.new(validate_lodgement_use_case, lodge_assessment_use_case, check_assessor_belongs_to_scheme)
+      use_case.execute("0000-0000-0000-0000-0000", valid_xml, "application/xml+RdSAP-Schema-19.0", "1")
 
       expect(validate_lodgement_use_case.is_called).to eq(true)
       expect(lodge_assessment_use_case.is_called).to eq(true)
