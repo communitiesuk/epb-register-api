fdd8b274822e5f708e3b9f3cd6a39f2a44b3ecec
Lawrence <lawrence.goldstien@madetech.com>
2020-05-12 11:29:02 +0100

    Return a 422 when no parameters in request
    Co-authored-by: Kev <kevin.keenoy@communities.gov.uk>

--- lib/controller/address_controller.rb
+++ lib/controller/address_controller.rb
@@ -1,8 +1,12 @@
 module Controller
   class AddressController < Controller::BaseController
-    get "/api/address/search",
-        jwt_auth: %w[address:search] do
+    get "/api/address/search", jwt_auth: %w[address:search] do
 
+      error_response(
+        422,
+        "INVALID_QUERY",
+        "Must specify either postcode or street and town or buildingReferenceNumber",
+      )
     end
   end
 end
--- spec/acceptance/address_search/search_domestic_addresses_spec.rb
+++ spec/acceptance/address_search/search_domestic_addresses_spec.rb
@@ -1,6 +1,20 @@
 context "searching for an address" do
   include RSpecAssessorServiceMixin
 
+  context "with an invalid combination of parameters" do
+    describe "no parameters" do
+      it "returns a validation error" do
+        assertive_get(
+          "/api/address/search",
+          [422],
+          true,
+          nil,
+          %w[address:search],
+        )
+      end
+    end
+  end
+
   context "with invalid auth details" do
     describe "with no authentication token" do
       it "returns a 401" do
