af4c4c3f1ea804f1e45e0bea51cb75e4eb7826bf
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-05-12 13:54:41 +0200

    Merge branch 'master' of /Users/marten.wetterberg/Projects/MHCLG/epb-register-api with conflicts.

--- lib/controller/address_controller.rb
+++ lib/controller/address_controller.rb
@@ -1,11 +1,7 @@
 module Controller
   class AddressController < Controller::BaseController
-    get "/api/address/search", jwt_auth: %w[address:search] do
-      error_response(
-        422,
-        "INVALID_QUERY",
-        "Must specify either postcode or street and town or buildingReferenceNumber",
-      )
+    get "/api/address/search",
+        jwt_auth: %w[address:search] do
     end
   end
 end
