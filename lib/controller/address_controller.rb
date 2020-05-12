f555dfcd9c4137b3ffe284abf32645aa0af956de
Yusuf <yusuf@madetech.com>
2020-05-12 13:06:19 +0100

    Add error response to address controller
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- lib/controller/address_controller.rb
+++ lib/controller/address_controller.rb
@@ -1,7 +1,11 @@
 module Controller
   class AddressController < Controller::BaseController
-    get "/api/address/search",
-        jwt_auth: %w[address:search] do
+    get "/api/address/search", jwt_auth: %w[address:search] do
+      error_response(
+        422,
+        "INVALID_QUERY",
+        "Must specify either postcode or street and town or buildingReferenceNumber",
+      )
     end
   end
 end
