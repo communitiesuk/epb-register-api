2b9dfef17a1f788c7d859a4bb7605ee0abdac51a
Yusuf <yusuf@madetech.com>
2020-05-19 14:44:47 +0100

    Refactor: Apply make slow-format

--- db/migrate/20200519132058_remove_related_party_disclosure_number_from_assessors.rb
+++ db/migrate/20200519132058_remove_related_party_disclosure_number_from_assessors.rb
@@ -1,4 +1,6 @@
-class RemoveRelatedPartyDisclosureNumberFromAssessors < ActiveRecord::Migration[6.0]
+class RemoveRelatedPartyDisclosureNumberFromAssessors < ActiveRecord::Migration[
+  6.0
+]
   def change
     remove_column :assessors, :related_party_disclosure_number
   end
