a093f5c9236878a6a9c6b76d6fd745e2884b7095
Yusuf <yusuf@madetech.com>
2020-05-20 15:39:28 +0100

    Refactor: Move address type tests to relevant acceptance test suite
    Co-authored-by: Lawrence <lawrence@madetech.com>

--- spec/acceptance/address_search/search_domestic_addresses_by_postcode_spec.rb
+++ spec/acceptance/address_search/search_domestic_addresses_by_postcode_spec.rb
@@ -1,6 +1,10 @@
 describe "searching for an address by postcode" do
   include RSpecAssessorServiceMixin
 
+  let(:valid_cepc_xml) do
+    File.read File.join Dir.pwd, "api/schemas/xml/examples/CEPC-7.11 (EPC).xml"
+  end
+
   let(:valid_rdsap_xml) do
     File.read File.join Dir.pwd, "api/schemas/xml/examples/RdSAP-19.01.xml"
   end
@@ -45,6 +49,25 @@ describe "searching for an address by postcode" do
         accepted_responses: [201],
         auth_data: { scheme_ids: [scheme_id] },
       )
+
+      non_domestic_xml = Nokogiri.XML valid_cepc_xml
+
+      assessment_id = non_domestic_xml.at("//CEPC:RRN")
+      assessment_id.children = "0000-0000-0000-0000-0002"
+
+      address_line_one = non_domestic_xml.at("//CEPC:Address-Line-1")
+      address_line_one.children = "3 Other Street"
+
+      scheme_assessor_id = non_domestic_xml.at("//CEPC:Certificate-Number")
+      scheme_assessor_id.children = "TEST000000"
+
+      lodge_assessment(
+        assessment_id: "0000-0000-0000-0000-0002",
+        assessment_body: non_domestic_xml.to_xml,
+        accepted_responses: [201],
+        auth_data: { scheme_ids: [scheme_id] },
+        schema_name: "CEPC-7.1",
+      )
     end
 
     describe "searching by postcode" do
@@ -61,7 +84,7 @@ describe "searching for an address by postcode" do
               .body,
           )
 
-        expect(response["data"]["addresses"].length).to eq 2
+        expect(response["data"]["addresses"].length).to eq 3
         expect(
           response["data"]["addresses"][0]["buildingReferenceNumber"],
         ).to eq "RRN-0000-0000-0000-0000-0000"
@@ -84,7 +107,7 @@ describe "searching for an address by postcode" do
                 .body,
             )
 
-          expect(response["data"]["addresses"].length).to eq 2
+          expect(response["data"]["addresses"].length).to eq 3
           expect(
             response["data"]["addresses"][0]["buildingReferenceNumber"],
           ).to eq "RRN-0000-0000-0000-0000-0000"
@@ -121,6 +144,32 @@ describe "searching for an address by postcode" do
             expect(response["data"]["addresses"][0]["town"]).to eq "Post-Town1"
             expect(response["data"]["addresses"][0]["postcode"]).to eq "A0 0AA"
           end
+        end
+      end
+
+      context "when an address type is provided" do
+        it "returns the correct address" do
+          response =
+            JSON.parse(
+              assertive_get(
+                "/api/search/addresses?postcode=A0%200AA&addressType=DOMESTIC",
+                [200],
+                true,
+                {},
+                %w[address:search],
+              )
+                .body,
+            )
+
+          expect(response["data"]["addresses"].length).to eq 2
+          expect(
+            response["data"]["addresses"][0]["buildingReferenceNumber"],
+          ).to eq "RRN-0000-0000-0000-0000-0000"
+          expect(
+            response["data"]["addresses"][0]["line1"],
+          ).to eq "1 Some Street"
+          expect(response["data"]["addresses"][0]["town"]).to eq "Post-Town1"
+          expect(response["data"]["addresses"][0]["postcode"]).to eq "A0 0AA"
         end
       end
     end
