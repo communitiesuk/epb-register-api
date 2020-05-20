0d2f314d59c8283bea667721450132516e972761
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-05-20 16:29:57 +0200

    Prevent pluralising of assessments_xml table
    Co-authored-by: George <george@madetech.com>

--- lib/gateway/assessments_xml_gateway.rb
+++ lib/gateway/assessments_xml_gateway.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.uncountable "assessments_xml"
+end
+
 module Gateway
   class AssessmentsXmlGateway
     def send_to_db(record); end
