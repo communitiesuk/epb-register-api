95acc439e876350c4043dce0dab7e6f88cb53aab
George Schena <george@madetech.com>
2020-05-21 11:06:34 +0100

    Create dedicated xml spec

---
+++ spec/acceptance/assessment/lodge_assessment/xml_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+describe "Acceptance::LodgeAssessment::XML" do
+  include RSpecAssessorServiceMixin
+
+  let(:valid_assessor_request_body) do
+    {
+      firstName: "Someone",
+      middleNames: "Muddle",
+      lastName: "Person",
+      dateOfBirth: "1991-02-25",
+      searchResultsComparisonPostcode: "",
+      qualifications: {
+        nonDomesticNos3: "ACTIVE",
+        nonDomesticNos4: "ACTIVE",
+        nonDomesticNos5: "INACTIVE",
+      },
+      contactDetails: {
+        telephoneNumber: "010199991010101", email: "person@person.com"
+      },
+    }
+  end
+
+  let(:valid_cepc_xml) do
+    File.read File.join Dir.pwd, "api/schemas/xml/examples/CEPC-7.11(ACIC).xml"
+  end
+
+  def get_stored_xml(assessment_id)
+    results =
+      ActiveRecord::Base.connection.execute(
+        "SELECT xml FROM assessments_xml WHERE assessment_id = '" +
+          ActiveRecord::Base.sanitize_sql(assessment_id) +
+          "'",
+      )
+
+    xml = ""
+    results.each { |row| xml = row["xml"] }
+    xml
+  end
+
+  context "when assessment xml is stored" do
+    it "is saved in assessments_xml table" do
+      scheme_id = add_scheme_and_get_id
+      add_assessor(scheme_id, "JASE000000", valid_assessor_request_body)
+
+      lodge_assessment(
+        assessment_id: "0000-0000-0000-0000-0000",
+        assessment_body: valid_cepc_xml,
+        accepted_responses: [201],
+        auth_data: { scheme_ids: [scheme_id] },
+        schema_name: "CEPC-7.1",
+      )
+
+      expect(valid_cepc_xml).to eq(
+        '<?xml version="1.0" encoding="UTF-8"?>
+' + get_stored_xml("0000-0000-0000-0000-0000"),
+      )
+    end
+  end
+end
