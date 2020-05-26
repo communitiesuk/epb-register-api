a2d4b1516be4f53ba4b5ffa7b53fcb7658a01cbb
Yusuf <yusuf@madetech.com>
2020-05-26 14:47:29 +0100

    Add unit tests to check invalid postcodes do not match regex pattern

--- spec/unit/helper/regex_helper_spec.rb
+++ spec/unit/helper/regex_helper_spec.rb
@@ -13,5 +13,55 @@ describe Helper::RegexHelper do
         end
       end
     end
+
+    context "with an invalid postcode" do
+      describe "INVALID" do
+        it "does not validate" do
+          expect("INVALID").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A00" do
+        it "does not validate" do
+          expect("A00").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A00 0AAB" do
+        it "does not validate" do
+          expect("A00 0AAB").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A000AAB" do
+        it "does not validate" do
+          expect("A000AAB").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A000 0AA" do
+        it "does not validate" do
+          expect("A000 0AA").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A0000AA" do
+        it "does not validate" do
+          expect("A0000AA").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A0 0AAA" do
+        it "does not validate" do
+          expect("A0 0AAA").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+
+      describe "A00AAA" do
+        it "does not validate" do
+          expect("A00AAA").not_to match Regexp.new described_class::POSTCODE
+        end
+      end
+    end
   end
 end
