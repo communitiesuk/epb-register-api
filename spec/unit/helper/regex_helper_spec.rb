faa23faac68636cdb540743c2666b7398322b362
Yusuf <yusuf@madetech.com>
2020-05-26 14:36:19 +0100

    Add unit test to validate postcodes without a space

--- spec/unit/helper/regex_helper_spec.rb
+++ spec/unit/helper/regex_helper_spec.rb
@@ -6,6 +6,12 @@ describe Helper::RegexHelper do
           expect("A0 0AA").to match Regexp.new described_class::POSTCODE
         end
       end
+
+      describe "A00AA" do
+        it "validates" do
+          expect("A00AA").to match Regexp.new described_class::POSTCODE
+        end
+      end
     end
   end
 end
