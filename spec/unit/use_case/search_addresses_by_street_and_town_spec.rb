6ac1b5718afcc396d0fe88c74a2f1fec0fe85b30
Lawrence <lawrence.goldstien@madetech.com>
2020-05-28 14:46:46 +0100

    Refactor: increase granularity of unit tests
    - updated searching for address by street and town use case unit test

--- spec/unit/use_case/search_addresses_by_street_and_town_spec.rb
+++ spec/unit/use_case/search_addresses_by_street_and_town_spec.rb
@@ -100,115 +100,231 @@ describe UseCase::SearchAddressesByStreetAndTown do
     let(:use_case) { described_class.new gateway }
 
     describe "by street and town" do
-      it "returns the expected address" do
-        results = use_case.execute street: "Home Road", town: "Placeville"
+      let(:results) { use_case.execute street: "Home Road", town: "Placeville" }
 
+      it "returns the expected number of results" do
         expect(results.length).to eq 4
+      end
+
+      it "returns the expected building reference number" do
         expect(
           results[0].building_reference_number,
         ).to eq "RRN-0000-0000-0000-0000-0000"
+      end
+
+      it "returns the expected first line of the address" do
         expect(results[0].line1).to eq "127 Home Road"
+      end
+
+      it "returns the expected second line of the address" do
         expect(results[0].line2).to be_nil
+      end
+
+      it "returns the expected third line of the address" do
         expect(results[0].line3).to be_nil
+      end
+
+      it "returns the expected town" do
         expect(results[0].town).to eq "Placeville"
+      end
+
+      it "returns the expected postcode" do
         expect(results[0].postcode).to eq "PL4 V11"
+      end
+
+      it "returns the expected address source" do
         expect(results[0].source).to eq "PREVIOUS_ASSESSMENT"
-        expect(results[0].existing_assessments).to eq [
-          assessment_id: "0000-0000-0000-0000-0000",
-          assessment_status: "ENTERED",
-          assessment_type: "CEPC",
-        ]
+      end
+
+      it "returns the expected list of existing assessments" do
+        expect(results[0].existing_assessments.length).to eq 1
+      end
+
+      it "returns an existing assessment with the expected RRN" do
+        expect(
+          results[0].existing_assessments[0][:assessment_id],
+        ).to eq "0000-0000-0000-0000-0000"
+      end
+
+      it "returns an existing assessment with the expected status" do
+        expect(
+          results[0].existing_assessments[0][:assessment_status],
+        ).to eq "ENTERED"
+      end
+
+      it "returns an existing assessment of the expected type" do
+        expect(
+          results[0].existing_assessments[0][:assessment_type],
+        ).to eq "CEPC"
       end
 
       context "when street is on address line 2" do
-        it "returns the expected address" do
-          results = use_case.execute street: "Home Road", town: "Placeville"
+        describe "searching for an address" do
+          let(:results) do
+            use_case.execute street: "Home Road", town: "Placeville"
+          end
 
-          expect(results.length).to eq 4
-          expect(
-            results[2].building_reference_number,
-          ).to eq "RRN-0000-0000-0000-0000-0002"
-          expect(results[2].line1).to eq "The Name"
-          expect(results[2].line2).to eq "129 Home Road"
-          expect(results[2].line3).to be_nil
-          expect(results[2].town).to eq "Placeville"
-          expect(results[2].postcode).to eq "PL4 V13"
-          expect(results[2].source).to eq "PREVIOUS_ASSESSMENT"
-          expect(results[2].existing_assessments).to eq [
-            assessment_id: "0000-0000-0000-0000-0002",
-            assessment_status: "ENTERED",
-            assessment_type: "RdSAP",
-          ]
+          it "returns the expected number of results" do
+            expect(results.length).to eq 4
+          end
+
+          it "returns the expected building reference number" do
+            expect(
+              results[2].building_reference_number,
+            ).to eq "RRN-0000-0000-0000-0000-0002"
+          end
+
+          it "returns the expected first line of the address" do
+            expect(results[2].line1).to eq "The Name"
+          end
+
+          it "returns the expected second line of the address" do
+            expect(results[2].line2).to eq "129 Home Road"
+          end
+
+          it "returns the expected third line of the address" do
+            expect(results[2].line3).to be_nil
+          end
+
+          it "returns the expected town" do
+            expect(results[2].town).to eq "Placeville"
+          end
+
+          it "returns the expected postcode" do
+            expect(results[2].postcode).to eq "PL4 V13"
+          end
+
+          it "returns the expected address source" do
+            expect(results[2].source).to eq "PREVIOUS_ASSESSMENT"
+          end
+
+          it "returns the expected list of existing assessments" do
+            expect(results[2].existing_assessments.length).to eq 1
+          end
+
+          it "returns an existing assessment with the expected RRN" do
+            expect(
+              results[2].existing_assessments[0][:assessment_id],
+            ).to eq "0000-0000-0000-0000-0002"
+          end
+
+          it "returns an existing assessment with the expected status" do
+            expect(
+              results[2].existing_assessments[0][:assessment_status],
+            ).to eq "ENTERED"
+          end
+
+          it "returns an existing assessment of the expected type" do
+            expect(
+              results[2].existing_assessments[0][:assessment_type],
+            ).to eq "RdSAP"
+          end
         end
       end
 
       context "when town is on address line 2" do
-        it "returns the expected address" do
-          results = use_case.execute street: "Home Road", town: "Placeville"
+        describe "searching for an address" do
+          let(:results) do
+            use_case.execute street: "Home Road", town: "Placeville"
+          end
 
-          expect(results.length).to eq 4
-          expect(
-            results[3].building_reference_number,
-          ).to eq "RRN-0000-0000-0000-0000-0003"
-          expect(results[3].line1).to eq "130 Home Road"
-          expect(results[3].line2).to eq "Placeville"
-          expect(results[3].line3).to be_nil
-          expect(results[3].town).to eq "Countyshire"
-          expect(results[3].postcode).to eq "PL4 V14"
-          expect(results[3].source).to eq "PREVIOUS_ASSESSMENT"
-          expect(results[3].existing_assessments).to eq [
-            assessment_id: "0000-0000-0000-0000-0003",
-            assessment_status: "ENTERED",
-            assessment_type: "RdSAP",
-          ]
+          it "returns the expected number of results" do
+            expect(results.length).to eq 4
+          end
+
+          it "returns the expected building reference number" do
+            expect(
+              results[2].building_reference_number,
+            ).to eq "RRN-0000-0000-0000-0000-0002"
+          end
+
+          it "returns the expected first line of the address" do
+            expect(results[2].line1).to eq "The Name"
+          end
+
+          it "returns the expected second line of the address" do
+            expect(results[2].line2).to eq "129 Home Road"
+          end
+
+          it "returns the expected third line of the address" do
+            expect(results[2].line3).to be_nil
+          end
+
+          it "returns the expected town" do
+            expect(results[2].town).to eq "Placeville"
+          end
+
+          it "returns the expected postcode" do
+            expect(results[2].postcode).to eq "PL4 V13"
+          end
+
+          it "returns the expected address source" do
+            expect(results[2].source).to eq "PREVIOUS_ASSESSMENT"
+          end
+
+          it "returns the expected list of existing assessments" do
+            expect(results[2].existing_assessments.length).to eq 1
+          end
+
+          it "returns an existing assessment with the expected RRN" do
+            expect(
+              results[2].existing_assessments[0][:assessment_id],
+            ).to eq "0000-0000-0000-0000-0002"
+          end
+
+          it "returns an existing assessment with the expected status" do
+            expect(
+              results[2].existing_assessments[0][:assessment_status],
+            ).to eq "ENTERED"
+          end
+
+          it "returns an existing assessment of the expected type" do
+            expect(
+              results[2].existing_assessments[0][:assessment_type],
+            ).to eq "RdSAP"
+          end
         end
       end
 
       context "when address type is provided" do
-        it "returns the domestic address" do
-          results =
+        describe "searching for domestic addresses" do
+          let(:results) do
             use_case.execute street: "Home Road",
                              town: "Placeville",
                              address_type: "DOMESTIC"
+          end
 
-          expect(results.length).to eq 3
-          expect(
-            results[0].building_reference_number,
-          ).to eq "RRN-0000-0000-0000-0000-0001"
-          expect(results[0].line1).to eq "128 Home Road"
-          expect(results[0].line2).to be_nil
-          expect(results[0].line3).to be_nil
-          expect(results[0].town).to eq "Placeville"
-          expect(results[0].postcode).to eq "PL4 V12"
-          expect(results[0].source).to eq "PREVIOUS_ASSESSMENT"
-          expect(results[0].existing_assessments).to eq [
-            assessment_id: "0000-0000-0000-0000-0001",
-            assessment_status: "ENTERED",
-            assessment_type: "RdSAP",
-          ]
+          it "returns the expected number of results" do
+            expect(results.length).to eq 3
+          end
+
+          it "returns a list existing assessments of the expected type" do
+            types =
+              results.map(&:existing_assessments).map(&:first).map do |ea|
+                ea[:assessment_type]
+              end
+            expect(types).to eq %w[RdSAP RdSAP RdSAP]
+          end
         end
 
-        it "returns the commercial address" do
-          results =
+        describe "searching for commercial addresses" do
+          let(:results) do
             use_case.execute street: "Home Road",
                              town: "Placeville",
                              address_type: "COMMERCIAL"
+          end
 
-          expect(results.length).to eq 1
-          expect(
-            results[0].building_reference_number,
-          ).to eq "RRN-0000-0000-0000-0000-0000"
-          expect(results[0].line1).to eq "127 Home Road"
-          expect(results[0].line2).to be_nil
-          expect(results[0].line3).to be_nil
-          expect(results[0].town).to eq "Placeville"
-          expect(results[0].postcode).to eq "PL4 V11"
-          expect(results[0].source).to eq "PREVIOUS_ASSESSMENT"
-          expect(results[0].existing_assessments).to eq [
-            assessment_id: "0000-0000-0000-0000-0000",
-            assessment_status: "ENTERED",
-            assessment_type: "CEPC",
-          ]
+          it "returns the expected number of results" do
+            expect(results.length).to eq 1
+          end
+
+          it "returns a list existing assessments of the expected type" do
+            types =
+              results.map(&:existing_assessments).map(&:first).map do |ea|
+                ea[:assessment_type]
+              end
+            expect(types).to eq %w[CEPC]
+          end
         end
       end
     end
