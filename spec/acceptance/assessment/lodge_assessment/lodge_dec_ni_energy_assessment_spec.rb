eb4b36122a5d53f7ea96c25ea0aa18fa797027e3
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-05-28 14:47:45 +0200

    Add ability to load a Northern Irish DEC

---
+++ spec/acceptance/assessment/lodge_assessment/lodge_dec_ni_energy_assessment_spec.rb
@@ -0,0 +1,181 @@
+# frozen_string_literal: true
+
+describe "Acceptance::LodgeDECNIEnergyAssessment" do
+  include RSpecAssessorServiceMixin
+
+  let(:valid_assessor_request_body) do
+    {
+      firstName: "Someone",
+      middleNames: "Muddle",
+      lastName: "Person",
+      dateOfBirth: "1991-02-25",
+      searchResultsComparisonPostcode: "",
+      qualifications: {
+        domesticSap: "INACTIVE",
+        domesticRdSap: "INACTIVE",
+        nonDomesticSp3: "INACTIVE",
+        nonDomesticCc4: "INACTIVE",
+        nonDomesticDec: "ACTIVE",
+        nonDomesticNos3: "INACTIVE",
+        nonDomesticNos4: "INACTIVE",
+        nonDomesticNos5: "INACTIVE",
+        gda: "INACTIVE",
+      },
+      contactDetails: {
+        telephoneNumber: "010199991010101", email: "person@person.com"
+      },
+    }
+  end
+
+  let(:inactive_assessor_request_body) do
+    {
+      firstName: "Someone",
+      middleNames: "Muddle",
+      lastName: "Person",
+      dateOfBirth: "1991-02-25",
+      searchResultsComparisonPostcode: "",
+      qualifications: { nonDomesticDec: "INACTIVE" },
+      contactDetails: {
+        telephoneNumber: "010199991010101", email: "person@person.com"
+      },
+    }
+  end
+
+  let(:valid_dec_ni_xml) do
+    File.read File.join Dir.pwd,
+                        "api/schemas/xml/examples/CEPC-NI-7.11(DEC).xml"
+  end
+
+  context "when lodging a DEC NI assessment (post)" do
+    context "when an assessor is inactive" do
+      let(:scheme_id) { add_scheme_and_get_id }
+
+      before do
+        add_assessor(scheme_id, "JASE000000", inactive_assessor_request_body)
+      end
+
+      context "when unqualified for DEC" do
+        it "returns status 400 with the correct error response" do
+          response =
+            JSON.parse(
+              lodge_assessment(
+                assessment_body: valid_dec_ni_xml,
+                accepted_responses: [400],
+                auth_data: { scheme_ids: [scheme_id] },
+                schema_name: "CEPC-NI-7.1",
+              )
+                .body,
+            )
+
+          expect(response["errors"][0]["title"]).to eq(
+            "Assessor is not active.",
+          )
+        end
+      end
+    end
+
+    it "returns status 201" do
+      scheme_id = add_scheme_and_get_id
+      add_assessor(scheme_id, "JASE000000", valid_assessor_request_body)
+
+      lodge_assessment(
+        assessment_body: valid_dec_ni_xml,
+        accepted_responses: [201],
+        auth_data: { scheme_ids: [scheme_id] },
+        schema_name: "CEPC-NI-7.1",
+      )
+    end
+
+    context "when saving a (DEC NI) assessment" do
+      let(:scheme_id) { add_scheme_and_get_id }
+      let(:doc) { Nokogiri.XML valid_dec_ni_xml }
+      let(:response) do
+        JSON.parse(fetch_assessment("1234-1234-1234-1234-1234").body)
+      end
+
+      before do
+        add_assessor(scheme_id, "JASE000000", valid_assessor_request_body)
+
+        assessment_id = doc.at("RRN")
+        assessment_id.children = "1234-1234-1234-1234-1234"
+
+        scheme_assessor_id = doc.at("Certificate-Number")
+        scheme_assessor_id.children = "JASE000000"
+      end
+
+      it "returns the data that was lodged" do
+        lodge_assessment(
+          assessment_body: doc.to_xml,
+          accepted_responses: [201],
+          auth_data: { scheme_ids: [scheme_id] },
+          schema_name: "CEPC-NI-7.1",
+        )
+
+        expected_response = {
+          "addressLine1" => "2 Lonely Street",
+          "addressLine2" => "",
+          "addressLine3" => "",
+          "addressLine4" => "",
+          "addressSummary" => "2 Lonely Street, Post-Town1, A0 0AA",
+          "assessmentId" => "1234-1234-1234-1234-1234",
+          "assessor" => {
+            "contactDetails" => {
+              "email" => "person@person.com",
+              "telephoneNumber" => "010199991010101",
+            },
+            "dateOfBirth" => "1991-02-25",
+            "firstName" => "Someone",
+            "lastName" => "Person",
+            "middleNames" => "Muddle",
+            "qualifications" => {
+              "domesticSap" => "INACTIVE",
+              "domesticRdSap" => "INACTIVE",
+              "nonDomesticCc4" => "INACTIVE",
+              "nonDomesticSp3" => "INACTIVE",
+              "nonDomesticDec" => "ACTIVE",
+              "nonDomesticNos3" => "INACTIVE",
+              "nonDomesticNos4" => "INACTIVE",
+              "nonDomesticNos5" => "INACTIVE",
+              "gda" => "INACTIVE",
+            },
+            "address" => {},
+            "companyDetails" => {},
+            "registeredBy" => {
+              "name" => "test scheme", "schemeId" => scheme_id
+            },
+            "schemeAssessorId" => "JASE000000",
+            "searchResultsComparisonPostcode" => "",
+          },
+          "currentCarbonEmission" => 0.0,
+          "currentEnergyEfficiencyBand" => "a",
+          "currentEnergyEfficiencyRating" => 99,
+          "optOut" => false,
+          "dateOfAssessment" => "2006-05-04",
+          "dateOfExpiry" => "2016-05-04",
+          "dateRegistered" => "2006-05-04",
+          "dwellingType" => nil,
+          "heatDemand" => {
+            "currentSpaceHeatingDemand" => 0.0,
+            "currentWaterHeatingDemand" => 0.0,
+            "impactOfCavityInsulation" => nil,
+            "impactOfLoftInsulation" => nil,
+            "impactOfSolidWallInsulation" => nil,
+          },
+          "postcode" => "A0 0AA",
+          "potentialCarbonEmission" => 0.0,
+          "potentialEnergyEfficiencyBand" => "a",
+          "potentialEnergyEfficiencyRating" => 99,
+          "totalFloorArea" => 0.0,
+          "town" => "Post-Town1",
+          "typeOfAssessment" => "DEC",
+          "relatedPartyDisclosureNumber" => nil,
+          "relatedPartyDisclosureText" => nil,
+          "recommendedImprovements" => [],
+          "propertySummary" => [],
+        }
+
+        expect(response["data"]).to eq(expected_response)
+      end
+    end
+  end
+end
