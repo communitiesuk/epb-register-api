82514a1232d256f5d39d03d12b391d783f0440ea
George Schena <george@madetech.com>
2020-05-28 16:53:22 +0100

    Lodge DEC-ARs

---
+++ spec/acceptance/assessment/lodge/england_wales/ar_spec.rb
@@ -0,0 +1,175 @@
+# frozen_string_literal: true
+
+describe "Acceptance::LodgeAdvisoryReport" do
+  include RSpecAssessorServiceMixin
+
+  let(:valid_assessor_request_body) do
+    {
+      firstName: "Someone",
+      middleNames: "Muddle",
+      lastName: "Person",
+      dateOfBirth: "1991-02-25",
+      searchResultsComparisonPostcode: "",
+      qualifications: {
+        nonDomesticNos3: "ACTIVE",
+        nonDomesticNos4: "ACTIVE",
+        nonDomesticNos5: "INACTIVE",
+      },
+      contactDetails: {
+        telephoneNumber: "010199991010101", email: "person@person.com"
+      },
+    }
+  end
+
+  let(:inactive_assessor_request_body) do
+    {
+      firstName: "Someone",
+      middleNames: "Muddle",
+      lastName: "Person",
+      dateOfBirth: "1991-02-25",
+      searchResultsComparisonPostcode: "",
+      qualifications: { nonDomesticDec: "INACTIVE" },
+      contactDetails: {
+        telephoneNumber: "010199991010101", email: "person@person.com"
+      },
+    }
+  end
+
+  let(:valid_ar_xml) do
+    File.read File.join Dir.pwd,
+                        "api/schemas/xml/examples/CEPC-7.11(Advisory-Report).xml"
+  end
+
+  context "when lodging a AR (post)" do
+    context "when an assessor is inactive" do
+      let(:scheme_id) { add_scheme_and_get_id }
+
+      before do
+        add_assessor(scheme_id, "JASE000000", inactive_assessor_request_body)
+      end
+
+      context "when unqualified for AR" do
+        it "returns status 400 with the correct error response" do
+          response =
+            JSON.parse(
+              lodge_assessment(
+                assessment_body: valid_ar_xml,
+                accepted_responses: [400],
+                auth_data: { scheme_ids: [scheme_id] },
+                schema_name: "CEPC-7.1",
+              )
+                .body,
+            )
+
+          expect(response["errors"][0]["title"]).to eq(
+            "Assessor is not active.",
+          )
+        end
+      end
+    end
+
+    it "returns status 201" do
+      scheme_id = add_scheme_and_get_id
+      add_assessor(scheme_id, "JASE000000", valid_assessor_request_body)
+
+      lodge_assessment(
+        assessment_body: valid_ar_xml,
+        accepted_responses: [201],
+        auth_data: { scheme_ids: [scheme_id] },
+        schema_name: "CEPC-7.1",
+      )
+    end
+
+    context "when saving a AR" do
+      let(:scheme_id) { add_scheme_and_get_id }
+      let(:doc) { Nokogiri.XML valid_ar_xml }
+      let(:response) do
+        JSON.parse(fetch_assessment("0000-0000-0000-0000-0000").body)
+      end
+
+      before do
+        add_assessor(scheme_id, "JASE000000", valid_assessor_request_body)
+
+        assessment_id = doc.at("RRN")
+        assessment_id.children = "0000-0000-0000-0000-0000"
+
+        scheme_assessor_id = doc.at("Certificate-Number")
+        scheme_assessor_id.children = "JASE000000"
+      end
+
+      it "returns the data that was lodged" do
+        lodge_assessment(
+          assessment_body: doc.to_xml,
+          accepted_responses: [201],
+          auth_data: { scheme_ids: [scheme_id] },
+          schema_name: "CEPC-7.1",
+        )
+
+        expected_response = {
+          "addressLine1" => "1 Lonely Street",
+          "addressLine2" => "",
+          "addressLine3" => "",
+          "addressLine4" => "",
+          "addressSummary" => "1 Lonely Street, Post-Town0, A0 0AA",
+          "assessmentId" => "0000-0000-0000-0000-0000",
+          "assessor" => {
+            "contactDetails" => {
+              "email" => "person@person.com",
+              "telephoneNumber" => "010199991010101",
+            },
+            "dateOfBirth" => "1991-02-25",
+            "firstName" => "Someone",
+            "lastName" => "Person",
+            "middleNames" => "Muddle",
+            "qualifications" => {
+              "domesticSap" => "INACTIVE",
+              "domesticRdSap" => "INACTIVE",
+              "nonDomesticCc4" => "INACTIVE",
+              "nonDomesticSp3" => "INACTIVE",
+              "nonDomesticDec" => "INACTIVE",
+              "nonDomesticNos3" => "ACTIVE",
+              "nonDomesticNos4" => "ACTIVE",
+              "nonDomesticNos5" => "INACTIVE",
+              "gda" => "INACTIVE",
+            },
+            "address" => {},
+            "companyDetails" => {},
+            "registeredBy" => {
+              "name" => "test scheme", "schemeId" => scheme_id
+            },
+            "schemeAssessorId" => "JASE000000",
+            "searchResultsComparisonPostcode" => "",
+          },
+          "currentCarbonEmission" => 0.0,
+          "currentEnergyEfficiencyBand" => "a",
+          "currentEnergyEfficiencyRating" => 99,
+          "optOut" => false,
+          "dateOfAssessment" => "2006-05-04",
+          "dateOfExpiry" => "2006-05-04",
+          "dateRegistered" => "2006-05-04",
+          "dwellingType" => nil,
+          "heatDemand" => {
+            "currentSpaceHeatingDemand" => 0.0,
+            "currentWaterHeatingDemand" => 0.0,
+            "impactOfCavityInsulation" => nil,
+            "impactOfLoftInsulation" => nil,
+            "impactOfSolidWallInsulation" => nil,
+          },
+          "postcode" => "A0 0AA",
+          "potentialCarbonEmission" => 0.0,
+          "potentialEnergyEfficiencyBand" => "a",
+          "potentialEnergyEfficiencyRating" => 99,
+          "totalFloorArea" => 99.0,
+          "town" => "Post-Town0",
+          "typeOfAssessment" => "DEC-AR",
+          "relatedPartyDisclosureNumber" => nil,
+          "relatedPartyDisclosureText" => nil,
+          "recommendedImprovements" => [],
+          "propertySummary" => [],
+        }
+
+        expect(response["data"]).to eq(expected_response)
+      end
+    end
+  end
+end
