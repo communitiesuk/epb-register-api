cc6c5e0973c1f8931cfb308a489559e0dbc99ad8
Yusuf <yusuf@madetech.com>
2020-05-29 11:49:02 +0100

    Refactor: Use let and before blocks for search addresses test file

--- spec/acceptance/address_search/search_addresses_spec.rb
+++ spec/acceptance/address_search/search_addresses_spec.rb
@@ -21,17 +21,18 @@ describe "Acceptance::AddressSearch" do
 
   context "with an invalid combination of parameters" do
     describe "no parameters" do
-      it "returns a validation error" do
-        response =
-          assertive_get(
-            "/api/search/addresses",
-            [422],
-            true,
-            nil,
-            %w[address:search],
-          )
-            .body
+      let(:response) do
+        assertive_get(
+          "/api/search/addresses",
+          [422],
+          true,
+          nil,
+          %w[address:search],
+        )
+          .body
+      end
 
+      it "returns a validation error" do
         expect(response).to include "INVALID_REQUEST"
       end
     end
@@ -53,8 +54,23 @@ describe "Acceptance::AddressSearch" do
 
   context "with an incomplete address" do
     describe "an address with only line1, town, and postcode" do
+      let(:scheme_id) { add_scheme_and_get_id }
+
       let(:response) do
-        scheme_id = add_scheme_and_get_id
+        JSON.parse(
+          assertive_get(
+            "/api/search/addresses?postcode=A0%200AA",
+            [200],
+            true,
+            nil,
+            %w[address:search],
+          )
+            .body,
+          symbolize_names: true,
+        )
+      end
+
+      before do
         add_assessor(scheme_id, "TEST000000", valid_assessor_request_body)
 
         lodge_assessment(
@@ -62,23 +78,14 @@ describe "Acceptance::AddressSearch" do
           accepted_responses: [201],
           auth_data: { scheme_ids: [scheme_id] },
         )
-
-        JSON.parse assertive_get(
-          "/api/search/addresses?postcode=A0%200AA",
-          [200],
-          true,
-          nil,
-          %w[address:search],
-        )
-                     .body
       end
 
       it "has nil for line2" do
-        expect(response["data"]["addresses"][0]["line2"]).to be_nil
+        expect(response[:data][:addresses][0][:line2]).to be_nil
       end
 
       it "has nil for line3" do
-        expect(response["data"]["addresses"][0]["line3"]).to be_nil
+        expect(response[:data][:addresses][0][:line3]).to be_nil
       end
     end
   end
