b77b7503318097ecdc99a6c1e8cb546e8bbfda2c
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-06-03 15:35:32 +0100

    Add migration to create gdp fuel savings

---
+++ db/migrate/20200603122802_add_gdp_fuel_savings.rb
@@ -0,0 +1,13 @@
+class AddGdpFuelSavings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :gdp_fuel_savings, { id: false } do |t|
+      t.string :green_deal_plan_id
+      t.integer :sequence
+    end
+
+    add_foreign_key :gdp_fuel_savings,
+                    :green_deal_plans,
+                    column: :green_deal_plan_id,
+                    primary_key: :green_deal_plan_id
+  end
+end
