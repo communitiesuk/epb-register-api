bc3639b4a086baa912f6afe3abf8d32c9f71f42b
Yusuf <yusuf@madetech.com>
2020-06-05 11:06:21 +0100

    Refactor: Introduce let and before blocks in ACIR spec so assertions are isolated
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- spec/acceptance/assessment/lodge/england_wales/acir_spec.rb
+++ spec/acceptance/assessment/lodge/england_wales/acir_spec.rb
@@ -10,110 +10,113 @@ describe "Acceptance::LodgeACIREnergyAssessment" do
   end
 
   context "when lodging ACIR" do
-    let(:response) do
-      JSON.parse(fetch_assessment("0000-0000-0000-0000-0000").body)
-    end
-
-    it "accepts an assessment with type ACIR" do
-      scheme_id = add_scheme_and_get_id
-      add_assessor(
-        scheme_id,
-        "MOSE000000",
-        fetch_assessor_stub.fetch_request_body(nonDomesticSp3: "ACTIVE"),
-      )
-
-      lodge_assessment(
-        assessment_body: valid_xml,
-        accepted_responses: [201],
-        auth_data: { scheme_ids: [scheme_id] },
-        schema_name: "CEPC-7.1",
-      )
-
-      expect(response["data"]["typeOfAssessment"]).to eq("ACIR")
-    end
-
-    it "returns the expected response" do
-      scheme_id = add_scheme_and_get_id
-      add_assessor(
-        scheme_id,
-        "MOSE000000",
-        fetch_assessor_stub.fetch_request_body(nonDomesticSp3: "ACTIVE"),
-      )
-
-      lodge_assessment(
-        assessment_body: valid_xml,
-        accepted_responses: [201],
-        auth_data: { scheme_ids: [scheme_id] },
-        schema_name: "CEPC-7.1",
-      )
-
-      expected_response = {
-        "addressLine1" => "2 Lonely Street",
-        "addressLine2" => "",
-        "addressLine3" => "",
-        "addressLine4" => "",
-        "assessmentId" => "0000-0000-0000-0000-0000",
-        "assessor" => {
-          "contactDetails" => {
-            "email" => "person@person.com",
-            "telephoneNumber" => "010199991010101",
-          },
-          "dateOfBirth" => "1991-02-25",
-          "firstName" => "Someone",
-          "lastName" => "Person",
-          "middleNames" => "Muddle",
-          "qualifications" => {
-            "domesticSap" => "INACTIVE",
-            "domesticRdSap" => "INACTIVE",
-            "nonDomesticCc4" => "INACTIVE",
-            "nonDomesticSp3" => "ACTIVE",
-            "nonDomesticDec" => "INACTIVE",
-            "nonDomesticNos3" => "INACTIVE",
-            "nonDomesticNos4" => "INACTIVE",
-            "nonDomesticNos5" => "INACTIVE",
-            "gda" => "INACTIVE",
-          },
-          "address" => {},
-          "companyDetails" => {},
-          "registeredBy" => {
-            "name" => "test scheme", "schemeId" => scheme_id
-          },
-          "schemeAssessorId" => "MOSE000000",
-          "searchResultsComparisonPostcode" => "",
-        },
-        "currentCarbonEmission" => 0.0,
-        "currentEnergyEfficiencyBand" => "a",
-        "currentEnergyEfficiencyRating" => 99,
-        "optOut" => false,
-        "dateOfAssessment" => "2006-05-04",
-        "dateOfExpiry" => "2006-05-04",
-        "dateRegistered" => "2006-05-04",
-        "dwellingType" => nil,
-        "heatDemand" => {
-          "currentSpaceHeatingDemand" => 0.0,
-          "currentWaterHeatingDemand" => 0.0,
-          "impactOfCavityInsulation" => nil,
-          "impactOfLoftInsulation" => nil,
-          "impactOfSolidWallInsulation" => nil,
-        },
-        "postcode" => "A0 0AA",
-        "potentialCarbonEmission" => 0.0,
-        "potentialEnergyEfficiencyBand" => "a",
-        "potentialEnergyEfficiencyRating" => 99,
-        "totalFloorArea" => 99.0,
-        "town" => "Post-Town1",
-        "typeOfAssessment" => "ACIR",
-        "relatedPartyDisclosureNumber" => nil,
-        "relatedPartyDisclosureText" => nil,
-        "recommendedImprovements" => [],
-        "propertySummary" => [],
-      }
-
-      expect(response["data"]).to eq(expected_response)
-    end
-
-    context "when an assessor is inactive" do
+    context "with an active assessor" do
       let(:scheme_id) { add_scheme_and_get_id }
+
+      let(:response) do
+        JSON.parse(fetch_assessment("0000-0000-0000-0000-0000").body)
+      end
+
+      before do
+        add_assessor(
+          scheme_id,
+          "MOSE000000",
+          fetch_assessor_stub.fetch_request_body(nonDomesticSp3: "ACTIVE"),
+        )
+
+        lodge_assessment(
+          assessment_body: valid_xml,
+          accepted_responses: [201],
+          auth_data: { scheme_ids: [scheme_id] },
+          schema_name: "CEPC-7.1",
+        )
+      end
+
+      it "accepts an assessment with type ACIR" do
+        expect(response["data"]["typeOfAssessment"]).to eq("ACIR")
+      end
+
+      it "returns the expected response" do
+        expected_response = {
+          "addressLine1" => "2 Lonely Street",
+          "addressLine2" => "",
+          "addressLine3" => "",
+          "addressLine4" => "",
+          "assessmentId" => "0000-0000-0000-0000-0000",
+          "assessor" => {
+            "contactDetails" => {
+              "email" => "person@person.com",
+              "telephoneNumber" => "010199991010101",
+            },
+            "dateOfBirth" => "1991-02-25",
+            "firstName" => "Someone",
+            "lastName" => "Person",
+            "middleNames" => "Muddle",
+            "qualifications" => {
+              "domesticSap" => "INACTIVE",
+              "domesticRdSap" => "INACTIVE",
+              "nonDomesticCc4" => "INACTIVE",
+              "nonDomesticSp3" => "ACTIVE",
+              "nonDomesticDec" => "INACTIVE",
+              "nonDomesticNos3" => "INACTIVE",
+              "nonDomesticNos4" => "INACTIVE",
+              "nonDomesticNos5" => "INACTIVE",
+              "gda" => "INACTIVE",
+            },
+            "address" => {},
+            "companyDetails" => {},
+            "registeredBy" => {
+              "name" => "test scheme", "schemeId" => scheme_id
+            },
+            "schemeAssessorId" => "MOSE000000",
+            "searchResultsComparisonPostcode" => "",
+          },
+          "currentCarbonEmission" => 0.0,
+          "currentEnergyEfficiencyBand" => "a",
+          "currentEnergyEfficiencyRating" => 99,
+          "optOut" => false,
+          "dateOfAssessment" => "2006-05-04",
+          "dateOfExpiry" => "2006-05-04",
+          "dateRegistered" => "2006-05-04",
+          "dwellingType" => nil,
+          "heatDemand" => {
+            "currentSpaceHeatingDemand" => 0.0,
+            "currentWaterHeatingDemand" => 0.0,
+            "impactOfCavityInsulation" => nil,
+            "impactOfLoftInsulation" => nil,
+            "impactOfSolidWallInsulation" => nil,
+          },
+          "postcode" => "A0 0AA",
+          "potentialCarbonEmission" => 0.0,
+          "potentialEnergyEfficiencyBand" => "a",
+          "potentialEnergyEfficiencyRating" => 99,
+          "totalFloorArea" => 99.0,
+          "town" => "Post-Town1",
+          "typeOfAssessment" => "ACIR",
+          "relatedPartyDisclosureNumber" => nil,
+          "relatedPartyDisclosureText" => nil,
+          "recommendedImprovements" => [],
+          "propertySummary" => [],
+        }
+
+        expect(response["data"]).to eq(expected_response)
+      end
+    end
+
+    context "with an inactive assessor" do
+      let(:scheme_id) { add_scheme_and_get_id }
+
+      let(:response) do
+        JSON.parse(
+          lodge_assessment(
+            assessment_body: valid_xml,
+            accepted_responses: [400],
+            auth_data: { scheme_ids: [scheme_id] },
+            schema_name: "CEPC-7.1",
+          )
+            .body,
+        )
+      end
 
       before do
         add_assessor(
@@ -123,41 +126,27 @@ describe "Acceptance::LodgeACIREnergyAssessment" do
         )
       end
 
-      context "when unqualified for ACIR" do
-        it "returns status 400 with the correct error response" do
-          response =
-            JSON.parse(
-              lodge_assessment(
-                assessment_body: valid_xml,
-                accepted_responses: [400],
-                auth_data: { scheme_ids: [scheme_id] },
-                schema_name: "CEPC-7.1",
-              )
-                .body,
-            )
-
-          expect(response["errors"][0]["title"]).to eq(
-            "Assessor is not active.",
-          )
-        end
+      it "returns status 400 with the correct error response" do
+        expect(response["errors"][0]["title"]).to eq("Assessor is not active.")
       end
     end
   end
 
   context "when rejecting an assessment" do
-    it "rejects an assessment without a ACI Related-Party-Disclosure" do
-      scheme_id = add_scheme_and_get_id
+    let(:scheme_id) { add_scheme_and_get_id }
+    let(:doc) { Nokogiri.XML valid_xml }
+
+    before do
       add_assessor(
         scheme_id,
         "MOSE000000",
         fetch_assessor_stub.fetch_request_body(nonDomesticSp3: "ACTIVE"),
       )
 
-      doc = Nokogiri.XML valid_xml
+      doc.at("ACI-Related-Party-Disclosure").remove
+    end
 
-      scheme_assessor_id = doc.at("ACI-Related-Party-Disclosure")
-      scheme_assessor_id.children = ""
-
+    it "rejects an assessment without an ACI Related-Party-Disclosure" do
       lodge_assessment(
         assessment_body: doc.to_xml,
         accepted_responses: [400],
