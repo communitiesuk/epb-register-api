82d21b5d7c07d35b140f0c27901158e99819cafe
George Schena <george@madetech.com>
2020-06-08 15:05:25 +0100

    Format

--- db/migrate/20200608133739_add_columns_to_green_deal_plans_table.rb
+++ db/migrate/20200608133739_add_columns_to_green_deal_plans_table.rb
@@ -1,21 +1,9 @@
 class AddColumnsToGreenDealPlansTable < ActiveRecord::Migration[6.0]
   def change
-    add_column :green_deal_plans,
-               :measures,
-               :jsonb,
-               null: false,
-               default: "{}"
+    add_column :green_deal_plans, :measures, :jsonb, null: false, default: "{}"
 
-    add_column :green_deal_plans,
-               :charges,
-               :jsonb,
-               null: false,
-               default: "{}"
+    add_column :green_deal_plans, :charges, :jsonb, null: false, default: "{}"
 
-    add_column :green_deal_plans,
-               :savings,
-               :jsonb,
-               null: false,
-               default: "{}"
+    add_column :green_deal_plans, :savings, :jsonb, null: false, default: "{}"
   end
 end
