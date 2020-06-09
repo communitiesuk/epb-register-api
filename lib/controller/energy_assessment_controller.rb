59584ce5b184b776f3763536500dd101d0dbd04e
Christine-horrocks <splogy2001@hotmail.com>
2020-06-09 12:59:17 +0100

    Remove get assessment with domestic-epc from controller

--- lib/controller/energy_assessment_controller.rb
+++ lib/controller/energy_assessment_controller.rb
@@ -171,21 +171,6 @@ module Controller
       end
     end
 
-    get "/api/assessments/domestic-epc/:assessment_id",
-        jwt_auth: %w[assessment:fetch] do
-      assessment_id = params[:assessment_id]
-      result =
-        @container.get_object(:fetch_assessment_use_case).execute(assessment_id)
-      json_api_response(code: 200, data: result)
-    rescue StandardError => e
-      case e
-      when UseCase::FetchAssessment::NotFoundException
-        not_found_error("Assessment not found")
-      else
-        server_error(e)
-      end
-    end
-
     post "/api/assessments", jwt_auth: %w[assessment:lodge] do
       do_lodge
     end
