communitiesuk/epb-register-api

commit 946d36fb2e88c6cfe0206802fd33fe48b44575d7

Add rake task to run all the tasks in lib/tasks folder

Rakefile
@@ -1,10 +1,9 @@
-require "sinatra/activerecord"
 require "sinatra"
+require "sinatra/activerecord"
 require "sinatra/activerecord/rake"
 Dir.glob("lib/tasks/*.rake").each { |r| load r }
 
-begin
-  require "rspec/core/rake_task"
-  RSpec::Core::RakeTask.new(:spec)
-rescue LoadError
+namespace :tasks do
+  desc "Run all tasks in lib/tasks"
+  task all: %i[import_postcode import_postcode_outcode generate_schemes generate_assessor generate_certificate]
 end

commit 39d65e4bbec12fe60e0a891ec683fecd4bc2fad1

Fix: add rspec loading back to rakefile
This resolves an issue introduced in 946d36fb2e88c6cfe0206802fd33fe48b44575d7

Rakefile
@@ -7,3 +7,9 @@ namespace :tasks do
   desc "Run all tasks in lib/tasks"
   task all: %i[import_postcode import_postcode_outcode generate_schemes generate_assessor generate_certificate]
 end
+
+begin
+  require "rspec/core/rake_task"
+  RSpec::Core::RakeTask.new(:spec)
+rescue LoadError
+end