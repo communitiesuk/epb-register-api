b8227354e6168af2e7ac7c8a408dba10f99c7a67
George Schena <george@madetech.com>
2020-06-15 12:05:14 +0100

    Allow nil to be passed if dates are empty

--- lib/domain/green_deal_plan.rb
+++ lib/domain/green_deal_plan.rb
@@ -42,8 +42,8 @@ module Domain
       {
         green_deal_plan_id: @green_deal_plan_id,
         assessment_id: @assessment_id,
-        start_date: @start_date.strftime("%Y-%m-%d"),
-        end_date: @end_date.strftime("%Y-%m-%d"),
+        start_date: @start_date.nil? ? nil : @start_date.strftime("%Y-%m-%d"),
+        end_date: @end_date.nil? ? nil : @end_date.strftime("%Y-%m-%d"),
         provider_details: {
           name: @provider_name,
           telephone: @provider_telephone,
@@ -52,7 +52,12 @@ module Domain
         interest: { rate: @interest_rate, fixed: @fixed_interest_rate },
         charge_uplift: {
           amount: @charge_uplift_amount,
-          date: @charge_uplift_date.strftime("%Y-%m-%d"),
+          date:
+            if @charge_uplift_date.nil?
+              nil
+            else
+              @charge_uplift_date.strftime("%Y-%m-%d")
+            end,
         },
         cca_regulated: @cca_regulated,
         structure_changed: @structure_changed,
