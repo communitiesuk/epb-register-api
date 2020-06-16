705babf6e84565760a73422bcc43112ea0303f0e
Lawrence <lawrence.goldstien@madetech.com>
2020-06-16 13:10:47 +0100

    Fix migration nested change function

--- db/migrate/20200616104452_add_migrate_boolean_to_assessments.rb
+++ db/migrate/20200616104452_add_migrate_boolean_to_assessments.rb
@@ -1,10 +1,8 @@
 class AddMigrateBooleanToAssessments < ActiveRecord::Migration[6.0]
   def change
-    def change
-      add_column :assessments,
-                 :migrated,
-                 :boolean,
-                 default: false
-    end
+    add_column :assessments,
+               :migrated,
+               :boolean,
+               default: false
   end
 end
