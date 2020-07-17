ef7f1e9d89ce1a542f075ad42d4088ec9a8d13d6
Yusuf <yusuf@madetech.com>
2020-07-17 12:57:26 +0100

    Refactor: Remove commented code from assessments gateway

--- lib/gateway/assessments_gateway.rb
+++ lib/gateway/assessments_gateway.rb
@@ -249,26 +249,8 @@ AND opt_out = false"
         domain.set(:heating_cost_potential, heating_cost_potential)
         domain.set(:hot_water_cost_potential, hot_water_cost_potential)
       end
+
       domain
     end
-
-    # def get_energy_rating_band(number)
-    #   case number
-    #   when 1..20
-    #     "g"
-    #   when 21..38
-    #     "f"
-    #   when 39..54
-    #     "e"
-    #   when 55..68
-    #     "d"
-    #   when 69..80
-    #     "c"
-    #   when 81..91
-    #     "b"
-    #   when 92..100
-    #     "a"
-    #   end
-    # end
   end
 end
