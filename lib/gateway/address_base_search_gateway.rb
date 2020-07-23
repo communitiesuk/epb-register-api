bab609a721074e1f20eda2536406836dab2072f9
Christine Horrocks <splogy2001@hotmail.com>
2020-07-23 12:27:02 +0100

    Inital changes for adding addressbase

---
+++ lib/gateway/address_base_search_gateway.rb
@@ -0,0 +1,30 @@
+module Gateway
+  class AddressBaseSearchGateway
+
+
+    def search_by_postcode(postcode, building_name_number, address_type)
+      postcode = postcode.delete " "
+
+      sql =
+          "SELECT
+            address_line1,
+            address_line2,
+            address_line3,
+            address_line4,
+            town,
+            postcode
+          FROM address_base
+          WHERE
+            LOWER(REPLACE(postcode, ' ', '')) = $1"
+
+      binds = [
+          ActiveRecord::Relation::QueryAttribute.new(
+              "postcode",
+              postcode.downcase,
+              ActiveRecord::Type::String.new,
+              ),
+      ]
+    end
+  end
+end
+
--- lib/use_case/search_addresses_by_postcode.rb
+++ lib/use_case/search_addresses_by_postcode.rb
@@ -2,12 +2,16 @@ module UseCase
   class SearchAddressesByPostcode
     def initialize
       @address_search_gateway = Gateway::AddressSearchGateway.new
+      @address_base_search_gateway = Gateway::AddressBaseSearchGateway.new
     end
 
     def execute(postcode:, building_name_number: nil, address_type: nil)
       @address_search_gateway.search_by_postcode postcode,
                                                  building_name_number,
                                                  address_type
+      @address_base_search_gateway.search_by_postcode postcode,
+                                                      building_name_number,
+                                                      address_type
     end
   end
 end
