099e904fcccbad24fc9ecf7c115817f3064db5eb
Lawrence <lawrence.goldstien@madetech.com>
2020-07-29 10:59:32 +0100

    Don't limit the assessments to strip params from

--- lib/domain/assessment.rb
+++ lib/domain/assessment.rb
@@ -12,15 +12,11 @@ module Domain
     }.freeze
 
     def initialize(data)
-      if %i[CEPC-RR].include? data[:type_of_assessment].to_sym
-        domain_object = TYPE2LODGEMENT[data[:type_of_assessment].to_sym]
+      domain_object = TYPE2LODGEMENT[data[:type_of_assessment].to_sym]
 
-        needed_params = domain_object.instance_method(:initialize).parameters.map(&:second)
+      needed_params = domain_object.instance_method(:initialize).parameters.map(&:second)
 
-        @assessment = domain_object.new(**data.slice(*needed_params))
-      else
-        @assessment = TYPE2LODGEMENT[data[:type_of_assessment].to_sym].new(data)
-      end
+      @assessment = domain_object.new(**data.slice(*needed_params))
     end
 
     def to_hash
