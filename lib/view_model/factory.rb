2843d807f962c629c4355464a600c45a409b14ff
Tito Sarrionandia <r@sarrionandia.com>
2020-07-30 17:36:27 +0100

    Check report type before calling factory method

--- lib/view_model/factory.rb
+++ lib/view_model/factory.rb
@@ -2,8 +2,17 @@ module ViewModel
   class Factory
     TYPES_OF_CEPC = %w[CEPC-8.0.0].freeze
     def create(xml, schema_type)
+
       if TYPES_OF_CEPC.include? schema_type
-        ViewModel::Cepc::CepcWrapper.new(xml, schema_type)
+        xml_doc = Nokogiri.XML(xml)
+        report_type = xml_doc.at("//CEPC:Report-Type").content
+        case report_type
+        when "3"
+          ViewModel::Cepc::CepcWrapper.new(xml, schema_type)
+        else
+          raise ArgumentError.new("Invalid CEPC report type")
+        end
+
       end
     end
   end
