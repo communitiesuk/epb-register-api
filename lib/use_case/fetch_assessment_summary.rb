eb11bad6f57075e5891dd582d46084d8cdbf6c54
Tito Sarrionandia <r@sarrionandia.com>
2020-07-31 08:21:16 +0100

    Refactor: Rename supplementary method

--- lib/use_case/fetch_assessment_summary.rb
+++ lib/use_case/fetch_assessment_summary.rb
@@ -2,7 +2,7 @@ module UseCase
   class FetchAssessmentSummary
     class NotFoundException < StandardError; end
 
-    def other_values_for_cepc(cepc_hash)
+    def add_cepc_supplementary_values!(cepc_hash)
       assessor_id = cepc_hash[:assessor][:scheme_assessor_id]
       assessor = Gateway::AssessorsGateway.new.fetch(assessor_id)
 
@@ -22,21 +22,24 @@ module UseCase
     end
 
     def execute(assessment_id)
-      lodged_xml_document = Gateway::AssessmentsXmlGateway.new.fetch assessment_id
+      lodged_xml_document =
+        Gateway::AssessmentsXmlGateway.new.fetch assessment_id
       raise NotFoundException unless lodged_xml_document
 
       # TODO: Check if there are multiple reports in lodged XML and only pass the desired one to the factory
 
-      lodged_values = build_view_model_from_xml_attributes(
+      lodged_values =
+        build_view_model_from_xml_attributes(
           lodged_xml_document[:xml],
-          lodged_xml_document[:schema_type])
+          lodged_xml_document[:schema_type],
+        )
 
-      case lodged_values.type
-      when :CEPC
-        full_summary = other_values_for_cepc(lodged_values.to_hash)
-      else
-        full_summary = lodged_values.to_hash
-      end
+      full_summary = case lodged_values.type
+                     when :CEPC
+                       add_cepc_supplementary_values!(lodged_values.to_hash)
+                     else
+                       lodged_values.to_hash
+                     end
 
       full_summary
     end
