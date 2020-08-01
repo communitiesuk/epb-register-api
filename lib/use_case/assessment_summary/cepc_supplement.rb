523d5cb2e07e57430d2e8ffa3c7c80ae5ea46c1a
Tito Sarrionandia <tito@madetech.com>
2020-08-01 21:42:18 +0100

    Refactor: Extract common methods to superclass

--- lib/use_case/assessment_summary/cepc_supplement.rb
+++ lib/use_case/assessment_summary/cepc_supplement.rb
@@ -1,22 +1,13 @@
 module UseCase
   module AssessmentSummary
     class CepcSupplement < UseCase::AssessmentSummary::Supplement
-
       def add_data!(hash)
-        assessor_id = hash[:assessor][:scheme_assessor_id]
-        assessor = Gateway::AssessorsGateway.new.fetch(assessor_id)
-
-        hash[:assessor][:registered_by] = {
-            name: assessor.registered_by_name,
-            scheme_id: assessor.registered_by_id,
-        }
+        registered_by!(hash)
 
         related_assessments =
-            Gateway::RelatedAssessmentsGateway.new.by_address_id hash[
-                                                                     :address
-                                                                 ][
-                                                                     :address_id
-                                                                 ]
+          Gateway::RelatedAssessmentsGateway.new.by_address_id hash[:address][
+                                                                 :address_id
+                                                               ]
 
         hash[:related_assessments] = related_assessments
 
--- lib/use_case/assessment_summary/supplement.rb
+++ lib/use_case/assessment_summary/supplement.rb
@@ -1,6 +1,15 @@
 module UseCase
   module AssessmentSummary
     class Supplement
+      def registered_by!(hash)
+        assessor_id = hash[:assessor][:scheme_assessor_id]
+        assessor = Gateway::AssessorsGateway.new.fetch(assessor_id)
+
+        hash[:assessor][:registered_by] = {
+          name: assessor.registered_by_name,
+          scheme_id: assessor.registered_by_id,
+        }
+      end
     end
   end
 end
