6f1a5cffd4fa0904b1e73b7a709a843db53a5615
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-08-04 14:13:14 +0200

    Check if date is not in future for lodgement rules, instead of before today

--- lib/lodgement_rules/non_domestic.rb
+++ lib/lodgement_rules/non_domestic.rb
@@ -6,9 +6,9 @@ module LodgementRules
         message:
           '"Inspection-Date", "Registration-Date" and "Issue-Date" must not be in the future and must not be more than 4 years ago',
         test: lambda do |adaptor|
-          Date.parse(adaptor.date_of_assessment).before?(Date.today) &&
-            Date.parse(adaptor.date_of_registration).before?(Date.today) &&
-            Date.parse(adaptor.date_of_issue).before?(Date.today) &&
+          !Date.parse(adaptor.date_of_assessment).future? &&
+            !Date.parse(adaptor.date_of_registration).future? &&
+            !Date.parse(adaptor.date_of_issue).future? &&
             Date.parse(adaptor.date_of_assessment).after?(Date.today - 365.25 * 4)
         end,
       },
