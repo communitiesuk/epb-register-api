890db3e0ccc281222f10bc81df433a55c4f0bdf5
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-08-04 14:39:01 +0200

    Lodgement rule rejects inspection date more than 4 years old

--- lib/lodgement_rules/non_domestic.rb
+++ lib/lodgement_rules/non_domestic.rb
@@ -6,10 +6,19 @@ module LodgementRules
         message:
           '"Inspection-Date", "Registration-Date" and "Issue-Date" must not be in the future and must not be more than 4 years ago',
         test: lambda do |adaptor|
-          !Date.parse(adaptor.date_of_assessment).future? &&
-            !Date.parse(adaptor.date_of_registration).future? &&
-            !Date.parse(adaptor.date_of_issue).future? &&
-            Date.parse(adaptor.date_of_assessment).after?(Date.today - 365.25 * 4)
+          dates = [
+            adaptor.date_of_assessment,
+            adaptor.date_of_registration,
+            adaptor.date_of_issue,
+          ]
+
+          failed_rules =
+            dates.select do |date|
+              Date.parse(date).future? ||
+                Date.parse(date).before?(Date.today - (365.25 * 4))
+            end
+
+          failed_rules.empty?
         end,
       },
     ].freeze
