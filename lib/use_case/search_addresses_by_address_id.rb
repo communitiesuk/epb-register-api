e78447cfbaed19d1cf09e6f85e3abff012124b9f
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-08-17 18:34:46 +0200

    Fix weird return issue for search address by addressId
    Co-authored-by: Jaseera <jaseera@madetech.com>

--- lib/use_case/search_addresses_by_address_id.rb
+++ lib/use_case/search_addresses_by_address_id.rb
@@ -4,9 +4,7 @@ module UseCase
       if address_id.start_with?("RRN")
         rrn = address_id[4..-1]
         Gateway::AddressSearchGateway.new.search_by_rrn rrn
-      end
-
-      if address_id.start_with?("UPRN")
+      elsif address_id.start_with?("UPRN")
         uprn = address_id[5..-1]
         Gateway::AddressBaseSearchGateway.new.search_by_uprn uprn
       end
