3931a2f8a3290387f0171469414dce83575734a5
Tito Sarrionandia <r@sarrionandia.com>
2020-08-19 13:00:47 +0100

    Stop RHI from using total floor area on assessments table

--- lib/gateway/renewable_heat_incentive_gateway.rb
+++ lib/gateway/renewable_heat_incentive_gateway.rb
@@ -13,7 +13,7 @@ module Gateway
       sql = <<-SQL
         SELECT
           assessments.assessment_id, scheme_assessor_id, date_of_assessment,
-          date_registered, type_of_assessment, total_floor_area,
+          date_registered, type_of_assessment,
           current_energy_efficiency_rating, potential_energy_efficiency_rating,
           postcode, current_space_heating_demand, current_water_heating_demand,
           impact_of_loft_insulation, tenure, property_age_band, cancelled_at,
@@ -60,7 +60,7 @@ module Gateway
         postcode: row["postcode"],
         property_age_band: row["property_age_band"],
         tenure: TENURE[row["tenure"]],
-        total_floor_area: row["total_floor_area"],
+        total_floor_area: assessment_summary[:total_floor_area],
         cavity_wall_insulation: insulation?("B", row),
         loft_insulation: insulation?("A", row),
         space_heating: row["current_space_heating_demand"],
