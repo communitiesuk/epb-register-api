7bc0eb86c1d213b3ac4ebaff98dd84ee3478cf4b
Tito Sarrionandia <r@sarrionandia.com>
2020-08-19 14:09:08 +0100

    Refactor: remove unused code

--- lib/gateway/assessments_gateway.rb
+++ lib/gateway/assessments_gateway.rb
@@ -32,65 +32,6 @@ module Gateway
       send_to_db assessment
     end
 
-    def search_by_postcode(postcode, assessment_types = [])
-      sql = <<-SQL
-        SELECT
-            scheme_assessor_id, assessment_id, date_of_assessment, date_registered,
-            type_of_assessment, current_energy_efficiency_rating,
-            potential_energy_efficiency_rating, opt_out, postcode, date_of_expiry,
-            address_line1, address_line2, address_line3, address_line4, town,
-            current_space_heating_demand, current_water_heating_demand, impact_of_loft_insulation,
-            impact_of_cavity_insulation, impact_of_solid_wall_insulation, tenure, property_age_band,
-            current_carbon_emission, potential_carbon_emission, property_summary, cancelled_at, not_for_issue_at,
-            address_id
-        FROM assessments
-        WHERE postcode = $1
-        AND cancelled_at IS NULL
-        AND not_for_issue_at IS NULL
-        AND opt_out = false
-      SQL
-
-      binds = [
-        ActiveRecord::Relation::QueryAttribute.new(
-          "postcode",
-          postcode,
-          ActiveRecord::Type::String.new,
-        ),
-      ]
-
-      unless assessment_types.nil? || assessment_types.empty?
-        sanitized_assessment_types =
-          assessment_types.map do |assessment_type|
-            unless %w[
-              RdSAP
-              SAP
-              CEPC
-              CEPC-RR
-              DEC
-              DEC-RR
-              AC-CERT
-              AC-REPORT
-            ].include? assessment_type
-              raise InvalidAssessmentType
-            end
-
-            ActiveRecord::Base.sanitize_sql(assessment_type)
-          end
-
-        sql +=
-          " AND type_of_assessment IN('" +
-          sanitized_assessment_types.join("', '") + "')"
-      end
-
-      response = Assessment.connection.exec_query sql, "SQL", binds
-
-      result = []
-
-      response.each { |row| result << row_to_domain(row) }
-
-      result
-    end
-
     def search_by_assessment_id(
       assessment_id, restrictive = true, assessment_type = []
     )
