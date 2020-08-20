8f8f7436733bdb3e479d0549282efac381b81e77
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-08-20 15:06:26 +0200

    Cast date param to always be a string for the assessor status report

--- lib/controller/assessor_status_controller.rb
+++ lib/controller/assessor_status_controller.rb
@@ -3,7 +3,7 @@ module Controller
     get "/api/reports/assessors/status", jwt_auth: %w[report:assessor:status] do
       events =
         UseCase::GetAssessorsStatusEventsByDate.new.execute(
-          Date.parse(params["date"]),
+          Date.parse(params["date"].nil? ? "" : params["date"]),
         )
 
       json_api_response(code: 200, data: { assessorStatusEvents: events })
