b2e34ddafc09b0d6e0382278c67bda38d1a16c8d
Tito Sarrionandia <r@sarrionandia.com>
2020-08-26 19:20:14 +0100

    Switch lodging to use generic assessmenrt index object

--- lib/domain/assessment.rb
+++ lib/domain/assessment.rb
@@ -1,18 +1,7 @@
 module Domain
   class Assessment
-    TYPE2LODGEMENT = {
-      "CEPC": Domain::CepcAssessment,
-      "SAP": Domain::SapAssessment,
-      "RdSAP": Domain::RdsapAssessment,
-      "DEC-RR": Domain::DecRrAssessment,
-      "DEC": Domain::DecAssessment,
-      "CEPC-RR": Domain::CepcRrAssessment,
-      "AC-CERT": Domain::AcCertAssessment,
-      "AC-REPORT": Domain::AcReportAssessment,
-    }.freeze
-
     def initialize(data)
-      domain_object = TYPE2LODGEMENT[data[:type_of_assessment].to_sym]
+      domain_object = Domain::AssessmentIndexRecord
 
       needed_params =
         domain_object.instance_method(:initialize).parameters.map(&:second)
