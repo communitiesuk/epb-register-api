7510bbe47cf3c4c072142c6c1ef6c2f2597a010d
Tito Sarrionandia <r@sarrionandia.com>
2020-08-28 11:20:20 +0100

    Add test for RDSAP scheme details supplement

---
+++ spec/acceptance/assessment/summary/supplement/rdsap_supplement_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "date"
+
+describe "Acceptance::AssessmentSummary::Supplement::RdSAP" do
+  include RSpecRegisterApiServiceMixin
+
+  context "when getting the summary" do
+    let(:rdsap) { Samples.xml "RdSAP-Schema-20.0.0" }
+    let(:scheme_id) { add_scheme_and_get_id }
+    let(:summary) do
+      assessor = AssessorStub.new.fetch_request_body(domesticRdSap: "ACTIVE")
+      add_assessor(scheme_id, "SPEC000000", assessor)
+
+      lodge_assessment(
+        assessment_body: rdsap,
+        auth_data: { scheme_ids: [scheme_id] },
+        schema_name: "RdSAP-Schema-20.0.0",
+      )
+
+      JSON.parse(
+        fetch_assessment_summary("0000-0000-0000-0000-0000").body,
+        symbolize_names: true,
+      )
+    end
+
+    context "and getting the assessor data" do
+      it "Adds scheme details" do
+        scheme = summary.dig(:data, :assessor, :registeredBy)
+        expect(scheme).to eq({ name: "test scheme", schemeId: scheme_id })
+      end
+    end
+  end
+end
