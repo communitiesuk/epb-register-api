b1691185f786776048561ca9b6d00ccd6e8ba35f
Tito Sarrionandia <r@sarrionandia.com>
2020-08-28 11:35:49 +0100

    Add test for overriding assessor contact details in supplement

--- spec/acceptance/assessment/summary/supplement/rdsap_supplement_spec.rb
+++ spec/acceptance/assessment/summary/supplement/rdsap_supplement_spec.rb
@@ -36,6 +36,30 @@ describe "Acceptance::AssessmentSummary::Supplement::RdSAP" do
           { telephoneNumber: "0921-19037", email: "a@b.c" },
         )
       end
+
+      it "Returns assessor DB email and phone values when not lodged" do
+        rdsap_without_contacts = Nokogiri.XML(rdsap)
+        rdsap_without_contacts.at("E-Mail").remove
+        rdsap_without_contacts.at("Telephone").remove
+        assessor = AssessorStub.new.fetch_request_body(domesticRdSap: "ACTIVE")
+        add_assessor(scheme_id, "SPEC000000", assessor)
+
+        lodge_assessment(
+          assessment_body: rdsap_without_contacts.to_xml,
+          auth_data: { scheme_ids: [scheme_id] },
+          schema_name: "RdSAP-Schema-20.0.0",
+        )
+
+        response =
+          JSON.parse(
+            fetch_assessment_summary("0000-0000-0000-0000-0000").body,
+            symbolize_names: true,
+          )
+
+        expect(response.dig(:data, :assessor, :contactDetails)).to eq(
+          { email: "person@person.com", telephoneNumber: "010199991010101" },
+        )
+      end
     end
   end
 end
