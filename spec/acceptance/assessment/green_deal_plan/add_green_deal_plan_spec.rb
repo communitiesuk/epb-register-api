6c44bfee50de0092ea78fca77b1ebb01379e071c
Tito Sarrionandia <r@sarrionandia.com>
2020-08-29 15:23:48 +0100

    Refactor: Green deal request stub

--- spec/acceptance/assessment/green_deal_plan/add_green_deal_plan_spec.rb
+++ spec/acceptance/assessment/green_deal_plan/add_green_deal_plan_spec.rb
@@ -4,41 +4,7 @@ describe "Acceptance::Assessment::GreenDealPlan:AddGreenDealPlan" do
   include RSpecRegisterApiServiceMixin
 
   let(:valid_green_deal_plan_request_body) do
-    {
-      greenDealPlanId: "ABC123456DEF",
-      startDate: "2020-01-30",
-      endDate: "2030-02-28",
-      providerDetails: {
-        name: "The Bank", telephone: "0800 0000000", email: "lender@example.com"
-      },
-      interest: { rate: 12.3, fixed: true },
-      chargeUplift: { amount: 1.25, date: "2025-03-29" },
-      ccaRegulated: true,
-      structureChanged: false,
-      measuresRemoved: false,
-      measures: [
-        {
-          sequence: 0,
-          measureType: "Loft insulation",
-          product: "WarmHome lagging stuff (TM)",
-          repaidDate: "2025-03-29",
-        },
-      ],
-      charges: [
-        {
-          sequence: 0,
-          startDate: "2020-03-29",
-          endDate: "2030-03-29",
-          dailyCharge: 0.34,
-        },
-      ],
-      savings: [
-        { fuelCode: "39", fuelSaving: 23_253, standingChargeFraction: 0 },
-        { fuelCode: "40", fuelSaving: -6331, standingChargeFraction: -0.9 },
-        { fuelCode: "41", fuelSaving: -15_561, standingChargeFraction: 0 },
-      ],
-      estimatedSavings: 1566,
-    }
+    GreenDealPlanStub.new.request_body
   end
 
   let(:valid_rdsap_xml) { Samples.xml "RdSAP-Schema-20.0.0" }
