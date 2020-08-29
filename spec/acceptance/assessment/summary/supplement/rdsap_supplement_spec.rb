7944854036f67979162fe531a10846f9f32c45ad
Tito Sarrionandia <r@sarrionandia.com>
2020-08-29 15:49:00 +0100

    Add supplement test for RdSAP green deal plan

--- spec/acceptance/assessment/summary/supplement/rdsap_supplement_spec.rb
+++ spec/acceptance/assessment/summary/supplement/rdsap_supplement_spec.rb
@@ -10,14 +10,27 @@ describe "Acceptance::AssessmentSummary::Supplement::RdSAP" do
     assessor = AssessorStub.new.fetch_request_body(domesticRdSap: "ACTIVE")
     add_assessor(scheme_id, "SPEC000000", assessor)
 
+    lodge_rdsap(Samples.xml("RdSAP-Schema-20.0.0"), scheme_id)
+    add_green_deal_plan(
+      assessment_id: "0000-0000-0000-0000-0000",
+      body: GreenDealPlanStub.new.request_body,
+    )
+    @regular_summary =
+      JSON.parse(
+        fetch_assessment_summary("0000-0000-0000-0000-0000").body,
+        symbolize_names: true,
+      )
+
     rdsap_without_contacts = Nokogiri.XML(Samples.xml("RdSAP-Schema-20.0.0"))
     rdsap_without_contacts.at("E-Mail").remove
     rdsap_without_contacts.at("Telephone").remove
     rdsap_without_contacts.at("RRN").content = "0000-0000-0000-0000-0001"
-
-    @regular_summary =
-      lodge_rdsap(Samples.xml("RdSAP-Schema-20.0.0"), scheme_id)
-    @no_contacts_summary = lodge_rdsap(rdsap_without_contacts.to_xml, scheme_id)
+    lodge_rdsap(rdsap_without_contacts.to_xml, scheme_id)
+    @no_contacts_summary =
+      JSON.parse(
+        fetch_assessment_summary("0000-0000-0000-0000-0001").body,
+        symbolize_names: true,
+      )
   end
 
   context "when getting the assessor data supplement" do
@@ -49,14 +62,64 @@ describe "Acceptance::AssessmentSummary::Supplement::RdSAP" do
     it "Returns assessments lodged against the same address" do
       related_assessments = @no_contacts_summary.dig(:data, :relatedAssessments)
       expect(related_assessments.count).to eq(1)
-      expect(related_assessments[0][:assessmentId])
-          .to eq("0000-0000-0000-0000-0000")
+      expect(related_assessments[0][:assessmentId]).to eq(
+        "0000-0000-0000-0000-0000",
+      )
     end
   end
 
   context "when getting the green deal plan" do
     it "does not add a green deal plan when there isn't one" do
       expect(@no_contacts_summary.dig(:data, :greenDealPlan)).to be_nil
+    end
+
+    it "adds a green deal plan when there is one" do
+      green_deal_plan = @regular_summary.dig(:data, :greenDealPlan)
+      expect(green_deal_plan[:ccaRegulated]).to be_truthy
+      expect(green_deal_plan[:chargeUplift]).to eq(
+        { amount: "1.25", date: "2025-03-29" },
+      )
+      expect(green_deal_plan[:charges]).to eq(
+        [
+          {
+            dailyCharge: 0.34,
+            endDate: "2030-03-29",
+            sequence: 0,
+            startDate: "2020-03-29",
+          },
+        ],
+      )
+      expect(green_deal_plan[:endDate]).to eq("2030-02-28")
+      expect(green_deal_plan[:estimatedSavings]).to eq(1566)
+      expect(green_deal_plan[:greenDealPlanId]).to eq("ABC123456DEF")
+      expect(green_deal_plan[:interest]).to eq({ fixed: true, rate: "12.3" })
+      expect(green_deal_plan[:measures]).to eq(
+        [
+          {
+            measureType: "Loft insulation",
+            product: "WarmHome lagging stuff (TM)",
+            repaidDate: "2025-03-29",
+            sequence: 0,
+          },
+        ],
+      )
+      expect(green_deal_plan[:measuresRemoved]).to be_falsey
+      expect(green_deal_plan[:providerDetails]).to eq(
+        {
+          email: "lender@example.com",
+          name: "The Bank",
+          telephone: "0800 0000000",
+        },
+      )
+      expect(green_deal_plan[:savings]).to eq(
+        [
+          { fuelCode: "39", fuelSaving: 23_253, standingChargeFraction: 0 },
+          { fuelCode: "40", fuelSaving: -6331, standingChargeFraction: -0.9 },
+          { fuelCode: "41", fuelSaving: -15_561, standingChargeFraction: 0 },
+        ],
+      )
+      expect(green_deal_plan[:startDate]).to eq("2020-01-30")
+      expect(green_deal_plan[:structureChanged]).to be_falsey
     end
   end
 end
@@ -66,11 +129,5 @@ def lodge_rdsap(xml, scheme_id)
     assessment_body: xml,
     auth_data: { scheme_ids: [scheme_id] },
     schema_name: "RdSAP-Schema-20.0.0",
-  )
-
-  assessment_id = Nokogiri.XML(xml).at("RRN").content
-  JSON.parse(
-    fetch_assessment_summary(assessment_id).body,
-    symbolize_names: true,
   )
 end
