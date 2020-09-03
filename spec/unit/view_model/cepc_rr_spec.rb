5b1a6357fd3b487df12036616d201242802ef295
Yusuf <yusuf@madetech.com>
2020-09-03 16:10:02 +0100

    Add support for CEPC-7.0 CEPC-RR

--- spec/unit/view_model/cepc_rr_spec.rb
+++ spec/unit/view_model/cepc_rr_spec.rb
@@ -32,6 +32,22 @@ describe ViewModel::CepcRrWrapper do
           },
         },
       },
+      {
+        schema_name: "CEPC-7.0",
+        xml: Samples.xml("CEPC-7.0", "cepc-rr"),
+        unsupported_fields: [],
+        different_fields: {
+          address: {
+            address_id: "000000000000",
+            address_line1: "1 Lonely Street",
+            address_line2: nil,
+            address_line3: nil,
+            address_line4: nil,
+            town: "Post-Town0",
+            postcode: "A0 0AA",
+          },
+        },
+      },
     ].freeze
 
     # You should only need to add to this list to test new fields on all CEPC schema
