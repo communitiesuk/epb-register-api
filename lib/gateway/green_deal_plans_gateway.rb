2569df0d39f776fb1bf5fe1e68f3d6c5388de15d
Lawrence <lawrence.goldstien@madetech.com>
2020-09-04 14:00:53 +0100

    Refactor: remove left join to avoid null rows

--- lib/gateway/green_deal_plans_gateway.rb
+++ lib/gateway/green_deal_plans_gateway.rb
@@ -65,18 +65,20 @@ module Gateway
     end
 
     def fetch(assessment_id)
-      sql =
-        "SELECT b.green_deal_plan_id, b.start_date, b.end_date,
-                b.provider_name, b.provider_telephone, b.provider_email,
-                b.interest_rate, b.fixed_interest_rate, b.charge_uplift_amount,
-                b.charge_uplift_date, b.cca_regulated, b.structure_changed, b.measures_removed,
-                b.charges, b.measures, b.savings
-           FROM green_deal_assessments a LEFT JOIN green_deal_plans b
-           ON (a.green_deal_plan_id = b.green_deal_plan_id)
-           WHERE assessment_id = '#{
-          ActiveRecord::Base.sanitize_sql(assessment_id)
-        }'"
-
+      sql = <<-SQL
+        SELECT
+          b.green_deal_plan_id, b.start_date, b.end_date,
+          b.provider_name, b.provider_telephone, b.provider_email,
+          b.interest_rate, b.fixed_interest_rate, b.charge_uplift_amount,
+          b.charge_uplift_date, b.cca_regulated, b.structure_changed,
+          b.measures_removed, b.charges, b.measures, b.savings
+        FROM
+          green_deal_assessments a
+        INNER JOIN
+          green_deal_plans b ON a.green_deal_plan_id = b.green_deal_plan_id
+        WHERE
+          assessment_id = '#{ActiveRecord::Base.sanitize_sql(assessment_id)}'
+      SQL
       response = GreenDealPlan.connection.execute(sql)
       result = []
 
