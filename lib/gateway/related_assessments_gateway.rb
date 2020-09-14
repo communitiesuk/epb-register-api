54058e399dda1291fce37936d30d71157e147b42
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-09-14 17:53:28 +0200

    Simplify query for related assessments to only work on address_id

--- lib/gateway/related_assessments_gateway.rb
+++ lib/gateway/related_assessments_gateway.rb
@@ -4,41 +4,16 @@ module Gateway
 
     def by_address_id(address_id)
       sql = <<-SQL
-        SELECT all_assessments.assessment_id,
-               all_assessments.assessment_type,
-               all_assessments.date_of_expiry,
-               CASE WHEN all_assessments.cancelled_at IS NOT NULL THEN 'CANCELLED'
-                    WHEN all_assessments.not_for_issue_at IS NOT NULL THEN 'NOT_FOR_ISSUE'
-                    WHEN all_assessments.date_of_expiry < CURRENT_DATE THEN 'EXPIRED'
-                    ELSE 'ENTERED'
-                   END AS assessment_status
-        FROM (
-            SELECT a.assessment_id,
+        SELECT a.assessment_id,
                a.type_of_assessment AS assessment_type,
                a.date_of_expiry,
-               a.cancelled_at,
-               a.not_for_issue_at
-            FROM (
-                WITH RECURSIVE
-                forwards AS (
-                  SELECT a.assessment_id, a.address_id FROM assessments a WHERE a.address_id = $1
-                  UNION
-                  SELECT a_forwards.assessment_id, a_forwards.address_id FROM assessments a_forwards
-                  INNER JOIN forwards f ON REPLACE(a_forwards.address_id, 'RRN-', '') = f.assessment_id
-                ),
-                backwards AS (
-                  SELECT a.assessment_id, a.address_id FROM assessments a WHERE a.address_id = $1
-                  UNION
-                  SELECT a_backwards.assessment_id, a_backwards.address_id FROM assessments a_backwards
-                  INNER JOIN backwards b ON REPLACE(b.address_id, 'RRN-', '') = a_backwards.assessment_id
-                )
-                SELECT forwards.assessment_id FROM forwards
-                UNION
-                SELECT backwards.assessment_id FROM backwards
-            ) existing_assessments
-            INNER JOIN assessments a ON existing_assessments.assessment_id = a.assessment_id
-            WHERE existing_assessments.assessment_id != REPLACE($1, 'RRN-', '')
-        ) as all_assessments
+               CASE WHEN a.cancelled_at IS NOT NULL THEN 'CANCELLED'
+                    WHEN a.not_for_issue_at IS NOT NULL THEN 'NOT_FOR_ISSUE'
+                    WHEN a.date_of_expiry < CURRENT_DATE THEN 'EXPIRED'
+                    ELSE 'ENTERED'
+                   END AS assessment_status
+        FROM assessments a
+        WHERE address_id = $1
         ORDER BY date_of_expiry DESC, assessment_id DESC
       SQL
 
