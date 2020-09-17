11f8befea1c2edd7278c620644f804dbae8d586e
Yusuf <yusuf@madetech.com>
2020-09-17 12:13:28 +0100

    Add SAP-16.0 view model for SAP summary

--- lib/view_model/factory.rb
+++ lib/view_model/factory.rb
@@ -26,6 +26,7 @@ module ViewModel
       SAP-Schema-16.3
       SAP-Schema-16.2
       SAP-Schema-16.1
+      SAP-Schema-16.0
       SAP-Schema-NI-18.0.0
       SAP-Schema-NI-17.4
       SAP-Schema-NI-17.3
