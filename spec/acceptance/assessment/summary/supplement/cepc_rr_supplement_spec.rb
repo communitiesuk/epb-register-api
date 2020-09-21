aca5bf919e5e13fc49a9e9d2beec2593b5aa2b75
Yusuf <yusuf@madetech.com>
2020-09-21 15:11:43 +0100

    Add acceptance test when there is no address ID field for CEPC-RR XML

--- spec/acceptance/assessment/summary/supplement/cepc_rr_supplement_spec.rb
+++ spec/acceptance/assessment/summary/supplement/cepc_rr_supplement_spec.rb
@@ -24,21 +24,31 @@ describe "Acceptance::AssessmentSummary::Supplement::CEPC_RR" do
         fetch_assessment_summary("0000-0000-0000-0000-0002").body,
         symbolize_names: true,
       )
+
+    third_assessment = Nokogiri.XML(Samples.xml("CEPC-8.0.0", "cepc-rr"))
+    third_assessment.at("//CEPC:RRN").content = "0000-0000-0000-0000-0003"
+    third_assessment.at("//CEPC:UPRN").remove
+    lodge_cepc_rr(third_assessment.to_xml, scheme_id)
+    @third_summary =
+      JSON.parse(
+        fetch_assessment_summary("0000-0000-0000-0000-0003").body,
+        symbolize_names: true,
+      )
   end
 
   context "when getting the assessor data supplement" do
-    it "Adds scheme details" do
+    it "adds scheme details" do
       scheme = @regular_summary.dig(:data, :assessor, :registeredBy)
       expect(scheme[:name]).to eq("test scheme")
       expect(scheme[:schemeId]).to be_a(Integer)
     end
 
-    it "Returns lodged email and phone values by default" do
+    it "returns lodged email and phone values by default" do
       contact_details = @regular_summary.dig(:data, :assessor, :contactDetails)
       expect(contact_details).to eq({ telephone: "012345", email: "a@b.c" })
     end
 
-    it "Overrides missing assessor email and phone values with DB values" do
+    it "overrides missing assessor email and phone values with DB values" do
       expect(@second_summary.dig(:data, :assessor, :contactDetails)).to eq(
         { email: "person@person.com", telephone: "010199991010101" },
       )
@@ -46,19 +56,25 @@ describe "Acceptance::AssessmentSummary::Supplement::CEPC_RR" do
   end
 
   context "when getting the related reports" do
-    it "Returns an empty list when there are no related reports" do
+    it "returns an empty list when there are no related reports" do
       expect(@regular_summary.dig(:data, :relatedAssessments)).to eq([])
+    end
+
+    context "when there is no UPRN field" do
+      it "returns an empty list when there are no related assessments" do
+        expect(@third_summary.dig(:data, :relatedAssessments)).to eq([])
+      end
     end
   end
 
   context "when getting the related certificate energy band" do
-    it "Returns empty when there is no dual lodgement" do
+    it "returns empty when there is no dual lodgement" do
       expect(
         @second_summary.dig(:data, :energyBandFromRelatedCertificate),
       ).to be_nil
     end
 
-    it "Returns the energy band from the dual lodged certificate" do
+    it "returns the energy band from the dual lodged certificate" do
       expect(
         @regular_summary.dig(:data, :energyBandFromRelatedCertificate),
       ).to eq("d")
