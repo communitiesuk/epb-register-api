cf0e881461f09a4f20256c8623c38b63044b5836
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-10-01 12:54:22 +0200

    Remove safety-check as old AddressId must always have LPRN- prefix

--- lib/gateway/related_assessments_gateway.rb
+++ lib/gateway/related_assessments_gateway.rb
@@ -5,66 +5,32 @@ module Gateway
     def by_address_id(address_id)
       return [] if address_id.blank?
 
-      if address_id.include?("UPRN-") || address_id.include?("RRN-")
-        sql = <<-SQL
-          SELECT a.assessment_id,
-                 a.type_of_assessment AS assessment_type,
-                 a.date_of_expiry,
-                 a.date_registered,
-                 a.opt_out
-          FROM assessments a
-          WHERE address_id = $1 AND
-                opt_out = false AND
-                not_for_issue_at IS NULL AND
-                cancelled_at IS NULL
-          ORDER BY date_of_expiry DESC, assessment_id DESC
-        SQL
+      sql = <<-SQL
+        SELECT a.assessment_id,
+               a.type_of_assessment AS assessment_type,
+               a.date_of_expiry,
+               a.date_registered,
+               a.opt_out
+        FROM assessments a
+        WHERE address_id = $1 AND
+              opt_out = false AND
+              not_for_issue_at IS NULL AND
+              cancelled_at IS NULL
+        ORDER BY date_of_expiry DESC, assessment_id DESC
+      SQL
 
-        results =
-          ActiveRecord::Base.connection.exec_query(
-            sql,
-            "SQL",
-            [
-              ActiveRecord::Relation::QueryAttribute.new(
-                "address_id",
-                address_id,
-                ActiveRecord::Type::String.new,
-              ),
-            ],
-          )
-      else
-        sql = <<-SQL
-          SELECT a.assessment_id,
-                 a.type_of_assessment AS assessment_type,
-                 a.date_of_expiry,
-                 a.date_registered,
-                 a.opt_out
-          FROM assessments a
-          WHERE (address_id = $1 OR address_id = $2) AND
-                opt_out = false AND
-                not_for_issue_at IS NULL AND
-                cancelled_at IS NULL
-          ORDER BY date_of_expiry DESC, assessment_id DESC
-        SQL
-
-        results =
-          ActiveRecord::Base.connection.exec_query(
-            sql,
-            "SQL",
-            [
-              ActiveRecord::Relation::QueryAttribute.new(
-                "address_id_prefix",
-                "LPRN-" + address_id.gsub("LPRN-", ""),
-                ActiveRecord::Type::String.new,
-              ),
-              ActiveRecord::Relation::QueryAttribute.new(
-                "address_id_no_prefix",
-                address_id.gsub("LPRN-", ""),
-                ActiveRecord::Type::String.new,
-              ),
-            ],
-          )
-      end
+      results =
+        ActiveRecord::Base.connection.exec_query(
+          sql,
+          "SQL",
+          [
+            ActiveRecord::Relation::QueryAttribute.new(
+              "address_id",
+              address_id,
+              ActiveRecord::Type::String.new,
+            ),
+          ],
+        )
 
       output =
         results.map do |result|
