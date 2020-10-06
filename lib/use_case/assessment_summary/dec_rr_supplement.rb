e950a2f20dc6e8eb66720a243516901abb1861df
Stéphane Meny <smeny@users.noreply.github.com>
2020-10-06 11:46:12 +0100

    Revert "[EPBR-564] Corrects expiry date in DEC-RR when returning DEC summary"
    This reverts commit 9de56359

--- lib/use_case/assessment_summary/dec_rr_supplement.rb
+++ lib/use_case/assessment_summary/dec_rr_supplement.rb
@@ -1,46 +1,25 @@
 module UseCase
   module AssessmentSummary
     class DecRrSupplement < UseCase::AssessmentSummary::Supplement
-      def add_data!(hash)
-        registered_by!(hash)
-        related_assessments!(hash)
-        related_cert_energy_band!(hash)
-        fix_expiry_date!(hash)
-        hash
-      end
-
-    private
-
       def related_cert_energy_band!(hash)
-        @related_cert ||=
+        related_cert =
           UseCase::AssessmentSummary::Fetch.new.execute(hash[:related_rrn])
 
         hash[:energy_band_from_related_certificate] =
           (
-            if @related_cert
-              @related_cert[:current_assessment][:energy_efficiency_band]
+            if related_cert
+              related_cert[:current_assessment][:energy_efficiency_band]
             end
           )
-      rescue StandardError => _e
+      rescue StandardError => e
         hash[:energy_band_from_related_certificate] = nil
       end
 
-      def fix_expiry_date!(hash)
-        @related_cert ||=
-          UseCase::AssessmentSummary::Fetch.new.execute(hash[:related_rrn])
-
-        floor_area = @related_cert[:technical_information][:floor_area]
-        expiry_date = Date.parse(@related_cert[:current_assessment][:date])
-
-        expiry_date =
-          if floor_area.to_i <= 1000
-            expiry_date.next_year 10
-          else
-            expiry_date.next_year 7
-          end
-
-        hash[:date_of_expiry] = expiry_date.strftime("%F")
-      rescue StandardError => _e
+      def add_data!(hash)
+        registered_by!(hash)
+        related_assessments!(hash)
+        related_cert_energy_band!(hash)
+        hash
       end
     end
   end
