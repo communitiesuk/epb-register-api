07595f831c67b632908158a174cabab2d37bac79
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-10-12 17:53:36 +0200

    Added outcode as a fallback when postcode is missing, for reporting
    Co-authored-by: Christine <christine.horrocks@communities.gov.uk>

--- lib/gateway/reporting_gateway.rb
+++ lib/gateway/reporting_gateway.rb
@@ -1,34 +1,51 @@
 module Gateway
   class ReportingGateway
     def assessments_by_region_and_type(start_date, end_date)
-      sql = <<-SQL
+      sql = <<~SQL
         SELECT 
           COUNT(type_of_assessment) AS number_of_assessments, 
-          type_of_assessment, 
-          region 
-        FROM assessments a 
-        LEFT JOIN 
+          type_of_assessment,
+          COALESCE(b.region, c.region) AS region
+        FROM assessments a
+        LEFT JOIN
           postcode_geolocation b 
-        ON(a.postcode = b.postcode) 
+        ON(a.postcode = b.postcode)
+        LEFT JOIN
+          postcode_outcode_geolocations c
+        ON(
+          b.postcode IS NULL 
+          AND 
+          SUBSTRING(a.postcode, 0, LENGTH(a.postcode) - 3) = c.outcode
+        )
         WHERE
           date_registered BETWEEN $1 AND $2
-        GROUP BY type_of_assessment, region
+        AND
+          (
+            cancelled_at IS NULL
+            OR
+            cancelled_at NOT BETWEEN $1 AND $2
+          )
+        GROUP BY type_of_assessment, COALESCE(b.region, c.region)
       SQL
 
       binds = [
         ActiveRecord::Relation::QueryAttribute.new(
           "start_date",
-          start_date - 1,
+          start_date,
           ActiveRecord::Type::String.new,
         ),
         ActiveRecord::Relation::QueryAttribute.new(
           "end_date",
-          end_date + 1,
+          end_date,
           ActiveRecord::Type::String.new,
         ),
       ]
 
-      ActiveRecord::Base.connection.exec_query sql, "SQL", binds
+      results = ActiveRecord::Base.connection.exec_query sql, "SQL", binds
+
+      results.map do |result|
+        result
+      end || [{number_of_assessments: "", "type_of_assessment": "", region: ""}]
     end
   end
 end
