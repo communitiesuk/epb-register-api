38704f3110e814c09dd6c9d33eb4bb991edb9e06
Christine Horrocks <splogy2001@hotmail.com>
2020-12-11 12:15:03 +0000

    Add dates to ensure specific order

--- spec/unit/use_case/open_data_export_spec.rb
+++ spec/unit/use_case/open_data_export_spec.rb
@@ -7,9 +7,11 @@ describe UseCase::OpenDataExport do
 
       let(:domestic_xml) { Nokogiri.XML Samples.xml("RdSAP-Schema-20.0.0") }
       let(:domestic_assessment_id) { domestic_xml.at("RRN") }
+      let(:domestic_assessment_date) { domestic_xml.at("Registration-Date") }
 
       let(:non_domestic_xml) { Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc") }
       let(:non_domestic_assessment_id) { non_domestic_xml.at("//CEPC:RRN") }
+      let(:non_domestic_assessment_date) { non_domestic_xml.at("//CEPC:Registration-Date") }
 
       before do
         add_assessor(
@@ -27,7 +29,7 @@ describe UseCase::OpenDataExport do
                 gda: "ACTIVE",
                 ),
             )
-
+        domestic_assessment_date.children = "2017-05-04"
         lodge_assessment(
             assessment_body: domestic_xml.to_xml,
             accepted_responses: [201],
@@ -35,6 +37,7 @@ describe UseCase::OpenDataExport do
             override: true,
             )
 
+        domestic_assessment_date.children = "2018-05-04"
         domestic_assessment_id.children = "0000-0000-0000-0000-0001"
         lodge_assessment(
             assessment_body: domestic_xml.to_xml,
@@ -43,6 +46,7 @@ describe UseCase::OpenDataExport do
             override: true,
             )
 
+        non_domestic_assessment_date.children = "2019-05-04"
         non_domestic_assessment_id.children = "0000-0000-0000-0000-0002"
         lodge_assessment(
             assessment_body: non_domestic_xml.to_xml,
@@ -59,9 +63,9 @@ describe UseCase::OpenDataExport do
 
         expect(response[0]).to eq <<~CSV
             type_of_assessment,assessment_id,date_of_expiry,report_type,date_of_assessment,date_of_registration,address_id,address_line1,address_line2,address_line3,address_line4,town,postcode,scheme_assessor_id
-            RdSAP,0000-0000-0000-0000-0000,2030-05-03,,2020-05-04,2020-05-04,RRN-0000-0000-0000-0000-0000,1 Some Street,"","","",Post-Town1,A0 0AA,SPEC000000
-            RdSAP,0000-0000-0000-0000-0001,2030-05-03,,2020-05-04,2020-05-04,RRN-0000-0000-0000-0000-0001,1 Some Street,"","","",Post-Town1,A0 0AA,SPEC000000
-            CEPC,0000-0000-0000-0000-0002,2026-05-04,3,2020-05-04,2020-05-04,RRN-0000-0000-0000-0000-0002,2 Lonely Street,,,,Post-Town1,A0 0AA,SPEC000000
+            RdSAP,0000-0000-0000-0000-0000,2027-05-03,,2020-05-04,2017-05-04,RRN-0000-0000-0000-0000-0000,1 Some Street,"","","",Post-Town1,A0 0AA,SPEC000000
+            RdSAP,0000-0000-0000-0000-0001,2028-05-03,,2020-05-04,2018-05-04,RRN-0000-0000-0000-0000-0001,1 Some Street,"","","",Post-Town1,A0 0AA,SPEC000000
+            CEPC,0000-0000-0000-0000-0002,2026-05-04,3,2020-05-04,2019-05-04,RRN-0000-0000-0000-0000-0002,2 Lonely Street,,,,Post-Town1,A0 0AA,SPEC000000
           CSV
       end
     end
