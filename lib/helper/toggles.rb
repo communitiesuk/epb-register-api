7002a206b0e133fe43ba3555685e390fa0911fce
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-01-13 16:39:30 +0000

    Turn on unleash debug logging if we are not in prod

--- lib/helper/toggles.rb
+++ lib/helper/toggles.rb
@@ -7,6 +7,7 @@ module Helper
         Unleash.configure do |config|
           config.url = ENV["EPB_UNLEASH_URI"]
           config.app_name = "toggles-" + ENV["STAGE"]
+          config.log_level = Logger::DEBUG if ENV["STAGE"] != "production"
         end
 
         @unleash = Unleash::Client.new
