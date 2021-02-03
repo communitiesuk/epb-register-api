230c79f592105df37205dab64abe65c7cd27630e
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-02-03 09:49:10 +0000

    Allow for missing properties when testing view model schemas
    * Use a symbol to specify the method to call on the view model wrapper
    * Get the sample document in the method, rather than including in arguments
    * Allow nil values for unsupported_fields and different_fields

--- spec/unit/view_model/xml_view_test_helper.rb
+++ spec/unit/view_model/xml_view_test_helper.rb
@@ -1,41 +1,35 @@
-def test_xml_doc(supported_schema, asserted_keys, test_report = false)
+def test_xml_doc(supported_schema, asserted_keys, method_called = :to_hash)
   supported_schema.each do |schema|
     view_model =
-      ViewModel::Factory.new.create(schema[:xml], schema[:schema_name], nil)
+      ViewModel::Factory.new.create Samples.xml(schema[:schema], schema[:type] || "epc"),
+                                    schema[:schema],
+                                    nil
 
-    view_model = test_report ? view_model.to_report : view_model.to_hash
+    source_hash = view_model.method(method_called).call
 
     asserted_keys.each do |key, value|
-      result = view_model[key]
+      result = source_hash[key]
 
       if schema.key?(:different_buried_fields) &&
           schema[:different_buried_fields].key?(key)
         value = value.merge(schema[:different_buried_fields][key])
       end
 
-      if schema[:unsupported_fields].include? key
+      if schema[:unsupported_fields]&.include? key
         expect(result).to be_nil,
-                          "Failed on #{schema[:schema_name]}:#{key}\n" \
+                          "Failed on #{schema[:schema]}:#{schema[:type]}:#{key}\n" \
                             "Unsupported fields must return nil, got \"#{result}\""
       elsif schema[:different_fields]&.key? key
         expect(result).to eq(schema[:different_fields][key]),
-                          "Failed on #{schema[:schema_name]}:#{key}\n with different value" \
-                            "EXPECTED: \"#{schema[:different_fields][key]}\"\n" \
-                            "     GOT: \"#{result}\"\n"
+                          "Failed on #{schema[:schema]}:#{schema[:type]}:#{key}\n with different value" \
+                            "EXPECTED: \"#{schema[:different_fields][key]}\" (#{schema[:different_fields][key].class})\n" \
+                            "     GOT: \"#{result}\" (#{result.class})\n"
       else
         expect(result).to eq(value),
-                          "Failed on #{schema[:schema_name]}:#{schema[:schema_type]}:#{key}\n" \
-                            "EXPECTED: \"#{value}\"\n" \
-                            "     GOT: \"#{result}\"\n"
+                          "Failed on #{schema[:schema]}:#{schema[:type]}:#{schema[:schema_type]}:#{key}\n" \
+                            "EXPECTED: \"#{value}\" (#{value.class})\n" \
+                            "     GOT: \"#{result}\" (#{result.class})\n"
       end
     end
   end
-end
-
-def lprn_test_value
-  "LPRN-000000000001"
-end
-
-def uprn_test_value
-  lprn_test_value.gsub("LPRN", "UPRN")
 end
