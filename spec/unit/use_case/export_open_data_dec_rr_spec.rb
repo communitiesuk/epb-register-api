786cb8ac7c45537d06fafc5102c709b931cc4de6
Tony <tony@madtech.com>
2021-02-08 16:10:52 +0000

    Remove default date_from in the usecase, add tests for non domestic

--- spec/unit/use_case/export_open_data_dec_rr_spec.rb
+++ spec/unit/use_case/export_open_data_dec_rr_spec.rb
@@ -1,3 +1,4 @@
+
 describe UseCase::ExportOpenDataDecrr do
   include RSpecRegisterApiServiceMixin
   context "when creating the open data reporting release" do
@@ -6,17 +7,16 @@ describe UseCase::ExportOpenDataDecrr do
       let(:expected) { described_class.new }
       let(:date_today) { DateTime.now.strftime("%F") }
 
-      # number in test in 2 x 4 (number of recommendations in each lodgement)
-      let(:number_assessments_to_test) { 5 }
+      let(:number_of_recommendations_expected) { 5 }
       let(:dec_plus_rr_xml) { Nokogiri.XML Samples.xml("CEPC-8.0.0", "dec+rr") }
       let(:dec_plus_rr_xml_id) { dec_plus_rr_xml.at("//RRN") }
       let(:dec_plus_rr_xml_date) { dec_plus_rr_xml.at("//Registration-Date") }
-      let(:dec_minus_rr_xml) do
+      let(:rr_minus_dec_xml) do
         Nokogiri.XML Samples.xml("CEPC-8.0.0", "dec-rr")
       end
-      let(:dec_minus_rr_xml_id) { dec_minus_rr_xml.at("RRN") }
-      let(:dec_minus_rr_xml_date) { dec_minus_rr_xml.at("//Registration-Date") }
-      let(:exported_data) { described_class.new.execute }
+      let(:rr_minus_dec_xml_id) { rr_minus_dec_xml.at("RRN") }
+      let(:rr_minus_dec_xml_date) { rr_minus_dec_xml.at("//Registration-Date") }
+      let(:exported_data) { described_class.new.execute("2019-07-01") }
 
       before do
         add_assessor(
@@ -35,7 +35,7 @@ describe UseCase::ExportOpenDataDecrr do
           ),
         )
 
-        # create a lodgement for cepc whose date valid
+        # create a lodgement for dec whose date is valid
         lodge_assessment(
           assessment_body: dec_plus_rr_xml.to_xml,
           accepted_responses: [201],
@@ -46,12 +46,10 @@ describe UseCase::ExportOpenDataDecrr do
           schema_name: "CEPC-8.0.0",
         )
 
-        # create a lodgement for cepc that should not be returned
-        dec_minus_rr_xml_id.children = "0000-0000-0000-0000-0010"
-
-        # create a lodgement for cepc whose date valid
+        # create a lodgement for rr without a DEC that should not be returned
+        rr_minus_dec_xml_id.children = "0000-0000-0000-0000-0010"
         lodge_assessment(
-          assessment_body: dec_minus_rr_xml.to_xml,
+          assessment_body: rr_minus_dec_xml.to_xml,
           accepted_responses: [201],
           auth_data: {
             scheme_ids: [scheme_id],
@@ -59,15 +57,13 @@ describe UseCase::ExportOpenDataDecrr do
           override: true,
           schema_name: "CEPC-8.0.0",
         )
-
-        # @TODO: create a lodgement for DEC  whose date is not valid
       end
 
       it "returns the correct number of items (excluding the dec-rr) " do
-        expect(exported_data.length).to eq(number_assessments_to_test)
+        expect(exported_data.length).to eq(number_of_recommendations_expected)
       end
 
-      it "should export the data for short in the first 2 rows" do
+      it "exports the recommendations in the expected format" do
         expect(exported_data[0]).to eq(
           {
             payback_type: "short",
@@ -79,18 +75,13 @@ describe UseCase::ExportOpenDataDecrr do
             rrn: "0000-0000-0000-0000-0001",
           },
         )
+      end
 
-        expect(exported_data[1]).to eq(
-          {
-            payback_type: "short",
-            cO2_Impact: "LOW",
-            recommendation:
-              "Consider introducing variable speed drives (VSD) for fans, pumps and compressors.",
-            recommendation_code: "EPC-L7",
-            recommendation_item: 2,
-            rrn: "0000-0000-0000-0000-0001",
-          },
-        )
+      it "exports the data ordered by payback_type" do
+        order_of_payback_type = exported_data.map do |recommendation|
+          recommendation[:payback_type]
+        end
+        expect(order_of_payback_type).to eq ["short", "short", "medium", "long", "other"]
       end
     end
   end
