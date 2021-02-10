8cc4d2542ef81965a59082e19b7e48102cc1784b
Barry Halper <barryhalper@gmail.com>
2021-02-10 09:43:16 +0000

    fix migration for open data logs

--- db/migrate/20210209150314_remove_assessment_hash_id_from_open_data_logs.rb
+++ db/migrate/20210209150314_remove_assessment_hash_id_from_open_data_logs.rb
@@ -1,4 +1,4 @@
-class RemoveAssessmentHashIdFromOpenDataLogs1 < ActiveRecord::Migration[6.1]
+class RemoveAssessmentHashIdFromOpenDataLogs < ActiveRecord::Migration[6.1]
   def change
     remove_column :open_data_logs, :assessment_hash_id, :string
   end
