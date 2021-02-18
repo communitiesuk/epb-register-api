8a5012cae16da9fa38d7e19102e75281cf94441c
Barry Halper <barryhalper@gmail.com>
2021-02-18 11:27:41 +0000

    EPB-986 Updated ODC rake task test to acceptance Added csv fixtures to test ODC output

--- spec/acceptance/reporting/open_data_export_spec.rb
+++ spec/acceptance/reporting/open_data_export_spec.rb
@@ -1,4 +1,25 @@
-describe "Rake open_data_export" do
+def read_csv_fixture(file_name)
+  # TODO: update the path to be more dynamic
+  fixture_path = File.dirname __FILE__.gsub("acceptance/reporting", "")
+  fixture_path << "/fixtures/open_data_export/csv/"
+  csv = CSV.open("#{fixture_path}#{file_name}.csv", headers: true)
+  csv.read
+end
+
+def fixture_headers(fixture_csv)
+  fixture_csv.headers.collect { |item| item ? item.strip : item }
+end
+
+def exported_data_headers(exported_data)
+  firstline = exported_data.split("\n")[0]
+  firstline.split(",")
+end
+
+def missing_headers(fixture_header_array, exported_header_array)
+  fixture_header_array.select { |item| !exported_header_array.include?(item) }
+end
+
+describe "Acceptance::Reports::OpenDataExport" do
   include RSpecRegisterApiServiceMixin
 
   before(:all) do
@@ -12,6 +33,10 @@ describe "Rake open_data_export" do
     dec_xml = Nokogiri.XML Samples.xml("CEPC-8.0.0", "dec")
     dec_assessment_id = dec_xml.at("RRN")
     dec_assessment_date = dec_xml.at("Registration-Date")
+
+    domestic_rdsap_xml = Nokogiri.XML Samples.xml("RdSAP-Schema-20.0.0")
+    domestic_rdsap_assessment_id = domestic_rdsap_xml.at("RRN")
+    domestic_rdsap_assessment_date = domestic_rdsap_xml.at("Registration-Date")
 
     add_assessor(
       scheme_id,
@@ -41,18 +66,6 @@ describe "Rake open_data_export" do
     )
 
     non_domestic_assessment_date.children = Date.today.strftime("%F")
-    non_domestic_assessment_id.children = "0000-0000-0000-0000-0002"
-    lodge_assessment(
-      assessment_body: non_domestic_xml.to_xml,
-      accepted_responses: [201],
-      auth_data: {
-        scheme_ids: [scheme_id],
-      },
-      override: true,
-      schema_name: "CEPC-8.0.0",
-    )
-
-    non_domestic_assessment_date.children = Date.today.strftime("%F")
     non_domestic_assessment_id.children = "0000-0000-0000-0000-0001"
     lodge_assessment(
       assessment_body: non_domestic_xml.to_xml,
@@ -64,8 +77,20 @@ describe "Rake open_data_export" do
       schema_name: "CEPC-8.0.0",
     )
 
+    non_domestic_assessment_date.children = Date.today.strftime("%F")
+    non_domestic_assessment_id.children = "0000-0000-0000-0000-0002"
+    lodge_assessment(
+      assessment_body: non_domestic_xml.to_xml,
+      accepted_responses: [201],
+      auth_data: {
+        scheme_ids: [scheme_id],
+      },
+      override: true,
+      schema_name: "CEPC-8.0.0",
+    )
+
     dec_assessment_date.children = Date.today.strftime("%F")
-    dec_assessment_id.children = "0000-0000-0000-0000-0005"
+    dec_assessment_id.children = "0000-0000-0000-0000-0003"
     lodge_assessment(
       assessment_body: dec_xml.to_xml,
       accepted_responses: [201],
@@ -74,6 +99,17 @@ describe "Rake open_data_export" do
       },
       override: true,
       schema_name: "CEPC-8.0.0",
+    )
+
+    domestic_rdsap_assessment_date.children = Date.today.strftime("%F")
+    domestic_rdsap_assessment_id.children = "0000-0000-0000-0000-0004"
+    lodge_assessment(
+      assessment_body: domestic_rdsap_xml.to_xml,
+      accepted_responses: [201],
+      auth_data: {
+        scheme_ids: [scheme_id],
+      },
+      override: true,
     )
   end
 
@@ -122,22 +158,35 @@ describe "Rake open_data_export" do
     end
   end
 
-  context "When we call the invoke method with future data" do
-    before do
-      ENV["bucket_name"] = "test_bucket"
-      ENV["instance_name"] = "test_instance"
-      future_date_from = Time.now + 60 * 86_400
-      ENV["date_from"] = future_date_from.strftime("%F")
-      ENV["assessment_type"] = "CEPC"
+  context "When we call the use case to extract the commercial data" do
+    let(:days_ago) { Date.today - 2 }
+    let(:use_case) { UseCase::ExportOpenDataCommercial.new }
+    let(:csv_data) { Helper::ExportHelper.to_csv(use_case.execute(days_ago)) }
+    let(:export_data_headers_array) { exported_data_headers(csv_data) }
+    let(:fixture_csv) { read_csv_fixture("commerical") }
+
+    let(:fixture_headers_array) { fixture_headers(fixture_csv) }
+
+    let(:known_outstanding_nodes) do
+      known_outstanding_nodes = %w[RENEWABLE_SOURCES]
     end
 
-    it "returns the expected error for no data being found" do
-      expect { get_task("open_data_export").invoke }.to output(
-        /no data to export/,
-      ).to_stdout
+    it "there to be no missing headers in the exported data based on the fixture" do
+      expect(
+        missing_headers(fixture_headers_array, export_data_headers_array),
+      ).to eq(known_outstanding_nodes)
     end
   end
 
-  # TODO: stub the csv of exported data and use it as the expectation agianst the export
-  # TODO add a test that test the logging
+  context "When we call the use case to extract the domestic data" do
+    let(:days_ago) { Date.today - 2 }
+    let(:use_case) { UseCase::ExportOpenDataDomestic.new }
+    let(:csv_data) { Helper::ExportHelper.to_csv(use_case.execute(days_ago)) }
+    let(:export_data_headers_array) { exported_data_headers(csv_data) }
+    let(:fixture_csv) { read_csv_fixture("domestic") }
+
+    let(:fixture_headers_array) { fixture_headers(fixture_csv) }
+
+    let(:known_outstanding_nodes) { known_outstanding_nodes = "" }
+  end
 end
