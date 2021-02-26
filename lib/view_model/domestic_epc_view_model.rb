f86149b92c3fa5e63acd2b375359dd69aec71891
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-02-26 12:53:29 +0000

    Include "Improvement" as a possible improvement title

--- lib/view_model/domestic_epc_view_model.rb
+++ lib/view_model/domestic_epc_view_model.rb
@@ -7,12 +7,15 @@ module ViewModel
     def improvement_title(node)
       # The SAP and RdSAP XSDs say
       # Text to precede the improvement description.
-      # If 'Improvement-Heading' is not provided the 'Improvement-Summary' is used instead.
+      # If 'Improvement-Heading' is not provided the 'Improvement-Summary' is used instead
+      # If 'Improvement-Summary' is not provided the 'Improvement' is used instead
       return "" unless node
 
-      heading = xpath(%w[Improvement-Heading], node)
-      summary = xpath(%w[Improvement-Summary], node)
-      heading.nil? || heading.empty? ? summary || "" : heading
+      [
+        xpath(%w[Improvement-Heading], node),
+        xpath(%w[Improvement-Summary], node),
+        xpath(%w[Improvement], node),
+      ].compact.delete_if(&:empty?).first || ""
     end
   end
 end
