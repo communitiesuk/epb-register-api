dbd07d88f96dfda7f9c178556a6a60ab5732e30d
Tony <tony@madetech.com>
2021-03-02 08:59:39 +0000

    EPBR-631 Alphabetised CEPC view models for cepc71, cepc70 and cepc 60

--- lib/view_model/cepc70/cepc.rb
+++ lib/view_model/cepc70/cepc.rb
@@ -1,40 +1,29 @@
 module ViewModel
   module Cepc70
     class Cepc < ViewModel::Cepc70::CommonSchema
-      def main_heating_fuel
-        xpath(%w[Main-Heating-Fuel])
+
+      def ac_inspection_commissioned
+        xpath(%w[AC-Inspection-Commissioned])
       end
 
-      def building_environment
-        xpath(%w[Building-Environment])
+      def ac_kw_rating
+        xpath(%w[AC-kW-Rating])
       end
 
-      def floor_area
-        xpath(%w[Technical-Information Floor-Area])
-      end
-
-      def building_level
-        xpath(%w[Building-Level])
+      def ac_present
+        xpath(%w[AC-Present])
       end
 
       def building_emission_rate
         xpath(%w[BER])
       end
 
-      def primary_energy_use
-        nil
+      def building_environment
+        xpath(%w[Building-Environment])
       end
 
-      def related_rrn
-        xpath(%w[Related-RRN])
-      end
-
-      def new_build_rating
-        xpath(%w[New-Build-Benchmark])
-      end
-
-      def existing_build_rating
-        xpath(%w[Existing-Stock-Benchmark])
+      def building_level
+        xpath(%w[Building-Level])
       end
 
       def energy_efficiency_rating
@@ -45,40 +34,40 @@ module ViewModel
         xpath(%w[EPC-Related-Party-Disclosure])
       end
 
-      def property_type
-        xpath(%w[Property-Type])
-      end
-
-      def other_fuel_description
-        xpath(%w[Other-Fuel-Description])
-      end
-
-      def ac_present
-        xpath(%w[AC-Present])
-      end
-
-      def transaction_type
-        xpath(%w[Transaction-Type])
-      end
-
-      def target_emissions
-        xpath(%w[TER])
-      end
-
-      def typical_emissions
-        xpath(%w[TYR])
-      end
-
-      def ac_kw_rating
-        xpath(%w[AC-kW-Rating])
-      end
-
       def estimated_ac_kw_rating
         xpath(%w[AC-Estimated-Output])
       end
 
+      def existing_build_rating
+        xpath(%w[Existing-Stock-Benchmark])
+      end
+
+      def floor_area
+        xpath(%w[Technical-Information Floor-Area])
+      end
+
+      def main_heating_fuel
+        xpath(%w[Main-Heating-Fuel])
+      end
+
+      def new_build_rating
+        xpath(%w[New-Build-Benchmark])
+      end
+
       def other_fuel_description
         xpath(%w[Other-Fuel-Description])
+      end
+
+      def primary_energy_use
+        nil
+      end
+
+      def property_type
+        xpath(%w[Property-Type])
+      end
+
+      def related_rrn
+        xpath(%w[Related-RRN])
       end
 
       def special_energy_uses
@@ -89,8 +78,16 @@ module ViewModel
         xpath(%w[SER])
       end
 
-      def ac_inspection_commissioned
-        xpath(%w[AC-Inspection-Commissioned])
+      def target_emissions
+        xpath(%w[TER])
+      end
+
+      def transaction_type
+        xpath(%w[Transaction-Type])
+      end
+
+      def typical_emissions
+        xpath(%w[TYR])
       end
     end
   end
--- lib/view_model/cepc71/cepc.rb
+++ lib/view_model/cepc71/cepc.rb
@@ -1,52 +1,13 @@
 module ViewModel
   module Cepc71
     class Cepc < ViewModel::Cepc71::CommonSchema
-      def main_heating_fuel
-        xpath(%w[Main-Heating-Fuel])
+
+      def ac_inspection_commissioned
+        xpath(%w[AC-Inspection-Commissioned])
       end
 
-      def building_environment
-        xpath(%w[Building-Environment])
-      end
-
-      def floor_area
-        xpath(%w[Technical-Information Floor-Area])
-      end
-
-      def building_level
-        xpath(%w[Building-Level])
-      end
-
-      def building_emission_rate
-        xpath(%w[BER])
-      end
-
-      def primary_energy_use
-        xpath(%w[Energy-Consumption-Current])
-      end
-
-      def related_rrn
-        xpath(%w[Related-RRN])
-      end
-
-      def new_build_rating
-        xpath(%w[New-Build-Benchmark])
-      end
-
-      def existing_build_rating
-        xpath(%w[Existing-Stock-Benchmark])
-      end
-
-      def energy_efficiency_rating
-        xpath(%w[Asset-Rating])
-      end
-
-      def epc_related_party_disclosure
-        xpath(%w[EPC-Related-Party-Disclosure])
-      end
-
-      def property_type
-        xpath(%w[Property-Type])
+      def ac_kw_rating
+        xpath(%w[AC-kW-Rating])
       end
 
       def ac_present
@@ -57,32 +18,60 @@ module ViewModel
         xpath(%w[AC-Rated-Output])
       end
 
-      def transaction_type
-        xpath(%w[Transaction-Type])
+      def building_environment
+        xpath(%w[Building-Environment])
       end
 
-      def other_fuel_description
-        xpath(%w[Other-Fuel-Description])
+      def building_emission_rate
+        xpath(%w[BER])
       end
 
-      def target_emissions
-        xpath(%w[TER])
+      def building_level
+        xpath(%w[Building-Level])
       end
 
-      def typical_emissions
-        xpath(%w[TYR])
+      def energy_efficiency_rating
+        xpath(%w[Asset-Rating])
       end
 
-      def ac_kw_rating
-        xpath(%w[AC-kW-Rating])
+      def epc_related_party_disclosure
+        xpath(%w[EPC-Related-Party-Disclosure])
       end
 
       def estimated_ac_kw_rating
         xpath(%w[AC-Estimated-Output])
       end
 
+      def existing_build_rating
+        xpath(%w[Existing-Stock-Benchmark])
+      end
+
+      def floor_area
+        xpath(%w[Technical-Information Floor-Area])
+      end
+
+      def main_heating_fuel
+        xpath(%w[Main-Heating-Fuel])
+      end
+
+      def new_build_rating
+        xpath(%w[New-Build-Benchmark])
+      end
+
+      def primary_energy_use
+        xpath(%w[Energy-Consumption-Current])
+      end
+
+      def property_type
+        xpath(%w[Property-Type])
+      end
+
       def other_fuel_description
         xpath(%w[Other-Fuel-Description])
+      end
+
+      def related_rrn
+        xpath(%w[Related-RRN])
       end
 
       def special_energy_uses
@@ -93,8 +82,16 @@ module ViewModel
         xpath(%w[SER])
       end
 
-      def ac_inspection_commissioned
-        xpath(%w[AC-Inspection-Commissioned])
+      def target_emissions
+        xpath(%w[TER])
+      end
+
+      def transaction_type
+        xpath(%w[Transaction-Type])
+      end
+
+      def typical_emissions
+        xpath(%w[TYR])
       end
     end
   end
