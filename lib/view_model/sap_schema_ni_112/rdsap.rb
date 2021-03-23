192cc76605f03cc375a8bc4eabc3988fe4e44245
Caitlin <caitlin@madetech.com>
2021-03-23 16:14:18 +0000

    EPBR-1120 Add tests for SAP schemas NI versions 11.2 to 14.1

--- lib/view_model/sap_schema_ni_112/rdsap.rb
+++ lib/view_model/sap_schema_ni_112/rdsap.rb
@@ -16,6 +16,30 @@ module ViewModel
         end
         nil
       end
+
+      def window_description
+        xpath(%w[Window Description])
+      end
+
+      def window_energy_efficiency_rating
+        xpath(%w[Window Energy-Efficiency-Rating])
+      end
+
+      def window_environmental_efficiency_rating
+        xpath(%w[Window Environmental-Efficiency-Rating])
+      end
+
+      def all_wall_descriptions
+        @xml_doc.search("Wall/Description").map(&:content)
+      end
+
+      def all_wall_energy_efficiency_rating
+        @xml_doc.search("Wall/Energy-Efficiency-Rating").map(&:content)
+      end
+
+      def all_wall_env_energy_efficiency_rating
+        @xml_doc.search("Wall/Environmental-Efficiency-Rating").map(&:content)
+      end
     end
   end
 end
