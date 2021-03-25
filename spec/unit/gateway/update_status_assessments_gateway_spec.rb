e81797affab1fbda64b572de43a5a454df4698b3
Tony <tony@madetech.com>
2021-03-25 16:27:37 +0000

    Make slow-format

--- spec/unit/gateway/update_status_assessments_gateway_spec.rb
+++ spec/unit/gateway/update_status_assessments_gateway_spec.rb
@@ -36,16 +36,14 @@ describe "Gateway::AssessmentsGateway" do
     end
 
     context "calling update_statuses on both assessments" do
-      assessments = ["0000-0000-0000-0000-0000", "0000-0000-0000-0000-0001"]
+      assessments = %w[0000-0000-0000-0000-0000 0000-0000-0000-0000-0001]
       field = "cancelled_at"
 
       xit "it cancels the first assessment" do
         assessment_gateway.update_statuses(assessments, field, Time.now.to_s)
-
       end
 
       xit "it cancels the second assessment" do
-
       end
     end
   end
