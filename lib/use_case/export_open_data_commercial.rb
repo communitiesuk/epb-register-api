a608c68590113bd801b7b686f0d3180a9a21c783
Christine Horrocks <splogy2001@hotmail.com>
2021-04-07 14:56:24 +0100

    Add safe operator to DEC and Non-dom ODE rake task

--- lib/use_case/export_open_data_commercial.rb
+++ lib/use_case/export_open_data_commercial.rb
@@ -36,7 +36,7 @@ module UseCase
         view_model_hash[:lodgement_date] =
           assessment["date_registered"].strftime("%F")
         view_model_hash[:lodgement_datetime] =
-          assessment["created_at"].strftime("%F %H:%M:%S")
+          assessment["created_at"]&.strftime("%F %H:%M:%S")
         view_model_hash[:assessment_id] =
           Helper::RrnHelper.hash_rrn(assessment["assessment_id"])
         unless view_model_hash[:building_reference_number].nil?
