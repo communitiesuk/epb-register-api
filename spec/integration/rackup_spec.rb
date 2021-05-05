a4cefbc08dd7d75f585b643c8e3c25a6cd33c051
Stéphane Meny <smeny@users.noreply.github.com>
2021-05-05 16:07:47 +0100

    Use rackup testing library in place of starting rackup process

--- spec/integration/rackup_spec.rb
+++ spec/integration/rackup_spec.rb
@@ -1,59 +1,41 @@
-require "net/http"
-
 describe "Integration::Rackup" do
   include RSpecRegisterApiServiceMixin
 
-  before(:all) do
-    process = IO.popen(["rackup", "-p 9191", err: %i[child out]])
-    @process_id = process.pid
-
-    unless process.readline.include?("port=9191")
-    end
-  end
-
-  after(:all) { Process.kill("KILL", @process_id) }
-
-  let(:request) { Net::HTTP.new("127.0.0.1", 9_191) }
-
   context "when rackup has started" do
     context "requests to /healthcheck" do
+      let(:response) { get("/healthcheck") }
       it "return a status of 200" do
-        req = Net::HTTP::Get.new("/healthcheck")
-        response = request.request(req)
-        expect(response.code).to eq("200")
+        expect(response.status).to eq(200)
       end
     end
 
     context "requests to a non-existent page" do
+      let(:response) { get("/does-not-exist") }
       it "return a status of 404" do
-        req = Net::HTTP::Get.new("/does-not-exist")
-        response = request.request(req)
-        expect(response.code).to eq("404")
+        expect(response.status).to eq(404)
       end
 
+      let(:response) { get("/energy-certificate/:%0000-0000-0000-0000-0000") }
       it "return a status of Method not found" do
-        req =
-          Net::HTTP::Get.new("/energy-certificate/:%0000-0000-0000-0000-0000")
-        response = request.request(req)
         json_response = JSON.parse(response.body, symbolize_names: true)
         expect(json_response[:errors][0][:title]).to eq("Method not found")
       end
     end
 
     context "requests to /api/schemes" do
+      let(:response) do
+        header("Authorization", "Bearer " + get_valid_jwt(%w[scheme:list]))
+        get("/api/schemes")
+      end
       it "return a status of 200" do
-        req = Net::HTTP::Get.new "/api/schemes"
-        response =
-          authenticate_and(req, %w[scheme:list]) { request.request req }
-        expect(response.code).to eq "200"
+        expect(response.status).to eq(200)
       end
     end
 
     context "unauthenticated requests to /api/schemes" do
+      let(:response) { get("/api/schemes") }
       it "return a status of 401" do
-        req = Net::HTTP::Get.new "/api/schemes"
-        response = request.request req
-        expect(response.code).to eq "401"
+        expect(response.status).to eq(401)
       end
     end
   end
