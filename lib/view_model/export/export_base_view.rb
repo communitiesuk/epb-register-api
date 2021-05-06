7c81e52802d6b2c94b34a1eb85e72ab10febb742
Barry Halper <barryhalper@gmail.com>
2021-05-06 14:11:59 +0100

    EPBR-1279 Updated redacted asssessor in commerical test

--- lib/view_model/export/export_base_view.rb
+++ lib/view_model/export/export_base_view.rb
@@ -47,19 +47,6 @@ module ViewModel::Export
       }
     end
 
-    def assessor
-      {
-        scheme_assessor_id: @view_model.scheme_assessor_id,
-        name: @view_model.assessor_name,
-        contact_details: {
-          email: @view_model.assessor_email,
-          telephone: @view_model.assessor_telephone,
-        },
-        company_details: {
-          name: @view_model.company_name,
-          address: @view_model.company_address,
-        },
-      }
-    end
+
   end
 end
