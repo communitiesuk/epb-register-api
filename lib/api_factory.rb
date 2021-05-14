7fcf3cf3041f09aa7879e8a50695ba6271bd3ee4
Stéphane Meny <smeny@users.noreply.github.com>
2021-05-14 15:12:17 +0100

    EPBR-1279: Fixes argument numbers in factory

--- lib/api_factory.rb
+++ lib/api_factory.rb
@@ -1,7 +1,10 @@
 class ApiFactory
-
   def self.assessments_gateway
     @assessments_gateway ||= Gateway::AssessmentsGateway.new
+  end
+
+  def self.assessments_search_gateway
+    @assessments_search_gateway ||= Gateway::AssessmentsSearchGateway.new
   end
 
   def self.assessments_xml_gateway
@@ -9,7 +12,12 @@ class ApiFactory
   end
 
   def self.assessments_export_use_case
-    @assessments_export_use_case ||= UseCase::ExportAssessmentAttributes.new(assessments_gateway, assessments_xml_gateway)
+    @assessments_export_use_case ||=
+      UseCase::ExportAssessmentAttributes.new(
+        assessments_gateway,
+        assessments_search_gateway,
+        assessments_xml_gateway,
+      )
   end
 
   def self.storage_configuration_reader(bucket_name:, instance_name:)
@@ -21,10 +29,11 @@ class ApiFactory
 
   def self.storage_gateway(bucket_name:, instance_name:)
     Gateway::StorageGateway.new(
-      storage_config: storage_configuration_reader(
-        bucket_name: bucket_name,
-        instance_name: instance_name
-      ).get_configuration
+      storage_config:
+        storage_configuration_reader(
+          bucket_name: bucket_name,
+          instance_name: instance_name,
+        ).get_configuration,
     )
   end
 end
