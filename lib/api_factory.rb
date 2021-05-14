38bd551abc01804a183dfaf59db0e9660ea21ce5
Stéphane Meny <smeny@users.noreply.github.com>
2021-05-14 11:13:15 +0100

    EPBR-1279: Implements JSON export rake task

---
+++ lib/api_factory.rb
@@ -0,0 +1,30 @@
+class ApiFactory
+
+  def self.assessments_gateway
+    @assessments_gateway ||= Gateway::AssessmentsGateway.new
+  end
+
+  def self.assessments_xml_gateway
+    @assessments_xml_gateway ||= Gateway::AssessmentsXmlGateway.new
+  end
+
+  def self.assessments_export_use_case
+    @assessments_export_use_case ||= UseCase::ExportAssessmentAttributes.new(assessments_gateway, assessments_xml_gateway)
+  end
+
+  def self.storage_configuration_reader(bucket_name:, instance_name:)
+    Gateway::StorageConfigurationReader.new(
+      bucket_name: bucket_name,
+      instance_name: instance_name,
+    )
+  end
+
+  def self.storage_gateway(bucket_name:, instance_name:)
+    Gateway::StorageGateway.new(
+      storage_config: storage_configuration_reader(
+        bucket_name: bucket_name,
+        instance_name: instance_name
+      ).get_configuration
+    )
+  end
+end
---
+++ spec/tasks/json_export_spec.rb
@@ -0,0 +1,82 @@
+describe "JsonExport" do
+  include RSpecRegisterApiServiceMixin
+
+  subject(:task) { get_task("json_export") }
+
+  context "when the export task runs with all needed parameters" do
+    let(:storage_gateway) { instance_double(Gateway::StorageGateway) }
+    let(:export_usecase) { instance_double(UseCase::ExportAssessmentAttributes) }
+
+    let(:start_date) { "2021-05-01" }
+    let(:bucket_name) { "bucket_name" }
+    let(:instance_name) { "epb-s3-service" }
+    let(:export) {
+      [{
+         assessment_id: "001",
+         data: { "attribute": "export 001" }
+       }, {
+         assessment_id: "002",
+         data: { "attribute": "export 002" }
+       }]
+    }
+
+    before do
+      allow(ENV).to receive(:[])
+      allow(ENV).to receive(:[]).with("start_date").and_return(start_date)
+      allow(ENV).to receive(:[]).with("instance_name").and_return(instance_name)
+      allow(ENV).to receive(:[]).with("bucket_name").and_return(bucket_name)
+
+      # Prevents logging during tests
+      allow(STDOUT).to receive(:puts)
+
+      # Mocks all dependencies created directly in the task
+      allow(ApiFactory).to receive(:assessments_export_use_case).and_return(export_usecase)
+      allow(ApiFactory).to receive(:storage_gateway).and_return(storage_gateway)
+
+      # Define mock expectations
+      allow(export_usecase).to receive(:execute).and_return(export)
+      allow(storage_gateway).to receive(:write_file)
+    end
+
+    it "initialises the storage gateway with task parameters" do
+      expect(ApiFactory).to receive(:storage_gateway).with(bucket_name: bucket_name, instance_name: instance_name)
+      expect { task.invoke }.to_not raise_error
+    end
+
+    it "calls the export use_case with the start_date parameter" do
+      expect(export_usecase).to receive(:execute).with(start_date)
+      expect { task.invoke }.to_not raise_error
+    end
+
+    it "calls the storage gateway with the data received from the export use case" do
+      expect(storage_gateway).to receive(:write_file).with("001.json", "{\"attribute\":\"export 001\"}")
+      expect(storage_gateway).to receive(:write_file).with("002.json", "{\"attribute\":\"export 002\"}")
+      expect { task.invoke }.to_not raise_error
+    end
+  end
+
+  context "when start_date is not provided to the export task" do
+    before do
+      allow(ENV).to receive(:[])
+      allow(ENV).to receive(:[]).with("instance_name").and_return("epb-s3-bucket")
+    end
+
+    it "fails to run with the relevant message" do
+      expected_message = "A required argument is missing: start_date"
+      expect { task.invoke }.to raise_error(Boundary::ArgumentMissing).with_message(expected_message)
+    end
+  end
+
+  context "when bucket_name or instance_name is not provided to the export task" do
+    before do
+      allow(ENV).to receive(:[])
+      allow(ENV).to receive(:[]).with("start_date").and_return("2021-05-01")
+    end
+
+    it "fails to run with the relevant message" do
+      expected_message = "A required argument is missing: bucket_name or instance_name"
+      expect { task.invoke }.to raise_error(Boundary::ArgumentMissing).with_message(expected_message)
+    end
+  end
+
+end
