cf73293557a20be521a7159f7cf53b696a9f8eae
Bella <bella.cockrell@madetech.com>
2021-05-20 12:41:57 +0100

    Removing left-over variable from test, as not used anywhere

--- spec/unit/use_case/export_open_data_opt_outs_spec.rb
+++ spec/unit/use_case/export_open_data_opt_outs_spec.rb
@@ -30,27 +30,6 @@ describe UseCase::ExportOpenDataOptOuts do
       ]
     end
 
-    let(:hashed_assessments) do
-      [
-        {
-          assessment_id:
-            "4af9d2c31cf53e72ef6f59d3f59a1bfc500ebc2b1027bc5ca47361435d988e1a",
-          type_of_assessment: "RdSAP",
-          address_line1: "1 Some Street",
-          address_line2: "",
-          address_line3: "",
-        },
-        {
-          assessment_id:
-            "55ce7d026c13e923d26cbfb0d6ed60734d3270ba981d629a168bb8eb2da3f8c4",
-          type_of_assessment: "RdSAP",
-          address_line1: "2 Some Street",
-          address_line2: "",
-          address_line3: "",
-        },
-      ]
-    end
-
     before do
       allow(reporting_gateway).to receive(:fetch_opted_out_assessments)
         .and_return(fetch_ids_response)
