306df58f95c84b8a05aa7d63c98cf479cbb9c019
Bella <bella.cockrell@madetech.com>
2021-05-20 12:10:28 +0100

    Adding test to ensure AC-CERTs are not returned when fetch_opted_out_assessments is called

--- spec/unit/gateway/reporting_gateway_spec.rb
+++ spec/unit/gateway/reporting_gateway_spec.rb
@@ -1,16 +1,18 @@
 describe "Gateway::ReportingGateway" do
   include RSpecRegisterApiServiceMixin
-  context "test data extacted form the reporting gateway" do
+  context "test data extracted from the reporting gateway" do
     subject { Gateway::ReportingGateway.new }
+    before(:all) do
+      @scheme_id = add_scheme_and_get_id
+      add_super_assessor(@scheme_id)
+    end
     context "Insert two assessments and opt out one of them" do
-      before(:all) do
-        scheme_id = add_scheme_and_get_id
-        add_super_assessor(scheme_id)
+      before do
         schema = "RdSAP-Schema-20.0.0"
         xml = Nokogiri.XML Samples.xml(schema)
-        call_lodge_assessment(scheme_id, schema, xml)
+        call_lodge_assessment(@scheme_id, schema, xml)
         xml.at("RRN").children = "0000-0000-0000-0000-0001"
-        call_lodge_assessment(scheme_id, schema, xml)
+        call_lodge_assessment(@scheme_id, schema, xml)
         opt_out_assessment("0000-0000-0000-0000-0001")
       end
 
@@ -31,6 +33,24 @@ describe "Gateway::ReportingGateway" do
       it "returns the opted out assessments only" do
         expect(subject.fetch_opted_out_assessments.count).to eq(1)
         expect(subject.fetch_opted_out_assessments[0]).to eq(expected_data)
+      end
+    end
+    context "Insert RdSAP, AC-CERT and opt out the RdSAP" do
+      before do
+        commercial_schema = "CEPC-8.0.0"
+        ac_cert_xml = Nokogiri.XML Samples.xml(commercial_schema, "ac-cert")
+        call_lodge_assessment(@scheme_id, commercial_schema, ac_cert_xml)
+        opt_out_assessment("0000-0000-0000-0000-0000")
+
+        rdsap_schema = "RdSAP-Schema-20.0.0"
+        rdsap_xml = Nokogiri.XML Samples.xml(rdsap_schema)
+        rdsap_xml.at("RRN").children = "0000-0000-0000-0000-0002"
+        call_lodge_assessment(@scheme_id, rdsap_schema, rdsap_xml)
+        opt_out_assessment("0000-0000-0000-0000-0002")
+      end
+
+      it "only returns the opted out RdSAP" do
+        expect(subject.fetch_opted_out_assessments.count).to eq(1)
       end
     end
   end
