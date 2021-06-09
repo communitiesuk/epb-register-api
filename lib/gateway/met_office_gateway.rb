51bbf691e7b15a0982512aa955a561ac61cf57ca
Barry Halper <barryhalper@gmail.com>
2021-06-09 13:55:15 +0100

    EPBR-988 Added CIP file use csae and  met office gateway

---
+++ lib/gateway/met_office_gateway.rb
@@ -0,0 +1,9 @@
+
+module Gateway
+  class MetOfficeGateway
+
+    def read_degrees_day_data
+      []
+    end
+  end
+end
---
+++ spec/unit/use_case/create_cip_file_spec.rb
@@ -0,0 +1,10 @@
+describe "UseCase::CreateCipFile" do
+
+  context "read degrees day data stored by the Met office " do
+    subject { UseCase::CreateCipFile new }
+
+    it 'read the object' do
+      expect{subject.execute}.not_to raise_error
+    end
+  end
+end
