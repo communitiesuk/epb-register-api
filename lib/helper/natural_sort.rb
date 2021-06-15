72485016d0e674f89decf05ca07f854d6febeb8a
Stéphane Meny <smeny@users.noreply.github.com>
2021-06-15 11:34:58 +0100

    Revert "Merge updated Natural Sort algorithm 'EPBR-1306'"
    This reverts commit 55d106af5503d52d8355bc62c2e61564dacc8872, reversing
    changes made to d23793d7751a42311c78c682fa5892483ab0bb0c.

--- lib/helper/natural_sort.rb
+++ lib/helper/natural_sort.rb
@@ -7,118 +7,50 @@ module Helper
 
         address_a =
           [
-            a[:postcode],
-            a[:address_line4],
-            a[:address_line3],
-            a[:address_line2],
             a[:address_line1],
-          ].map { |item| item.to_s.strip.upcase.tr(",","") }
+            a[:address_line2],
+            a[:address_line3],
+            a[:address_line4],
+            b[:postcode],
+            a[:town],
+          ].reverse
+            .compact
+            .join(" ")
+            .gsub(",", "")
+            .gsub("  ", " ")
+            .upcase
+            .split(" ")
 
         address_b =
           [
-            b[:postcode],
-            b[:address_line4],
-            b[:address_line3],
-            b[:address_line2],
             b[:address_line1],
-          ].map { |item| item.to_s.strip.upcase.tr(",","") }
+            b[:address_line2],
+            b[:address_line3],
+            b[:address_line4],
+            b[:postcode],
+            b[:town],
+          ].reverse
+            .compact
+            .join(" ")
+            .gsub(",", "")
+            .gsub("  ", " ")
+            .upcase
+            .split(" ")
 
-        postcode_comparison = compare_postcode(address_a, address_b)
-        if postcode_comparison == 0
-          address_line_comparison = compare_address_line_for_number(address_a, address_b)
-          address_line_comparison == 0 ? compare_flat_numbers(address_a,address_b) : address_line_comparison
+        res = 0
 
-          if address_line_comparison == 0
-            flat_number_comparison = compare_flat_numbers(address_a,address_b)
-            flat_number_comparison == 0 ? compare_addresses_alphabetically(address_a, address_b) : flat_number_comparison
-          else
-            address_line_comparison
-          end
-        else
-          postcode_comparison
-        end
-      end
-    end
-
-    def self.compare_postcode(a, b)
-      compare_to(a[0],b[0])
-    end
-
-    def self.compare_address_line_for_number(a,b)
-      address_lines_a = [a[1], a[2], a[3], a[4]]
-      address_lines_b = [b[1], b[2], b[3], b[4]]
-
-      property_a_number, property_a_letter = get_property_number_and_letter(address_lines_a)
-      property_b_number, property_b_letter = get_property_number_and_letter(address_lines_b)
-
-      compared = compare_to(property_a_number, property_b_number)
-      compared == 0 ? compare_to(property_a_letter, property_b_letter) : compared
-    end
-
-    def self.get_property_number_and_letter(address_block)
-      property_number = 0
-      property_letter = ""
-      address_block.each do |line|
-        if line.to_i != 0
-          property_number = line.to_i
-          line_split = line.split(" ")
-
-          if property_number.to_s != line_split.first
-            remove_hyphen = line_split.first.tr("-"," ")
-            address_line_digits = remove_hyphen.split(" ")[1]&.scan(/\d+/)
-
-            if address_line_digits != [] && !address_line_digits.nil?
-              property_letter = remove_hyphen.split(" ")[1].to_s
-            else
-              property_letter = line_split.first[-1]
-           end
-          end
-        end
-      end
-      return property_number, property_letter
-    end
-
-    def self.compare_flat_numbers(a,b)
-      flat_number_a = get_flat_number(a)
-      flat_number_b = get_flat_number(b)
-
-      compare_to(flat_number_a,flat_number_b)
-    end
-
-    def self.get_flat_number(a)
-      numbers_in_address = a[1..4].reverse.join(" ").scan(/\d+/)
-      if numbers_in_address != [] && numbers_in_address.count > 1
-        numbers_in_address.first.to_i
-      else
-        0
-      end
-    end
-
-    def self.compare_addresses_alphabetically(a, b)
-      address_a = a[1..4].reverse.join(" ")
-      address_b = b[1..4].reverse.join(" ")
-
-      numbers_in_address_a = address_a.scan(/\d+/)
-      numbers_in_address_b = address_b.scan(/\d+/)
-
-      if numbers_in_address_a.count < 2 && numbers_in_address_b.count < 2
-        comparison_result = 0
-        address_a.split("").each_with_index do |char, index|
-          if char == address_b[index]
-            next
-          else
-            comparison_result = compare_to(char, address_b[index])
+        address_a.each_with_index do |line, index|
+          compare_to = address_b[index].nil? ? "" : address_b[index]
+          if line.to_i != compare_to.to_i
+            res = line.to_i < compare_to.to_i ? -1 : 1
             break
+          elsif line != compare_to
+            res = line <=> compare_to
           end
         end
-        comparison_result
-      else
-        0
-      end
-    end
 
-    def self.compare_to(a,b)
-      a <=> b
+        res
+      end
     end
   end
 end
