cb4beda13cae11bfbb1ce5e6970042a106559720
Stéphane Meny <smeny@users.noreply.github.com>
2021-06-24 10:48:12 +0100

    Increases memory allocation to 2G for production

--- scripts/generate-paas-manifest.sh
+++ scripts/generate-paas-manifest.sh
@@ -4,12 +4,17 @@
 APPLICATION_NAME=$1  # e.g. mhclg-epb-something-api-integration
 STAGE=$2 # i.e. [integration, staging, production]
 
+case "$STAGE" in
+ production) MEMORY="2G" ;;
+ *) MEMORY="1G" ;;
+esac
+
 cat << EOF
 ---
 applications:
   - name: $APPLICATION_NAME
     command: null
-    memory: 1G
+    memory: $MEMORY
     buildpacks:
       - ruby_buildpack
     health-check-type: http
