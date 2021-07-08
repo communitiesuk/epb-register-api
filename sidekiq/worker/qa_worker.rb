afeaa5f3df094a9aba588571c9078b0173bca8c8
Barry Halper <barryhalper@gmail.com>
2021-07-08 12:01:56 +0100

    EPBR-1309 Added temporary worker class to sidekiq to test rake/db access

---
+++ sidekiq/worker/qa_worker.rb
@@ -0,0 +1,21 @@
+module Worker
+  class QaWorker
+    include Sidekiq::Worker
+
+    def perform
+      get_task("qa_rake").invoke
+    end
+
+    def get_task(name)
+      rake = Rake::Application.new
+      Rake.application = rake
+      rake.load_rakefile
+      rake.tasks.find { |task| task.to_s == name }
+    end
+
+  end
+end
+
+
+
+
