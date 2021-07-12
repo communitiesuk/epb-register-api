69a5c7459efddd2b16719506c1dde008cd1a4209
Caitlin <caitlin@madetech.com>
2021-07-12 11:54:45 +0100

    Add total_floor_area for NI schemes

--- lib/view_model/sap_schema_ni_120/common_schema.rb
+++ lib/view_model/sap_schema_ni_120/common_schema.rb
@@ -169,7 +169,10 @@ module ViewModel
       end
 
       def total_floor_area
-        xpath(%w[Property-Summary Total-Floor-Area])
+        building_part_areas = @xml_doc.search("//SAP-Building-Part/SAP-Floor-Dimensions/SAP-Floor-Dimension/Total-Floor-Area").map(&:content)
+        room_in_roof_areas = @xml_doc.search("//SAP-Building-Part/SAP-Room-In-Roof/Floor-Area").map(&:content)
+        all_areas = (building_part_areas + room_in_roof_areas).map(&:to_f)
+        all_areas.sum.round.to_i.to_s
       end
 
       def dwelling_type
