c2faf9f928fff9c4a1c94c21b7143a822c94adab
Barry Halper <barryhalper@gmail.com>
2021-07-19 14:15:30 +0100

    Updated quit_if_prodcution method to check the absense of stage in the ENV

--- lib/tasks/task_helpers.rb
+++ lib/tasks/task_helpers.rb
@@ -1,8 +1,11 @@
 module Tasks
   class TaskHelpers
     def self.quit_if_production
-      unless %w[test development integration staging].include? ENV["STAGE"]
+
+      unless ENV["STAGE"].nil?
+      unless  %w[test development integration staging].include? ENV["STAGE"]
         raise StandardError, "This task can only be run if the STAGE is test, development, integration or staging"
+      end
       end
     end
   end
