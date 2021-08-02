1eeb93638bde82e82eb6a328e83efbd251effdd1
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2021-08-02 12:57:16 +0100

    add a basic test for related assessments gateway

---
+++ spec/unit/gateway/related_assessments_gateway_spec.rb
@@ -0,0 +1,31 @@
+describe Gateway::RelatedAssessmentsGateway do
+  include RSpecRegisterApiServiceMixin
+
+  context "test getting related assessments" do
+    subject { described_class.new }
+
+    related_assessment_ids = %w[
+      0000-0000-0000-0000-0001
+      0000-0000-0000-0000-0003
+      0000-0000-0000-0000-0042
+    ]
+
+    address_id = nil
+
+    before(:all) do
+      @scheme_id = add_scheme_and_get_id
+      add_super_assessor(@scheme_id)
+      schema = "RdSAP-Schema-20.0.0"
+      xml = Nokogiri.XML Samples.xml(schema)
+      address_id = xml.at("UPRN").children.to_s
+      related_assessment_ids.each do |assessment_id|
+        xml.at("RRN").children = assessment_id
+        call_lodge_assessment scheme_id: @scheme_id, schema_name: schema, xml_document: xml
+      end
+    end
+
+    it "returns related assessment reference objects" do
+      expect(subject.by_address_id(address_id).map { |assessment| assessment.to_hash[:assessment_id] }.sort).to eq related_assessment_ids
+    end
+  end
+end
