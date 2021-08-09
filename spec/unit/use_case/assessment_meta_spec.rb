b1e81157a155f41ece6308f25401e7a2c7325d71
Barry Halper <barryhalper@gmail.com>
2021-08-09 15:48:58 +0100

    make format

--- spec/unit/use_case/assessment_meta_spec.rb
+++ spec/unit/use_case/assessment_meta_spec.rb
@@ -1,18 +1,17 @@
 describe UseCase::AssessmentMeta do
   context "Extract meta data from the database for an assessment" do
+    subject { UseCase::AssessmentMeta.new(gateway) }
 
-    let(:gateway){
+    let(:gateway) do
       instance_double("AssessmentMetaGateway")
-    }
-
-    subject {UseCase::AssessmentMeta.new(gateway)}
+    end
 
     before do
-      allow(gateway).to receive(:fetch).and_return({assessment: "0000-0000-0000-0000-0000"})
+      allow(gateway).to receive(:fetch).and_return({ assessment: "0000-0000-0000-0000-0000" })
     end
 
-    it 'executes the use case which calls the gateway' do
-      expect(subject.execute("0000-0000-0000-0000-0000")).to eq({assessment: "0000-0000-0000-0000-0000"})
+    it "executes the use case which calls the gateway" do
+      expect(subject.execute("0000-0000-0000-0000-0000")).to eq({ assessment: "0000-0000-0000-0000-0000" })
     end
   end
-  end
+end
