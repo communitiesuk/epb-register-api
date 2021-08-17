21f1f3ddecadc0d7688b2d7401c5625b46264a99
Barry Halper <barryhalper@gmail.com>
2021-08-17 09:58:10 +0100

    EPBR-1456 Updated Gateway test to non_domestic is not export for domestic

--- spec/unit/gateway/export_ni_gateway_spec.rb
+++ spec/unit/gateway/export_ni_gateway_spec.rb
@@ -21,7 +21,6 @@ describe Gateway::ExportNiGateway do
 
         domestic_sap_xml = Nokogiri.XML Samples.xml("SAP-Schema-18.0.0")
         domestic_sap_assessment_id = domestic_sap_xml.at("RRN")
-
         domestic_ni_sap_postcode = domestic_ni_sap_xml.at("Property Address Postcode")
         domestic_ni_rdsap_postcode = domestic_ni_rdsap_xml.at("Property Address Postcode")
         domestic_ni_sap_postcode.children = "BT4 3NE"
@@ -60,6 +59,19 @@ describe Gateway::ExportNiGateway do
           schema_name: "SAP-Schema-18.0.0",
           override: true,
         )
+
+        non_domestic_xml = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
+        non_domestic_assessment_id = non_domestic_xml.at("//CEPC:RRN")
+        non_domestic_assessment_id.children = "9000-0000-0000-0000-1019"
+        lodge_assessment(
+          assessment_body: non_domestic_xml.to_xml,
+          accepted_responses: [201],
+          auth_data: {
+            scheme_ids: [scheme_id],
+          },
+          override: true,
+          schema_name: "CEPC-8.0.0",
+          )
       end
 
       let!(:exported_data) do
