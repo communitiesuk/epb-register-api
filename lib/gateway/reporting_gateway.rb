31f7cc8d35a92555f13b6af5068bb402c8fc1b21
Christine Horrocks <splogy2001@hotmail.com>
2021-09-01 17:19:18 +0100

    Format DateTime on cancelled at in not for publication rake

--- lib/gateway/reporting_gateway.rb
+++ lib/gateway/reporting_gateway.rb
@@ -222,7 +222,7 @@ module Gateway
     def fetch_not_for_publication_assessments
       sql = <<~SQL
         SELECT assessment_id, type_of_assessment, address_id, address_line1, address_line2, address_line3,
-        town, postcode, to_char(date_registered, 'YYYY-MM-DD') as date_registered, opt_out, cancelled_at,
+        town, postcode, to_char(date_registered, 'YYYY-MM-DD') as date_registered, opt_out, to_char(cancelled_at , 'YYYY-MM-DD HH:MI:SS') as cancelled_at,
         to_char(not_for_issue_at , 'YYYY-MM-DD HH:MI:SS') as not_for_issue_at
         FROM assessments
         WHERE type_of_assessment IN ('SAP', 'RdSAP', 'CEPC', 'DEC')
