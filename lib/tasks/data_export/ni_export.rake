4f7d58bc3496ab2ba46f16d8aadc97ebeae4a64c
Barry Halper <barryhalper@gmail.com>
2021-09-03 15:15:38 +0100

    EPBR-1478 Update rake to include assessment type argument, raise errors gracefully and transmit file to s3

--- lib/tasks/data_export/ni_export.rake
+++ lib/tasks/data_export/ni_export.rake
@@ -1,8 +1,32 @@
 namespace :data_export do
   desc "Exporting assessments data for Northern Ireland"
 
-  task :ni_assessments do
+  task :ni_assessments, %i[type_of_assessments] do |_, args|
+    type_of_assessments = args.type_of_assessments
+
+
+    raise Boundary::ArgumentMissing, "type_of_assessments" unless type_of_assessments
+
     exporter = ApiFactory.ni_assessments_export_use_case
-    exports = exporter.execute(%w[RdSAP SAP])
+    data = exporter.execute(type_of_assessments)
+
+    raise Boundary::OpenDataEmpty if data.length.zero?
+
+    csv_data = Helper::ExportHelper.to_csv(data)
+    transmit_ni_file(csv_data, type_of_assessments)
   end
+end
+
+private
+def transmit_ni_file(data, type_of_assessments)
+  assessment_types = type_of_assessments.is_a?(Array) ? type_of_assessments.join('_') : type_of_assessments
+  filename = "ni_assessments_export_#{assessment_types.downcase}_#{DateTime.now.strftime('%F')}.csv"
+  bucket_name = ENV["bucket_name"]
+  instance_name = ENV["instance_name"]
+
+  storage_gateway = ApiFactory.storage_gateway(
+    bucket_name: bucket_name,
+    instance_name: instance_name,
+    )
+  storage_gateway.write_file(filename, data)
 end
--- spec/acceptance/reporting/export_nI_assesements_spec.rb
+++ spec/acceptance/reporting/export_nI_assesements_spec.rb
@@ -1,9 +1,9 @@
 describe "Acceptance::Reports::ExportNIAssessments" do
   include RSpecRegisterApiServiceMixin
 
-  after { WebMock.disable! }
-
   context "when calling rake to export NI data"
+
+  let(:storage_gateway) { instance_double(Gateway::StorageGateway) }
 
   let(:ni_gateway) do
     instance_double(Gateway::ExportNiGateway)
@@ -13,17 +13,49 @@ describe "Acceptance::Reports::ExportNIAssessments" do
     instance_double(Gateway::AssessmentsXmlGateway)
   end
 
-  it "does not raise an error" do
+  let(:bucket_name) { "bucket_name" }
+  let(:instance_name) { "epb-s3-service" }
+
+  before do
+    allow(ENV).to receive(:[])
+    allow(ENV).to receive(:[]).with("instance_name").and_return(instance_name)
+    allow(ENV).to receive(:[]).with("bucket_name").and_return(bucket_name)
+
+    # Prevents logging during tests
+    allow($stdout).to receive(:puts)
+
     domestic_ni_sap_xml = Nokogiri.XML Samples.xml("SAP-Schema-NI-18.0.0")
     allow(ni_gateway).to receive(:fetch_assessments).with(%w[RdSAP SAP]).and_return([
-      { "assessment_id" => "0000-0000-0000-0000-0000", "lodgement_date" => "2020-05-04", "lodgement_datetime" => "2021-02-22 00:00:00", "uprn" => "UPRN-000000000001", "opt_out" => false, "cancelled" => false },
-      { "assessment_id" => "8888-0000-0000-0000-0002", "lodgement_date" => "2020-05-04", "lodgement_datetime" => "2021-02-22 00:00:00", "uprn" => "UPRN-000000000000" },
-      { "assessment_id" => "9999-0000-0000-0000-0000", "lodgement_date" => "2020-05-04", "lodgement_datetime" => "2021-02-22 00:00:00", "uprn" => nil },
-    ])
+                                                                                      { "assessment_id" => "0000-0000-0000-0000-0000", "lodgement_date" => "2020-05-04", "lodgement_datetime" => "2021-02-22 00:00:00", "uprn" => "UPRN-000000000001", "opt_out" => false, "cancelled" => false },
+                                                                                      { "assessment_id" => "8888-0000-0000-0000-0002", "lodgement_date" => "2020-05-04", "lodgement_datetime" => "2021-02-22 00:00:00", "uprn" => "UPRN-000000000000" },
+                                                                                      { "assessment_id" => "9999-0000-0000-0000-0000", "lodgement_date" => "2020-05-04", "lodgement_datetime" => "2021-02-22 00:00:00", "uprn" => nil },
+                                                                                    ])
     allow(xml_gateway).to receive(:fetch).and_return({ xml: domestic_ni_sap_xml.to_xml, schema_type: "RdSAP-Schema-NI-20.0.0" })
     allow(ApiFactory).to receive(:ni_assessments_gateway).and_return(ni_gateway)
     allow(ApiFactory).to receive(:assessments_xml_gateway).and_return(xml_gateway)
+    allow(storage_gateway).to receive(:write_file)
+    allow(ApiFactory).to receive(:storage_gateway).and_return(storage_gateway)
+  end
 
-    expect { get_task("data_export:ni_assessments").invoke }.not_to raise_error
+  it "does not raise an error" do
+    expect { get_task("data_export:ni_assessments").invoke(%w[RdSAP SAP]) }.not_to raise_error
+  end
+
+  # it "pass converted file to S3" do
+  #   allow(Gateway::StorageGateway).to receive(:new).and_return(
+  #     storage_gateway,
+  #     )
+  #   allow(storage_gateway).to receive(:write_file)
+  #
+  #   get_task("data_export:ni_assessments").invoke(%w[RdSAP SAP])
+  #
+  #   expect(storage_gateway).to have_received(:write_file).with(
+  #     "open_data_export_not_for_publication_#{DateTime.now.strftime('%F')}.csv",
+  #     csv_data,
+  #     )
+  # end
+
+  it 'raises an error if no argument is passed' do
+    expect { get_task("data_export:ni_assessments").invoke }.to raise_error(Boundary::ArgumentMissing)
   end
 end
