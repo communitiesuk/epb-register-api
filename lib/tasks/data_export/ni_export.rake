e5f401d6395152ace1c90b4a64312629706ce481
Barry Halper <barryhalper@gmail.com>
2021-09-06 11:34:36 +0100

    make format

--- lib/tasks/data_export/ni_export.rake
+++ lib/tasks/data_export/ni_export.rake
@@ -19,15 +19,15 @@ namespace :data_export do
 end
 
 private
-def transmit_ni_file(data, type_of_assessments)
-  assessment_types = type_of_assessments.is_a?(Array) ? type_of_assessments.join('_') : type_of_assessments
-  filename = "ni_assessments_export_#{assessment_types.downcase}_#{DateTime.now.strftime('%F')}.csv"
 
+def transmit_ni_file(data, type_of_assessments)
+  assessment_types = type_of_assessments.is_a?(Array) ? type_of_assessments.join("_") : type_of_assessments
+  filename = "ni_assessments_export_#{assessment_types.downcase}_#{DateTime.now.strftime('%F')}.csv"
 
   storage_config_reader = Gateway::StorageConfigurationReader.new(
     instance_name: ENV["INSTANCE_NAME"],
     bucket_name: ENV["BUCKET_NAME"],
-    )
+  )
   storage_gateway = Gateway::StorageGateway.new(storage_config: storage_config_reader.get_configuration)
   storage_gateway.write_file(filename, data)
 end
--- spec/acceptance/reporting/export_nI_assesements_spec.rb
+++ spec/acceptance/reporting/export_nI_assesements_spec.rb
@@ -3,9 +3,7 @@ describe "Acceptance::Reports::ExportNIAssessments" do
 
   subject(:task) { get_task("data_export:ni_assessments") }
 
-
-  context 'call the rake to export the data' do
-
+  context "call the rake to export the data" do
     let(:storage_gateway) { instance_double(Gateway::StorageGateway) }
 
     let(:ni_gateway) do
@@ -20,10 +18,9 @@ describe "Acceptance::Reports::ExportNIAssessments" do
       instance_double(UseCase::ExportNiAssessments)
     end
 
-
-    let(:file_name) {
+    let(:file_name) do
       "ni_assessments_export_rdsap_sap_#{DateTime.now.strftime('%F')}.csv"
-    }
+    end
 
     let(:export_use_case) do
       instance_double(UseCase::ExportNiAssessments)
@@ -46,7 +43,7 @@ describe "Acceptance::Reports::ExportNIAssessments" do
       # Define mock expectations
       allow(ApiFactory).to receive(:ni_assessments_export_use_case).and_return(
         export_use_case,
-        )
+      )
       allow(export_use_case).to receive(:execute).and_return(export)
       allow(ApiFactory).to receive(:storage_gateway).and_return(storage_gateway)
       HttpStub.s3_put_csv(file_name)
@@ -55,17 +52,12 @@ describe "Acceptance::Reports::ExportNIAssessments" do
     it "sends the converted csv to the S3 bucket " do
       task.invoke(%w[RdSAP SAP])
       expect(WebMock).to have_requested(
-                           :put,
-                           "#{HttpStub::S3_BUCKET_URI}#{file_name}",
-                           ).with( body: "ASSESSMENT_ID,ADDRESS1,ADDRESS2\n9999-0000-0000-0000-0000,1 Some Street,\"\"\n",
-        headers: {
-          "Host" => "s3.eu-west-2.amazonaws.com",
-        },
-        )
+        :put,
+        "#{HttpStub::S3_BUCKET_URI}#{file_name}",
+      ).with(body: "ASSESSMENT_ID,ADDRESS1,ADDRESS2\n9999-0000-0000-0000-0000,1 Some Street,\"\"\n",
+             headers: {
+               "Host" => "s3.eu-west-2.amazonaws.com",
+             })
     end
-
-
   end
-
-
 end
