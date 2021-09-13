514854b15445b938920ed6599a96295285acdedb
Barry Halper <barryhalper@gmail.com>
2021-09-13 12:51:38 +0100

    EPBR-489 Update gateway to save data to the database

--- lib/gateway/audit_logs_gateway.rb
+++ lib/gateway/audit_logs_gateway.rb
@@ -1,9 +1,35 @@
 module Gateway
   class AuditLogsGateway
+    def add_audit_event(event_type:, entity_id:, entity_type:, data: nil)
+      insert_sql = <<-SQL
+            INSERT INTO audit_logs(event_type, entity_id, entity_type, data)
+            VALUES ($1, $2, $3, $4)
+      SQL
 
-    def add_audit_event(event_type:, entity_id:, entity_type:,  data: nil)
-      1
+      bindings = [
+        ActiveRecord::Relation::QueryAttribute.new(
+          "event_type",
+          event_type,
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "entity_id",
+          entity_id,
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "entity_type",
+          entity_type,
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "data",
+          data,
+          ActiveRecord::Type::String.new,
+        ),
+      ]
+
+      ActiveRecord::Base.connection.exec_query(insert_sql, "SQL", bindings)
     end
-
   end
 end
--- spec/unit/gateway/audit_logs_gateway_spec.rb
+++ spec/unit/gateway/audit_logs_gateway_spec.rb
@@ -4,14 +4,29 @@ describe Gateway::AuditLogsGateway do
   subject(:gateway) { described_class.new }
 
   context "when adding data to the audit log table " do
-
-    it 'does not raise an error' do
-      expect {subject}.not_to raise_error
+    let(:saved_data) do
+      ActiveRecord::Base.connection.exec_query("SELECT * FROM audit_logs")
     end
 
-    it 'receives add_audit_log method' do
-      allow(subject).to receive(:add_audit_event).and_return(1)
-      expect(subject.add_audit_event(entity_type: "assessment", entity_id: "0000-0000-0000-0000-0001", event_type: "opt_out")).to eq(1)
+    it "does not raise an error" do
+      expect { gateway }.not_to raise_error
+    end
+
+    it "saves the correct data into the database for an opted_out assessment" do
+      gateway.add_audit_event(entity_type: "assessment", entity_id: "0000-0000-0000-0000-0001", event_type: "opt_out")
+      expect(saved_data.length).to eq(1)
+      expect(saved_data.first.symbolize_keys).to match a_hash_including({ entity_type: "assessment", entity_id: "0000-0000-0000-0000-0001", event_type: "opt_out" })
+    end
+
+    it "the date saved into the timestamp field is today" do
+      gateway.add_audit_event(entity_type: "assessment", entity_id: "0000-0000-0000-0000-0001", event_type: "opt_out")
+      expect(saved_data.first["timestamp"].strftime("%Y%m%d")).to eq(Time.now.strftime("%Y%m%d"))
+    end
+
+    it "saves the correct data into the database for an another event type " do
+      gateway.add_audit_event(entity_type: "assessment", entity_id: "0000-0000-0000-0000-0002", event_type: "opt_in")
+      expect(saved_data.length).to eq(1)
+      expect(saved_data.first.symbolize_keys).to match a_hash_including({ entity_type: "assessment", entity_id: "0000-0000-0000-0000-0002", event_type: "opt_in" })
     end
   end
 end
