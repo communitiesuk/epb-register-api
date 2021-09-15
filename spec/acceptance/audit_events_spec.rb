cdc6dbb446f5bdc7442290589a5e23eb9708a163
Aga Dufrat <a.dufrat@gmail.com>
2021-09-15 15:10:42 +0100

    Refactor audit log tests

--- spec/acceptance/audit_events_spec.rb
+++ spec/acceptance/audit_events_spec.rb
@@ -4,7 +4,6 @@ describe "Audit events" do
   before do
     EventBroadcaster.enable!
 
-    scheme_id = add_scheme_and_get_id
     add_super_assessor(scheme_id: scheme_id)
     lodge_assessment(
       assessment_body: Samples.xml("RdSAP-Schema-20.0.0"),
@@ -15,11 +14,26 @@ describe "Audit events" do
     )
   end
 
-  context "when lodging an assessment" do
-    let(:saved_data) do
-      ActiveRecord::Base.connection.exec_query("SELECT * FROM audit_logs")
-    end
+  let(:scheme_id) { add_scheme_and_get_id }
 
+  let(:saved_data) do
+    ActiveRecord::Base.connection.exec_query("SELECT * FROM audit_logs ORDER BY timestamp DESC")
+  end
+
+  it "saves the request headers as JSON" do
+    expect(JSON.parse(saved_data.first["data"])).to match a_hash_including(
+      { "REQUEST_METHOD" => "POST",
+        "SERVER_NAME" => "example.org",
+        "QUERY_STRING" => "",
+        "PATH_INFO" => "/api/assessments",
+        "CONTENT_LENGTH" => "16479",
+        "REMOTE_ADDR" => "127.0.0.1",
+        "CONTENT_TYPE" => "application/xml+RdSAP-Schema-20.0.0" },
+    )
+    expect(JSON.parse(saved_data.first["data"])).to have_key("HTTP_AUTHORIZATION")
+  end
+
+  context "when lodging an assessment" do
     it "saves the event to the audit log" do
       expect(saved_data).to match [a_hash_including(
         { "entity_type" => "assessment",
@@ -27,26 +41,9 @@ describe "Audit events" do
           "event_type" => "lodgement" },
       )]
     end
-
-    it "saves the request headers as JSON" do
-      expect(JSON.parse(saved_data.first["data"])).to match a_hash_including(
-        { "REQUEST_METHOD" => "POST",
-          "SERVER_NAME" => "example.org",
-          "QUERY_STRING" => "",
-          "PATH_INFO" => "/api/assessments",
-          "CONTENT_LENGTH" => "16479",
-          "REMOTE_ADDR" => "127.0.0.1",
-          "CONTENT_TYPE" => "application/xml+RdSAP-Schema-20.0.0" },
-      )
-      expect(JSON.parse(saved_data.first["data"])).to have_key("HTTP_AUTHORIZATION")
-    end
   end
 
   context "when opting out an assessment" do
-    let(:saved_data) do
-      ActiveRecord::Base.connection.exec_query("SELECT * FROM audit_logs")
-    end
-
     before do
       opt_out_assessment(assessment_id: "0000-0000-0000-0000-0000")
     end
@@ -58,26 +55,9 @@ describe "Audit events" do
           "event_type" => "opt out" },
       )
     end
-
-    it "saves the request headers as JSON" do
-      expect(JSON.parse(saved_data.last["data"])).to match a_hash_including(
-        { "REQUEST_METHOD" => "POST",
-          "SERVER_NAME" => "example.org",
-          "QUERY_STRING" => "",
-          "PATH_INFO" => "/api/assessments",
-          "CONTENT_LENGTH" => "16479",
-          "REMOTE_ADDR" => "127.0.0.1",
-          "CONTENT_TYPE" => "application/xml+RdSAP-Schema-20.0.0" },
-      )
-      expect(JSON.parse(saved_data.last["data"])).to have_key("HTTP_AUTHORIZATION")
-    end
   end
 
   context "when opting in an assessment" do
-    let(:saved_data) do
-      ActiveRecord::Base.connection.exec_query("SELECT * FROM audit_logs")
-    end
-
     before do
       opt_out_assessment(assessment_id: "0000-0000-0000-0000-0000", opt_out: false)
     end
