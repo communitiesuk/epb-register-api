37205b1a8d98f384361db2d8346656842abc321d
Aga Dufrat <a.dufrat@gmail.com>
2021-10-25 13:07:39 +0100

    Make SaveDailyAssessmentsStats return formatted stats

--- lib/use_case/save_daily_assessments_stats.rb
+++ lib/use_case/save_daily_assessments_stats.rb
@@ -1,7 +1,39 @@
 module UseCase
   class SaveDailyAssessmentsStats
-    def initialize(assessment_statistics_gateway)
+    def initialize(assessment_statistics_gateway:, assessments_gateway:)
       @assessment_statistics_gateway = assessment_statistics_gateway
+      @assessments_gateway = assessments_gateway
+    end
+
+    def execute(date:)
+      @assessments = @assessments_gateway.fetch_assessments_by_date(date)
+
+      format_stats_data
+    end
+
+  private
+
+    def format_stats_data
+      result = []
+      grouped_by_type_and_scheme.each do |assessment_type, scheme_assessments|
+        scheme_assessments.each do |scheme_id, assessments|
+          hash = {}
+          hash[:assessment_type] = assessment_type
+          hash[:scheme_id] = scheme_id
+          hash[:assessments_count] = assessments.size
+          # TODO: get it from the XML
+          hash[:rating_average] = assessments.map { |a| a[:current_energy_rating] }.sum / assessments.size
+          result << hash
+        end
+      end
+
+      result
+    end
+
+    def grouped_by_type_and_scheme
+      @assessments.group_by { |assessment| assessment["assessment_type"] }.transform_values do |x|
+        x.group_by { |y| y[:scheme_id] }
+      end
     end
   end
 end
--- spec/unit/use_case/save_daily_assessments_stats_spec.rb
+++ spec/unit/use_case/save_daily_assessments_stats_spec.rb
@@ -1,19 +1,48 @@
 describe UseCase::SaveDailyAssessmentsStats do
   subject(:use_case) do
-    described_class.new(gateway)
+    described_class.new(assessment_statistics_gateway: statistics_gateway, assessments_gateway: assessments_gateway)
   end
 
-  let(:gateway) do
-    instance_double(Gateway::AssessmentStatisticsGateway)
-  end
+  let(:statistics_gateway) { instance_double(Gateway::AssessmentStatisticsGateway) }
+  let(:assessments_gateway) { instance_double(Gateway::AssessmentsGateway) }
 
-  context "when saving statistics data to the database" do
+  context "when deriving the statistics for a given date" do
     before do
-      allow(gateway).to receive(:save)
+      allow(statistics_gateway).to receive(:save)
+      allow(assessments_gateway).to receive(:fetch_assessments_by_date).and_return(
+        [
+          { "assessment_id" => "0000-0000-0000-0000", "assessment_type" => "RdSAP", "current_energy_rating": 58, "scheme_id": 1 },
+          { "assessment_id" => "0000-0000-0000-0001", "assessment_type" => "SAP", "current_energy_rating": 50, "scheme_id": 1 },
+          { "assessment_id" => "0000-0000-0000-0002", "assessment_type" => "SAP", "current_energy_rating": 30, "scheme_id": 2 },
+          { "assessment_id" => "0000-0000-0000-0003", "assessment_type" => "SAP", "current_energy_rating": 50, "scheme_id": 2 },
+          { "assessment_id" => "0000-0000-0000-0003", "assessment_type" => "SAP", "current_energy_rating": 89, "scheme_id": 2 },
+        ],
+      )
     end
 
-    it "loads the class without error" do
-      expect { use_case }.not_to raise_error
+    it "calculates the avarage and groups them by assessment type and scheme id" do
+      expect(use_case.execute(date: "2021-10-25")).to eq(
+        [
+          {
+            assessment_type: "RdSAP",
+            assessments_count: 1,
+            rating_average: 58,
+            scheme_id: 1,
+          },
+          {
+            assessment_type: "SAP",
+            assessments_count: 1,
+            rating_average: 50,
+            scheme_id: 1,
+          },
+          {
+            assessment_type: "SAP",
+            assessments_count: 3,
+            rating_average: 56,
+            scheme_id: 2,
+          },
+        ],
+      )
     end
   end
 end
