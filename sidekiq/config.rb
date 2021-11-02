47dd29a9530fb9b8d676fb979621d7c12992e50e
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2021-11-02 10:37:59 +0000

    Use load_from_hash!
    We have an issue where old jobs are not removed from the Redis cron queue.
    With the exclamation mark added to the method call this will first clear down old jobs before adding new jobs from the file.

--- sidekiq/config.rb
+++ sidekiq/config.rb
@@ -26,5 +26,5 @@ end
 schedule_file = "./sidekiq/schedule.yml"
 
 if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
 end
