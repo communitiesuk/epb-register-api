e13a605bb54b10e89fc459f456b2364d9f5ecdf0
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2021-11-04 15:49:34 +0000

    remove state from save daily assessments stats use case

--- lib/use_case/save_daily_assessments_stats.rb
+++ lib/use_case/save_daily_assessments_stats.rb
@@ -7,17 +7,17 @@ module UseCase
     end
 
     def execute(date:, assessment_types: nil)
-      @assessments = @assessments_gateway.fetch_assessments_by_date(date: date, assessment_types: assessment_types).map(&:symbolize_keys)
+      assessments = @assessments_gateway.fetch_assessments_by_date(date: date, assessment_types: assessment_types).map(&:symbolize_keys)
 
-      raise Boundary::NoData, date if @assessments.empty?
+      raise Boundary::NoData, date if assessments.empty?
 
-      @assessments.each do |assessment|
+      assessments.each do |assessment|
         stats = stats_from_xml(assessment[:assessment_id])
 
         assessment.merge!(stats) unless stats.nil?
       end
 
-      format_stats_data.each do |stat|
+      format_stats_data(assessments: assessments).each do |stat|
         @assessment_statistics_gateway.save(
           day_date: date,
           assessment_type: stat[:assessment_type],
@@ -26,22 +26,20 @@ module UseCase
           rating_average: stat[:rating_average],
         )
       end
-
-      format_stats_data
     end
 
   private
 
-    def format_stats_data
+    def format_stats_data(assessments:)
       result = []
 
-      grouped_by_assessment_type_and_transaction.each do |assessment_type, transaction_types|
-        transaction_types.each do |transaction_type, assessments|
+      group_by_type_and_transaction(assessments).each do |assessment_type, transaction_types|
+        transaction_types.each do |transaction_type, assessments_with_type|
           hash = {
             assessment_type: assessment_type,
             transaction_type: transaction_type,
-            assessments_count: assessments.size,
-            rating_average: average_rating(assessments),
+            assessments_count: assessments_with_type.size,
+            rating_average: average_rating(assessments_with_type),
           }
 
           result << hash
@@ -64,8 +62,8 @@ module UseCase
       Presenter::Export::Statistics.new(wrapper).build
     end
 
-    def grouped_by_assessment_type_and_transaction
-      @assessments.group_by { |assessment| assessment[:type_of_assessment] }.transform_values do |assessments_for_assessment_type|
+    def group_by_type_and_transaction(assessments)
+      assessments.group_by { |assessment| assessment[:type_of_assessment] }.transform_values do |assessments_for_assessment_type|
         assessments_for_assessment_type.group_by { |assessment| assessment[:transaction_type] }
       end
     end
