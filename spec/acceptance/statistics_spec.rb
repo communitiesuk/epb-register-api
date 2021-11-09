d88f6372a0e852fd533c879834699acc24a93242
Barry Halper <barryhalper@gmail.com>
2021-11-09 14:28:41 +0000

    EPBR-1688 Update acceptance test to check the value of the response

--- spec/acceptance/statistics_spec.rb
+++ spec/acceptance/statistics_spec.rb
@@ -1,4 +1,4 @@
-describe "Acceptance::AssessmentStatistics" do
+describe "Acceptance::AssessmentStatistics", set_with_timecop: true do
   include RSpecRegisterApiServiceMixin
 
   before(:all) do
@@ -14,19 +14,45 @@ describe "Acceptance::AssessmentStatistics" do
       },
       override: true,
     )
+    rrn = domestic_rdsap_xml.at("RRN")
+    rrn.children = "0000-0000-0000-0000-0002"
+    lodge_assessment(
+      assessment_body: domestic_rdsap_xml.to_xml,
+      accepted_responses: [201],
+      auth_data: {
+        scheme_ids: [scheme_id],
+      },
+      override: true,
+      )
+
+    ApiFactory.save_daily_assessments_stats_use_case
+              .execute(date: Time.now.strftime("%F"), assessment_types: %w[SAP RdSAP CEPC DEC AC-CERT AC-REPORT])
   end
 
-  it "returns a 200 when calling the statistics data end point" do
-    fetch_statistics(
-      accepted_responses: [200],
-      scopes: %w[statistics:fetch],
-    )
+  context "when calling the statistics data end" do
+    it "returns a 200 status" do
+      fetch_statistics(
+        accepted_responses: [200],
+        scopes: %w[statistics:fetch],
+      )
+    end
+
+  it "produces a json object of the aggregated data" do
+
+    response =   fetch_statistics(
+          accepted_responses: [200],
+          scopes: %w[statistics:fetch],
+          )
+    expect(JSON.parse(response.body, symbolize_names: true)[:data]).to eq([{:assessmentType=>"RdSAP", :monthYear=>Time.now.strftime("%m-%Y"), :numAssessments=>2, :ratingAverage=>50.0}])
+  end
   end
 
-  it "returns a 403 when calling the statistics data with the wrong scope" do
-    fetch_statistics(
-      accepted_responses: [403],
-      scopes: %w[assessments:fetch],
-    )
+  context "when calling the calling the statistics data end point with the wrong token" do
+    it "returns a 403 status" do
+      fetch_statistics(
+        accepted_responses: [403],
+        scopes: %w[assessments:fetch],
+      )
+    end
   end
 end
