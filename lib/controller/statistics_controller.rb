4dd735ab8757e4b8cb8bf5c641f706ea4df561e1
Barry Halper <barryhalper@gmail.com>
2021-11-10 09:36:49 +0000

    EPBR-1688 Update end point to only allow requests for new stats token

--- lib/controller/statistics_controller.rb
+++ lib/controller/statistics_controller.rb
@@ -1,7 +1,7 @@
 module Controller
   class StatisticsController < Controller::BaseController
     get "/api/statistics",
-        auth_token_has_all: %w[assessment:fetch] do
+        auth_token_has_all: %w[statistics:fetch] do
       use_case = UseCase::FetchMonthlyAssessmentStats.new(Gateway::AssessmentStatisticsGateway.new)
       report = use_case.execute
       json_api_response(data: report)
