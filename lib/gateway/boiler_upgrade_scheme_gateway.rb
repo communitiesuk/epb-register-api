5a6ea24becd0c09640dc2df7e10b15b79b435142
Barry Halper <barryhalper@gmail.com>
2022-05-24 12:39:00 +0100

    EPBR-226 Refactored BUS gateway into common module to be used as a mixin for HERA gateway

--- lib/gateway/boiler_upgrade_scheme_gateway.rb
+++ lib/gateway/boiler_upgrade_scheme_gateway.rb
@@ -1,57 +1,33 @@
 module Gateway
   class BoilerUpgradeSchemeGateway
+    include Gateway::CommonAddressSearchGateway
+
     DOMESTIC_TYPES = %w[
       RdSAP
       SAP
     ].freeze
 
+    ASSESSMENT_TYPES = %w[
+      RdSAP
+      SAP
+      CEPC
+    ].freeze
+
     def search_by_postcode_and_building_identifier(postcode:, building_identifier:)
       identifier = clean_building_identifier building_identifier
       if identifier.match?(/^\d+$/)
-        search_by_postcode_and_building_number postcode: postcode, building_number: identifier
+        search_by_postcode_and_building_number postcode: postcode, building_number: identifier, assessment_types: ASSESSMENT_TYPES
       else
-        search_by_postcode_and_building_name postcode: postcode, building_name: identifier
+        search_by_postcode_and_building_name postcode: postcode, building_name: identifier, assessment_types: ASSESSMENT_TYPES
       end
     end
 
     def search_by_uprn(uprn)
-      sql = <<-SQL
-        SELECT
-          a.assessment_id AS epc_rrn,
-          a.type_of_assessment AS report_type,
-          a.date_of_expiry AS expiry_date
-        FROM assessments AS a
-        WHERE assessment_id IN (
-          SELECT assessment_id FROM assessments_address_id WHERE address_id = $1
-        )
-        AND a.type_of_assessment IN ('RdSAP', 'SAP', 'CEPC')
-      SQL
-
-      do_search(
-        sql: sql,
-        binds: [
-          string_attribute("uprn", uprn),
-        ],
-      )
+      fetch_by_uprn(uprn, ASSESSMENT_TYPES)
     end
 
     def search_by_rrn(rrn)
-      sql = <<-SQL
-        SELECT
-          assessment_id AS epc_rrn,
-          type_of_assessment AS report_type,
-          date_of_expiry AS expiry_date
-        FROM assessments
-        WHERE assessment_id = $1
-        AND type_of_assessment IN ('RdSAP', 'SAP', 'CEPC')
-      SQL
-
-      do_search(
-        sql: sql,
-        binds: [
-          string_attribute("rrn", rrn),
-        ],
-      )
+      fetch_by_rrn(rrn)
     end
 
   private
@@ -84,73 +60,6 @@ module Gateway
       nil
     end
 
-    def search_by_postcode_and_building_name(postcode:, building_name:)
-      sql = <<-SQL
-        SELECT
-            a.assessment_id AS epc_rrn,
-            a.type_of_assessment AS report_type,
-            a.date_of_expiry AS expiry_date
-          FROM assessments AS a
-          WHERE a.postcode = $1 AND (a.address_line1 ILIKE $2 OR a.address_line2 ILIKE $2)
-          AND a.type_of_assessment IN ('RdSAP', 'SAP', 'CEPC')
-      SQL
-
-      do_search(
-        sql: sql,
-        binds: [
-          string_attribute("postcode", Helper::ValidatePostcodeHelper.format_postcode(postcode)),
-          string_attribute("building_name", "%#{building_name}%"),
-        ],
-      )
-    end
-
-    def search_by_postcode_and_building_number(postcode:, building_number:)
-      sql = <<-SQL
-        SELECT
-            a.assessment_id AS epc_rrn,
-            a.type_of_assessment AS report_type,
-            a.date_of_expiry AS expiry_date
-          FROM assessments AS a
-          WHERE a.postcode = $1 AND
-            (
-              a.address_line1 ~ $2
-              OR a.address_line2 ~ $2
-              OR a.address_line1 ~ $3
-              OR a.address_line2 ~ $3
-              OR a.address_line1 ~ $4
-              OR a.address_line2 ~ $4
-              OR a.address_line1 = $5
-              OR a.address_line2 = $5
-            )
-          AND a.type_of_assessment IN ('RdSAP', 'SAP', 'CEPC')
-      SQL
-
-      do_search(
-        sql: sql,
-        binds: [
-          string_attribute("postcode", Helper::ValidatePostcodeHelper.format_postcode(postcode)),
-          string_attribute("number_in_middle", sprintf('\D+%s\D+', building_number)),
-          string_attribute("number_at_start", sprintf('^%s\D+', building_number)),
-          string_attribute("number_at_end", sprintf('\D+%s$', building_number)),
-          string_attribute("number_exact", building_number),
-        ],
-      )
-    end
-
-    def do_search(sql:, binds:)
-      results = ActiveRecord::Base.connection.exec_query(sql, "SQL", binds)
-      details_list = results.map { |result| row_to_domain(result) { |summary| !summary[:superseded_by] } }.compact
-
-      case details_list.count
-      when 0
-        nil
-      when 1
-        details_list.first
-      else
-        Domain::AssessmentReferenceList.new(*details_list.map(&:rrn))
-      end
-    end
-
     def fetch_property_description(node_name:, summary:)
       if summary[:property_summary]
         summary[:property_summary].each do |feature|
@@ -167,14 +76,6 @@ module Gateway
       end
 
       summary[:recommended_improvements].any? { |improvement| improvement[:improvement_type] == type }
-    end
-
-    def string_attribute(name, value)
-      ActiveRecord::Relation::QueryAttribute.new(
-        name,
-        value,
-        ActiveRecord::Type::String.new,
-      )
     end
 
     def clean_building_identifier(building_identifier)
