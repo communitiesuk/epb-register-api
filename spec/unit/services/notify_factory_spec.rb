1d41cd48f0aa7d864c3b4c88650e98ce5ac9504b
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2022-07-04 12:10:52 +0100

    stub request state in notify factory test for test isolation

--- spec/unit/services/notify_factory_spec.rb
+++ spec/unit/services/notify_factory_spec.rb
@@ -1,11 +1,12 @@
 describe NotifyFactory do
-  let(:save_audit_event_use_case) { instance_spy(UseCase::SaveAuditEvent) }
-
   describe "notify to audit log" do
-    notifiers = described_class.methods.select { |method| method.end_with?("to_audit_log") }
+    before do
+      allow(RequestModule).to receive(:relevant_request_headers).and_return("{}")
+    end
 
+    notifiers = described_class.methods.select { |method| method.end_with?("to_audit_log") }
     notifiers.each do |notifier_method|
-      it "#{notifier_method} calls the save audit event use case" do
+      it "#{notifier_method} calls the save audit event use case without erroring" do
         args = case notifier_method
                when :opt_out_to_audit_log
                  { entity_id: "0000-0000", is_opt_out: true }
