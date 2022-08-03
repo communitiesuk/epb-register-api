50f80a0198bdd53f7d9e7073270515649a77b7b5
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2022-08-03 13:11:32 +0100

    correct migration class/filename spelling (just to trigger new deploy)

--- db/migrate/20211022130717_create_assessment_statistics.rb
+++ db/migrate/20211022130717_create_assessment_statistics.rb
@@ -1,4 +1,4 @@
-class CreateAssessmentStatisitcs < ActiveRecord::Migration[6.1]
+class CreateAssessmentStatistics < ActiveRecord::Migration[6.1]
   def change
     create_table :assessment_statistics, primary_key: nil do |t|
       t.integer :assessments_count, null: false
