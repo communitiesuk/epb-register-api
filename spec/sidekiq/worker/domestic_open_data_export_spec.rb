22f02b1a4b76b1e18d55dd84a164717d32cd4da2
al01010 <alice01010@hotmail.co.uk>
2022-10-10 15:42:41 +0100

    EPBR-2537 Refactored test

--- spec/sidekiq/worker/domestic_open_data_export_spec.rb
+++ spec/sidekiq/worker/domestic_open_data_export_spec.rb
@@ -2,80 +2,31 @@ require_relative "../../acceptance/reporting/open_data_export_test_helper"
 require "sentry-ruby"
 
 RSpec.describe Worker::DomesticOpenDataExport do
-  include RSpecRegisterApiServiceMixin
-
   describe "#perform" do
-    before do
-      Timecop.freeze(2022, 9, 1, 0, 0, 0)
-      WebMock.enable!
-    end
-
-    after do
-      Timecop.return
-      WebMock.disable!
-    end
-
     context "when there is data to send" do
       before do
-        scheme_id = lodge_assessor
-        domestic_rdsap_xml =
-          get_assessment_xml(
-            "RdSAP-Schema-20.0.0",
-            "0000-0000-0000-0000-0004",
-            Date.yesterday.strftime("%Y-%m-%d"),
-          )
-        lodge_assessment(
-          assessment_body: domestic_rdsap_xml.to_xml,
-          accepted_responses: [201],
-          auth_data: {
-            scheme_ids: [scheme_id],
-          },
-          migrated: true,
-        )
-
-        domestic_rdsap_xml =
-          get_assessment_xml(
-            "RdSAP-Schema-20.0.0",
-            "0000-0000-0000-0000-1004",
-            Date.yesterday.strftime("%Y-%m-%d"),
-          )
-        lodge_assessment(
-          assessment_body: domestic_rdsap_xml.to_xml,
-          accepted_responses: [201],
-          auth_data: {
-            scheme_ids: [scheme_id],
-          },
-          migrated: true,
-        )
-
-        EnvironmentStub
-          .all
-
-        stub_request(:put, "https://s3.eu-west-2.amazonaws.com/test_bucket/test/open_data_export_sap-rdsap_2022-09-01_1.csv")
-          .to_return(status: 200, body: "", headers: {})
+        allow(Worker::OpenDataExportHelper).to receive(:call_rake)
       end
 
       it "executes the rake which calls the use case" do
         expect { described_class.new.perform }.not_to raise_error
       end
 
-      it "sends the file to the S3 bucket" do
+      it "calls rake with correct arguments" do
         described_class.new.perform
-        expect(WebMock).to have_requested(
-          :put,
-          "https://s3.eu-west-2.amazonaws.com/test_bucket/test/open_data_export_sap-rdsap_2022-09-01_1.csv",
-        )
+        expect(Worker::OpenDataExportHelper).to have_received(:call_rake).with("SAP-RDSAP")
       end
     end
 
     context "when there is no data to send" do
       before do
+        allow(Worker::OpenDataExportHelper).to receive(:call_rake).and_raise(Boundary::OpenDataEmpty)
         allow(Sentry).to receive(:capture_exception)
       end
 
       it "send the error to sentry" do
         described_class.new.perform
-        expect(Sentry).to have_received(:capture_exception).with(Boundary::OpenDataEmpty).exactly(1).times
+        expect(Sentry).to have_received(:capture_exception).with(Boundary::OpenDataEmpty)
       end
     end
   end
--- spec/sidekiq/worker/open_data_export_helper_spec.rb
+++ spec/sidekiq/worker/open_data_export_helper_spec.rb
@@ -1,17 +1,76 @@
+require_relative "../../acceptance/reporting/open_data_export_test_helper"
+
 describe Worker::OpenDataExportHelper do
+  include RSpecRegisterApiServiceMixin
+
+  before do
+    Timecop.freeze(2022, 9, 1, 0, 0, 0)
+    WebMock.enable!
+  end
+
+  after do
+    Timecop.return
+    WebMock.disable!
+  end
+
   describe "#get_last_months_methods" do
-    before do
-      Timecop.freeze(2022, 9, 1, 0, 0, 0)
-    end
-
-    after do
-      Timecop.return
-    end
-
     it "returns the correct dates" do
       expect(described_class.get_last_months_dates).to be_a(Hash)
       expect(described_class.get_last_months_dates[:start_date]).to eq "2022-08-01"
       expect(described_class.get_last_months_dates[:end_date]).to eq "2022-09-01"
+    end
+  end
+
+  describe "#call_rake" do
+    before do
+      scheme_id = lodge_assessor
+      domestic_rdsap_xml =
+        get_assessment_xml(
+          "RdSAP-Schema-20.0.0",
+          "0000-0000-0000-0000-0004",
+          Date.yesterday.strftime("%Y-%m-%d"),
+        )
+      lodge_assessment(
+        assessment_body: domestic_rdsap_xml.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        migrated: true,
+      )
+
+      domestic_rdsap_xml =
+        get_assessment_xml(
+          "RdSAP-Schema-20.0.0",
+          "0000-0000-0000-0000-1004",
+          Date.yesterday.strftime("%Y-%m-%d"),
+        )
+      lodge_assessment(
+        assessment_body: domestic_rdsap_xml.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        migrated: true,
+      )
+
+      EnvironmentStub
+        .all
+
+      stub_request(:put, "https://s3.eu-west-2.amazonaws.com/test_bucket/test/open_data_export_sap-rdsap_2022-09-01_1.csv")
+        .to_return(status: 200, body: "", headers: {})
+    end
+
+    it "executes the rake which calls the use case" do
+      expect { described_class.call_rake("SAP-RDSAP") }.not_to raise_error
+    end
+
+    it "sends the file to the S3 bucket" do
+      described_class.call_rake("SAP-RDSAP")
+      expect(WebMock).to have_requested(
+        :put,
+        "https://s3.eu-west-2.amazonaws.com/test_bucket/test/open_data_export_sap-rdsap_2022-09-01_1.csv",
+      )
     end
   end
 end
