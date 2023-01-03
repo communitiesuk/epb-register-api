fe022cd4dd3d65e223e667cf6d31d4fc20107e3a
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2023-01-03 14:31:57 +0000

    Auto-updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_144140) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_194606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
