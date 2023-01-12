e48a4f662d9bccaa43614021c12eca89536cd304
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-01-12 15:50:31 +0000

    EPBR-2706 Remove statement_timout to register-api database as we don't have the permissions to apply it

--- db/migrate/20230111093615_set_statement_timeout.rb
+++ db/migrate/20230111093615_set_statement_timeout.rb
@@ -1,11 +1,5 @@
 class SetStatementTimeout < ActiveRecord::Migration[7.0]
     def up
-      database_name = connection.current_database
-      execute "ALTER DATABASE #{database_name} SET statement_timeout='10min';"
-    end
-
-    def down
-      database_name = connection.current_database
-      execute "ALTER DATABASE #{database_name} SET statement_timeout=0;"
+      # removed as the migration didn't work due to insufficient privileges on the PaaS database
     end
 end
