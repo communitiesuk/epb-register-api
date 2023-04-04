c1177c2cec90dbe729880654c6448dfd32a88ac8
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2023-04-04 12:30:22 +0100

    remove explicit chdir operation in sidekiq worker for updating address base as can error

--- sidekiq/worker/update_address_base.rb
+++ sidekiq/worker/update_address_base.rb
@@ -3,9 +3,7 @@ module Worker
     include Sidekiq::Worker
 
     def perform
-      Dir.chdir("#{__dir__}/../..") do
-        system("npm run update-address-base-auto")
-      end
+      system("npm run update-address-base-auto")
     end
   end
 end
