4d0d4ad96363ec3c22c9f849cb3b7f75b7ae2877
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-06-06 11:05:14 +0100

    EPBR-1863 - fix: added trigram extension to migration

--- db/migrate/20230605140351_add_trigram_index_assessments_towns.rb
+++ db/migrate/20230605140351_add_trigram_index_assessments_towns.rb
@@ -1,9 +1,11 @@
 class AddTrigramIndexAssessmentsTowns < ActiveRecord::Migration[7.0]
   def up
+    execute "CREATE EXTENSION IF NOT EXISTS pg_trgm"
     execute "CREATE INDEX index_towns_on_assessments_trigram ON assessments USING gist (town  gist_trgm_ops)"
   end
 
   def down
     execute "DROP INDEX IF EXISTS index_towns_on_assessments_trigram"
+    execute "DROP EXTENSION IF EXISTS pg_trgm"
   end
 end
