65dc34e3714ba39c5d203352f6a5b45327e39631
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-06-06 14:03:28 +0100

    EPBR-1863 - fix: update migration to deal with existence on integration

--- db/migrate/20230605140351_add_trigram_index_assessments_towns.rb
+++ db/migrate/20230605140351_add_trigram_index_assessments_towns.rb
@@ -1,9 +1,9 @@
 class AddTrigramIndexAssessmentsTowns < ActiveRecord::Migration[7.0]
   def up
-    execute "CREATE INDEX index_towns_on_assessments_trigram ON assessments USING gist (town  gist_trgm_ops)"
+    execute "CREATE INDEX IF NOT EXISTS index_towns_on_assessments_trigram ON assessments USING gist (town  gist_trgm_ops)"
   end
 
   def down
-    execute "DROP EXTENSION IF EXISTS pg_trgm"
+    execute "DROP INDEX IF EXISTS index_towns_on_assessments_trigram"
   end
 end
