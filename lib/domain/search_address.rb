dd1baffcaa68ef62bfcabdeb4bb2454a95099654
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-06-15 12:36:53 +0100

    created domain object

---
+++ lib/domain/search_address.rb
@@ -0,0 +1,16 @@
+module Domain
+  class SearchAddress
+    def initialize(record)
+      @assessment_id = record[:assessment_id]
+      @address = record[:address]
+
+    end
+
+    def to_hash
+      {
+        assessment_id: @assessment_id,
+        address: @address.slice(:address_line_1, :address_line_2, :address_line_3, :address_line_4).values.reject { |c| c.empty? }.join(' ').to_s.strip.downcase
+      }
+    end
+  end
+end
---
+++ spec/unit/domain/search_address_spec.rb
@@ -0,0 +1,47 @@
+describe Domain::SearchAddress do
+  subject(:object) do
+    described_class
+  end
+
+  let(:record){
+    {
+      assessment_id: "0000-0000-0000-0000-00001",
+      address: {
+        address_id: "UPRN-000000000123",
+        address_line_1: "22 Acacia Avenue",
+        address_line_2: "some place",
+        address_line_3: "",
+        address_line_4: "",
+        town: "Anytown",
+        postcode: "AB1 2CD",
+      },
+    }
+
+  }
+
+  context "when initializing the object" do
+    it "does not raise an error" do
+      expect{described_class.new record}.not_to raise_error
+    end
+
+    describe "#to_hash" do
+      it "returns a hash with the expected keys and values" do
+        search_address =  described_class.new(record).to_hash
+        expect(search_address[:assessment_id]).to eq "0000-0000-0000-0000-00001"
+        expect(search_address[:address]).to eq "22 acacia avenue some place"
+      end
+
+
+      it "returns an address without spaces" do
+        record[:address][:address_line_4] = 'another town'
+        search_address =  described_class.new(record).to_hash
+        expect(search_address[:assessment_id]).to eq "0000-0000-0000-0000-00001"
+        expect(search_address[:address]).to eq "22 acacia avenue some place another town"
+      end
+    end
+
+  end
+
+
+
+end
