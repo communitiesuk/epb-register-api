93bc1563b474f4b7a3901a6a0ba19395e177688d
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-06-19 12:14:14 +0100

    EPBR-1863 feat: Add search address gateway

--- lib/domain/search_address.rb
+++ lib/domain/search_address.rb
@@ -3,13 +3,12 @@ module Domain
     def initialize(record)
       @assessment_id = record[:assessment_id]
       @address = record[:address]
-
     end
 
     def to_hash
       {
         assessment_id: @assessment_id,
-        address: @address.slice(:address_line_1, :address_line_2, :address_line_3, :address_line_4).values.reject { |c| c.empty? }.join(' ').to_s.strip.downcase
+        address: @address.slice(:address_line_1, :address_line_2, :address_line_3, :address_line_4).values.reject { |c| c.empty? }.join(" ").to_s.strip.downcase,
       }
     end
   end
---
+++ lib/gateway/search_address_gateway.rb
@@ -0,0 +1,25 @@
+module Gateway
+  class SearchAddressGateway < StandardError
+    def insert(object)
+      insert_sql = <<-SQL
+            INSERT INTO assessment_search_address(assessment_id, address)
+            VALUES ($1, $2)
+      SQL
+
+      bindings = [
+        ActiveRecord::Relation::QueryAttribute.new(
+          "assessment_id",
+          object[:assessment_id],
+          ActiveRecord::Type::String.new,
+        ),
+        ActiveRecord::Relation::QueryAttribute.new(
+          "assessment_id",
+          object[:address],
+          ActiveRecord::Type::String.new,
+        ),
+      ]
+
+      ActiveRecord::Base.connection.exec_query(insert_sql, "SQL", bindings)
+    end
+  end
+end
