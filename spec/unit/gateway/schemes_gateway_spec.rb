33b8bfd3c5555c363ed31a31a3d0a44ed5574ccf
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-10-23 15:45:45 +0100

    EPBR-4025 updated schema test to use few transactions

--- spec/unit/gateway/schemes_gateway_spec.rb
+++ spec/unit/gateway/schemes_gateway_spec.rb
@@ -4,11 +4,10 @@ describe Gateway::SchemesGateway do
   describe "#fetch_active" do
     context "when there are active and inactive schemes" do
       before do
-        ActiveRecord::Base.connection.exec_query("INSERT INTO schemes (scheme_id) VALUES ('1')")
+        ActiveRecord::Base.connection.exec_query("INSERT INTO schemes (scheme_id, active) VALUES ('1',false)")
         ActiveRecord::Base.connection.exec_query("INSERT INTO schemes (scheme_id) VALUES ('2')")
         ActiveRecord::Base.connection.exec_query("INSERT INTO schemes (scheme_id) VALUES ('3')")
         ActiveRecord::Base.connection.exec_query("INSERT INTO schemes (scheme_id) VALUES ('4')")
-        ActiveRecord::Base.connection.exec_query("UPDATE schemes SET active=false WHERE scheme_id = '1'")
       end
 
       it "returns only active scheme_ids" do
