fe62d3ba162a93913715712c25d1a67d71c75ba3
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-01-22 09:25:06 +0000

    initial commit

--- spec/unit/use_case/process_postcode_csv_spec.rb
+++ spec/unit/use_case/process_postcode_csv_spec.rb
@@ -16,21 +16,44 @@ describe UseCase::ProcessPostcodeCsv do
       allow(gateway).to receive(:create_outcode_table)
       allow(gateway).to receive(:switch_postcode_table)
       allow(gateway).to receive(:switch_outcode_table)
+      allow(gateway).to receive(:insert_postcode_batch)
+      allow(gateway).to receive(:insert_outcodes)
     end
 
-    it "sends the file to the exec method" do
-      file = "spec/fixtures/postcodes_test.csv.zip"
-      file_io = File.open(file)
+    let(:expected_data){
+      ["'AB1 0AA', 57.101474, -2.242851, 'North East'", "'AB1 0AB', 57.102554, -2.246308, 'North West'", "'AB1 0AD', 57.100556, -2.248342, 'Eastern of England'"]
+    }
 
-      Zip::InputStream.open(file_io) do |csv_io|
-        while (entry = csv_io.get_next_entry)
-          next unless entry.size.positive?
+    let(:expected_outcodes){
+      {"AB1"=>{:latitude=>[57.101474, 57.102554, 57.100556], :longitude=>[-2.242851, -2.246308, -2.248342], :region=>["North East", "North West", "Eastern of England"]}}
+    }
 
-          puts "[#{Time.now}] #{entry.name} was unzipped with a size of #{entry.size} bytes"
-          postcode_csv = CSV.new(csv_io, headers: true)
-          expect { use_case.execute(postcode_csv) }.not_to raise_error
-        end
-      end
+
+    let(:file_io){
+      file = "spec/fixtures/postcodes_test.csv"
+      File.open(file)
+    }
+    let(:postcode_csv){
+      CSV.new(file_io, headers: true)
+    }
+
+    it "sends the file to the exec method from a zip" do
+      expect { use_case.execute(postcode_csv) }.not_to raise_error
     end
+
+    it "sends expected args to insert_postcode_batch" do
+       use_case.execute(postcode_csv)
+       expect(gateway).to have_received(:insert_postcode_batch).exactly(1).with(expected_data).times
+    end
+
+    it "sends expected args to insert_outcodes" do
+      use_case.execute(postcode_csv)
+      expect(gateway).to have_received(:insert_outcodes).exactly(1).with(expected_outcodes).times
+    end
+
+
+
   end
+
+
 end
