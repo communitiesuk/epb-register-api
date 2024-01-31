e801555c57df606bd146e6a2f5979d6a203021db
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-01-31 15:48:20 +0000

    fix: rake test to mock use case returning an int

--- spec/tasks/update_assessments_from_landmark_spec.rb
+++ spec/tasks/update_assessments_from_landmark_spec.rb
@@ -1,23 +1,26 @@
-describe "Update asssessments from landmark rake" do
+describe "Update assessments from landmark rake" do
   include RSpecRegisterApiServiceMixin
   let(:described_class) { get_task("oneoff:update_assessments_from_landmark") }
   let(:file_name) { "landmark_non_domestic_epcs_dates.csv" }
 
-  let(:data) do
-    file = "spec/fixtures/landmark_dates.csv"
-    File.open(file)
+
+  let(:storage_gateway) { instance_double(Gateway::StorageGateway) }
+  let(:use_case) do
+    instance_double(UseCase::UpdateAssessmentsFromLandmark)
   end
 
   before do
-    WebMock.enable!
-    WebMock.stub_request(:get, "https://slack.com/api/files.upload").to_return(status: 200, headers: {}, body: { ok: true }.to_json)
     allow($stdout).to receive(:puts)
     EnvironmentStub
       .all
       .with("LANDMARK_BUCKET", "test-bucket")
       .with("FILE_NAME", file_name)
 
-    HttpStub.s3_get_object(file_name, data)
+    allow(ApiFactory).to receive(:update_assessments_from_landmark).and_return(
+      use_case,
+      )
+    allow(ApiFactory).to receive(:storage_gateway).and_return(storage_gateway)
+    allow(use_case).to receive(:execute).and_return(5)
   end
 
   it "execs the rake" do
@@ -25,6 +28,8 @@ describe "Update asssessments from landmark rake" do
   end
 
   it "prints the number of updated epcs" do
-    expect { described_class.invoke }.not_to raise_error
+    expect { described_class.invoke }.to output(
+      /5 assessments have been updated/,
+      ).to_stdout
   end
 end
