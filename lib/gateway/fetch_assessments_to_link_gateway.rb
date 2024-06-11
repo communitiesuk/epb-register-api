637ca6decc5aef79a512f370690c644bc553d47c
Alice Lui <54395483+al01010@users.noreply.github.com>
2024-06-11 14:58:16 +0100

    EPBR-5891: Add gateway to fetch non-dom assessments with identical addresses but different address_ids

---
+++ lib/gateway/fetch_assessments_to_link_gateway.rb
@@ -0,0 +1,36 @@
+module Gateway
+  class FetchAssessmentsToLinkGateway
+    def fetch_assessments
+      sql = <<-SQL
+        WITH addresses_we_want AS (
+        WITH grouped_addresses AS (
+        SELECT address, postcode, count(*) as ct, count (DISTINCT(aaid.address_id)) as cid
+        FROM (assessments a LEFT JOIN assessment_search_address asa ON a.assessment_id = asa.assessment_id)
+        LEFT JOIN assessments_address_id aaid ON (a.assessment_id = aaid.assessment_id AND asa.assessment_id = aaid.assessment_id)
+        WHERE a.type_of_assessment IN ('CEPC', 'CEPC-RR', 'AC-CERT', 'AC-REPORT', 'DEC', 'DEC-RR')
+        AND a.cancelled_at IS NULL
+        AND a.not_for_issue_at IS NULL
+        GROUP BY address, postcode)
+        SELECT address, postcode
+        FROM grouped_addresses
+        WHERE ct > 1 AND cid >1),
+        non_domestic_certs AS (SELECT a.assessment_id,
+                                  asa.address,
+                                  a.date_registered,
+                                  a.postcode,
+                                  aaid.address_id
+                            FROM assessments a
+                                     JOIN assessments_address_id aaid ON a.assessment_id = aaid.assessment_id
+                                     JOIN assessment_search_address asa ON a.assessment_id = asa.assessment_id
+                            WHERE a.type_of_assessment IN ('CEPC', 'CEPC-RR', 'AC-CERT', 'AC-REPORT', 'DEC', 'DEC-RR')
+                            AND a.cancelled_at IS NULL
+                            AND a.not_for_issue_at IS NULL)
+        SELECT aww.address, aww.postcode, assessment_id, address_id, date_registered
+        FROM addresses_we_want aww LEFT JOIN non_domestic_certs ndc ON (aww.address = ndc.address AND aww.postcode = ndc.postcode)
+        ORDER BY aww.address, aww.postcode;
+      SQL
+
+      ActiveRecord::Base.connection.exec_query(sql, "SQL")
+    end
+  end
+end
---
+++ spec/unit/gateway/fetch_assessments_to_link_gateway_spec.rb
@@ -0,0 +1,132 @@
+describe Gateway::FetchAssessmentsToLinkGateway do
+  include RSpecRegisterApiServiceMixin
+
+  let(:gateway) { described_class.new }
+
+  describe "#fetch_assessments" do
+    before do
+      insert_into_address_base("000000000001", "A0 0AA", "1 Commercial Street", "", "", "E")
+      insert_into_address_base("000000000012", "A0 0AA", "4 Commercial Street", "", "", "E")
+      insert_into_address_base("000000000016", "A0 0AA", "Some Unit", "", "", "E")
+
+      scheme_id = add_scheme_and_get_id
+      add_super_assessor(scheme_id:)
+
+      cepc_xml_1 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc+rr")
+      cepc_xml_1.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "1 Commercial Street" }
+      lodge_assessment(
+        assessment_body: cepc_xml_1.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+      cepc_xml_2 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
+      cepc_xml_2.at("//CEPC:RRN").content = "0000-0000-0000-0000-0002"
+      cepc_xml_2.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "1 Commercial Street" }
+      cepc_xml_2.at("//CEPC:UPRN").content = "RRN-0000-0000-0000-0000-0002"
+      lodge_assessment(
+        assessment_body: cepc_xml_2.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+      cepc_xml_3 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
+      cepc_xml_3.at("//CEPC:RRN").content = "0000-0000-0000-0000-0003"
+      cepc_xml_3.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "14 Commercial Street" }
+      cepc_xml_3.at("//CEPC:UPRN").content = "RRN-0000-0000-0000-0000-0003"
+      lodge_assessment(
+        assessment_body: cepc_xml_3.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+      cepc_xml_4 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
+      cepc_xml_4.at("//CEPC:RRN").content = "0000-0000-0000-0000-0004"
+      cepc_xml_4.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "14 Commercial Street" }
+      cepc_xml_4.at("//CEPC:UPRN").content = "RRN-0000-0000-0000-0000-0004"
+      lodge_assessment(
+        assessment_body: cepc_xml_4.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+      cepc_xml_5 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
+      cepc_xml_5.at("//CEPC:RRN").content = "0000-0000-0000-0000-0005"
+      cepc_xml_5.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "4 Commercial Street" }
+      cepc_xml_5.at("//CEPC:UPRN").content = "UPRN-000000000012"
+      lodge_assessment(
+        assessment_body: cepc_xml_5.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+      cepc_xml_6 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
+      cepc_xml_6.at("//CEPC:RRN").content = "0000-0000-0000-0000-0006"
+      cepc_xml_6.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "4 Commercial Street" }
+      cepc_xml_6.at("//CEPC:UPRN").content = "UPRN-000000000012"
+      lodge_assessment(
+        assessment_body: cepc_xml_6.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+      dec_plus_rr_xml = Nokogiri.XML Samples.xml("CEPC-8.0.0", "dec+rr")
+      dec_plus_rr_xml
+        .xpath("//*[local-name() = 'UPRN']")
+        .each do |node|
+        node.content = "UPRN-000000000016"
+      end
+      dec_plus_rr_xml
+        .xpath("//*[local-name() = 'RRN']")
+        .each_with_index do |node, index|
+        node.content = "1111-0000-0000-0000-000#{index}"
+      end
+      dec_plus_rr_xml
+        .xpath("//*[local-name() = 'Related-RRN']")
+        .reverse
+        .each_with_index do |node, index|
+        node.content = "1111-0000-0000-0000-000#{index}"
+      end
+
+      lodge_assessment(
+        assessment_body: dec_plus_rr_xml.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        schema_name: "CEPC-8.0.0",
+        migrated: true,
+      )
+    end
+
+    it "fetches all assessments with the same address name" do
+      result = gateway.fetch_assessments
+      expect(result.count).to eq 5
+      expect(result.columns).to eq %w[address postcode assessment_id address_id date_registered]
+      expect(result.rows.to_s).to include("0000-0000-0000-0000-0000", "0000-0000-0000-0000-0001", "0000-0000-0000-0000-0002", "0000-0000-0000-0000-0003", "0000-0000-0000-0000-0004")
+    end
+
+    it "does not fetch correctly linked assessments" do
+      result = gateway.fetch_assessments
+      expect(result.rows.to_s).not_to include("0000-0000-0000-0000-0005", "0000-0000-0000-0000-0006", "1111-0000-0000-0000-0000", "1111-0000-0000-0000-0001")
+    end
+  end
+end
