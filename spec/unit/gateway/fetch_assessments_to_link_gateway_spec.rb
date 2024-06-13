a25fd95be75b66742503f90fcfd9f9f7eca3b621
Alice Lui <54395483+al01010@users.noreply.github.com>
2024-06-13 12:20:58 +0100

    EPBR-5891: Adjust tests in fetch assessments to link gateway to fix broken pipeline

--- spec/unit/gateway/fetch_assessments_to_link_gateway_spec.rb
+++ spec/unit/gateway/fetch_assessments_to_link_gateway_spec.rb
@@ -33,6 +33,7 @@ describe Gateway::FetchAssessmentsToLinkGateway do
       schema_name: "CEPC-8.0.0",
       migrated: true,
     )
+    # fetches with additional punctuation
     ac_cert_xml_1 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "ac-cert")
     ac_cert_xml_1.at("RRN").content = "0000-0000-0000-0000-0012"
     ac_cert_xml_1.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "1, Commercial Street" }
@@ -72,6 +73,7 @@ describe Gateway::FetchAssessmentsToLinkGateway do
       schema_name: "CEPC-8.0.0",
       migrated: true,
     )
+    # does not fetch correctly linked assessments
     cepc_xml_5 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
     cepc_xml_5.at("//CEPC:RRN").content = "0000-0000-0000-0000-0005"
     cepc_xml_5.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "4 Commercial Street" }
@@ -87,7 +89,7 @@ describe Gateway::FetchAssessmentsToLinkGateway do
     )
     cepc_xml_6 = Nokogiri.XML Samples.xml("CEPC-8.0.0", "cepc")
     cepc_xml_6.at("//CEPC:RRN").content = "0000-0000-0000-0000-0006"
-    cepc_xml_6.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "4 Commercial Street" }
+    cepc_xml_6.xpath("//*[local-name() = 'Address-Line-1']").each { |node| node.content = "4, Commercial Street" }
     cepc_xml_6.at("//CEPC:UPRN").content = "UPRN-000000000012"
     lodge_assessment(
       assessment_body: cepc_xml_6.to_xml,
@@ -98,6 +100,7 @@ describe Gateway::FetchAssessmentsToLinkGateway do
       schema_name: "CEPC-8.0.0",
       migrated: true,
     )
+    # does not fetch correctly linked assessments
     dec_plus_rr_xml = Nokogiri.XML Samples.xml("CEPC-8.0.0", "dec+rr")
     dec_plus_rr_xml
       .xpath("//*[local-name() = 'UPRN']")
@@ -115,7 +118,6 @@ describe Gateway::FetchAssessmentsToLinkGateway do
       .each_with_index do |node, index|
       node.content = "1111-0000-0000-0000-000#{index}"
     end
-
     lodge_assessment(
       assessment_body: dec_plus_rr_xml.to_xml,
       accepted_responses: [201],
@@ -130,27 +132,27 @@ describe Gateway::FetchAssessmentsToLinkGateway do
   end
 
   describe "#create_and_populate_temp_table" do
-    it "creates a temporary table with the expected columns" do
+    it "creates a temporary table with the expected columns", aggregate_failures: true  do
       expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.table_exists?).to be(true)
       expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.column_names).to eq(%w[address postcode assessment_id address_id date_registered group_id])
     end
 
-    it "populates the temporary table with assessments with the same address name, including those that vary only by punctuation" do
+    it "populates the temporary table with assessments with the same address name, including those that vary only by punctuation", aggregate_failures: true do
       expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.count).to eq(6)
-      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.pluck(:assessment_id)).to eq(%w[0000-0000-0000-0000-0000 0000-0000-0000-0000-0001 0000-0000-0000-0000-0002 0000-0000-0000-0000-0012 0000-0000-0000-0000-0003 0000-0000-0000-0000-0004])
+      data = Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.pluck(:assessment_id).sort
+      expect(data).to eq(%w[0000-0000-0000-0000-0000 0000-0000-0000-0000-0001 0000-0000-0000-0000-0002 0000-0000-0000-0000-0003 0000-0000-0000-0000-0004 0000-0000-0000-0000-0012])
     end
 
     it "does not fetch correctly linked assessments" do
       expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.pluck(:assessment_id)).not_to include("0000-0000-0000-0000-0005", "0000-0000-0000-0000-0006", "1111-0000-0000-0000-0000", "1111-0000-0000-0000-0001")
     end
 
-    it "assigns an id for each group of matching addresses" do
-      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(group_id: 1).pluck(:address).count).to eq(4)
-      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(group_id: 1).pluck(:address).uniq).to eq(["1 commercial street 2 lonely street some area some county"])
-      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(group_id: 1).pluck(:address_id)).to eq(%w[UPRN-000000000001 UPRN-000000000001 RRN-0000-0000-0000-0000-0002 RRN-0000-0000-0000-0000-0012])
+    it "assign a group_id to assessments with matching addresses and postcodes", aggregate_failures: true do
+      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(address: "1 commercial street 2 lonely street some area some county", postcode: "A0 0AA").pluck(:group_id).uniq.length).to eq 1
+      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(address: "1 commercial street 2 lonely street some area some county", postcode: "A0 0AA").pluck(:group_id).length).to eq 4
 
-      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(group_id: 2).pluck(:address).count).to eq(2)
-      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(group_id: 2).pluck(:address).uniq).to eq(["14 commercial street 2 lonely street some area some county"])
+      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(address: "14 commercial street 2 lonely street some area some county", postcode: "A0 0AA").pluck(:group_id).uniq.length).to eq 1
+      expect(Gateway::FetchAssessmentsToLinkGateway::TempLinkingTable.where(address: "14 commercial street 2 lonely street some area some county", postcode: "A0 0AA").pluck(:group_id).length).to eq 2
     end
   end
 end
