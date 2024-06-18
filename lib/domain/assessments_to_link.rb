ee694f4114cef0252927639c6d34e6d38c49f3ad
Alice Lui <54395483+al01010@users.noreply.github.com>
2024-06-18 12:06:39 +0100

    EPBR-5891: Add domain to select the best address_id for linking assessments

---
+++ lib/domain/assessments_to_link.rb
@@ -0,0 +1,26 @@
+module Domain
+  class AssessmentsToLink
+    def initialize(data:)
+      @data = data
+      sort_by_date
+    end
+
+    def sort_by_date
+      @data.sort_by! { |assessment| assessment["date_registered"] }
+    end
+
+    def set_best_address_id(address_base_gateway:)
+      find_uprn = @data.find { |assessment| assessment["address_id"].start_with?("UPRN") }
+      if find_uprn.nil?
+        @best_address_id = "RRN-#{@data.first['assessment_id']}"
+      else
+        @best_address_id = find_uprn["address_id"]
+        unless address_base_gateway.check_uprn_exists(@best_address_id)
+          @best_address_id = "RRN-#{@data.first['assessment_id']}"
+        end
+      end
+    end
+
+    attr_reader :best_address_id
+  end
+end
---
+++ spec/unit/domain/assessments_to_link_spec.rb
@@ -0,0 +1,73 @@
+describe Domain::AssessmentsToLink do
+  subject(:domain) do
+    described_class.new(data:)
+  end
+
+  let(:data) do
+    [
+      { "assessment_id" => "0000-0000-0000-0000-0012", "address_id" => "RRN-0000-0000-0000-0000-0012", "date_registered" => Time.utc(2020, 0o5, 20) },
+      { "assessment_id" => "0000-0000-0000-0000-0000", "address_id" => "UPRN-000000000001", "date_registered" => Time.utc(2020, 0o5, 0o5) },
+      { "assessment_id" => "0000-0000-0000-0000-0001", "address_id" => "UPRN-000000000001", "date_registered" => Time.utc(2020, 0o5, 0o5) },
+      { "assessment_id" => "0000-0000-0000-0000-0002", "address_id" => "RRN-0000-0000-0000-0000-0002", "date_registered" => Time.utc(2020, 0o5, 0o4) },
+      { "assessment_id" => "0000-0000-0000-0000-0007", "address_id" => "RRN-0000-0000-0000-0000-0002", "date_registered" => Time.utc(2020, 0o5, 0o4) },
+    ]
+  end
+
+  it "test" do
+    expect { domain }.not_to raise_error
+  end
+
+  describe "#sort_by_date" do
+    it "orders the data by date registered" do
+      expect(domain.sort_by_date.last["assessment_id"]).to eq "0000-0000-0000-0000-0012"
+    end
+  end
+
+  describe "#set_best_address_id" do
+    let(:address_base_gateway) { instance_double Gateway::AddressBaseSearchGateway }
+
+    before do
+      allow(address_base_gateway).to receive(:check_uprn_exists)
+    end
+
+    context "when a valid UPRN is in the list of address_ids" do
+      it "selects the UPRN as the best address id" do
+        expect { domain.set_best_address_id(address_base_gateway:) }.not_to raise_error
+      end
+
+      it "passes the correct UPRN to the gateway" do
+        domain.set_best_address_id(address_base_gateway:)
+        expect(address_base_gateway).to have_received(:check_uprn_exists).with("UPRN-000000000001").exactly(1).times
+      end
+    end
+
+    context "when there is an invalid UPRN" do
+      before do
+        allow(address_base_gateway).to receive(:check_uprn_exists).and_return false
+        domain.set_best_address_id(address_base_gateway:)
+      end
+
+      it "the will select the RRN associated with the oldest assessment as the address_id" do
+        expect(domain.best_address_id).to eq("RRN-0000-0000-0000-0000-0002")
+      end
+    end
+
+    context "when there are no UPRN's to select from" do
+      let(:data) do
+        [
+          { "assessment_id" => "0000-0000-0000-0000-0012", "address_id" => "RRN-0000-0000-0000-0000-0012", "date_registered" => Time.utc(2020, 0o5, 20) },
+          { "assessment_id" => "0000-0000-0000-0000-0002", "address_id" => "RRN-0000-0000-0000-0000-0002", "date_registered" => Time.utc(2020, 0o5, 0o4) },
+          { "assessment_id" => "0000-0000-0000-0000-0007", "address_id" => "RRN-0000-0000-0000-0000-0002", "date_registered" => Time.utc(2020, 0o5, 0o4) },
+        ]
+      end
+
+      before do
+        domain.set_best_address_id(address_base_gateway:)
+      end
+
+      it "the will select the RRN associated with the oldest assessment as the address_id" do
+        expect(domain.best_address_id).to eq("RRN-0000-0000-0000-0000-0002")
+      end
+    end
+  end
+end
