773d7cdf826860b0f564f799fe6b281630c335f9
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-09-03 10:19:55 +0100

    EPBR-6728 Adds a slack gateway to post files
    Moved the code to post files to slack from a Helper to a Gateway.
    Implements latest version of the Slack APi via the slack ruby gem.

---
+++ lib/gateway/slack_gateway.rb
@@ -0,0 +1,54 @@
+require "slack"
+
+module Gateway
+  class SlackGateway
+    def initialize(slack_web_client:)
+      Slack.configure do |config|
+        config.token = ENV["SLACK_EPB_BOT_TOKEN"]
+      end
+      @client = slack_web_client || Slack::Web::Client.new
+    end
+
+    def upload_file(file_path:, message:)
+      filename = File.basename(file_path)
+      channel_id = ENV["STAGE"] == "production" ? "CJ9DGBZ6C" : "C02LETGH0CR"
+      upload_response = @client.files_getUploadURLExternal(filename:, length: File.size(file_path))
+      post_file(url: upload_response["upload_url"], file_path:, filename:)
+      files = [{ id: upload_response["file_id"], title: message }].to_json
+      @client.files_completeUploadExternal(files:, channel_id:, initial_comment: message)
+    rescue Boundary::SlackMessageError => e
+      raise e
+    end
+
+    def post_file(url:, file_path:, filename:)
+      uri = URI(url)
+      req = Net::HTTP::Post.new(uri)
+
+      form = [
+        [
+          "file",
+          File.open(file_path),
+        ],
+        [
+          "filename",
+          filename,
+        ],
+      ]
+      req.set_form(
+        form,
+        "multipart/form-data",
+      )
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(req)
+      end
+
+      unless response.is_a?(Net::HTTPSuccess) && JSON.parse(response.body)["ok"] == true
+        raise Boundary::SlackMessageError, "Slack error: #{response.body}"
+      end
+    end
+  end
+end
---
+++ spec/unit/gateway/slack_gateway_spec.rb
@@ -0,0 +1,63 @@
+describe Gateway::SlackGateway, :set_with_timecop do
+  subject(:gateway) { described_class.new(slack_web_client:) }
+
+  let(:slack_web_client) { instance_double(Slack::Web::Client) }
+  let(:file_path) { File.join Dir.pwd, "spec/fixtures/postcodes_test.csv" }
+  let(:slack_external_url_body) { { "upload_url" => "https://files.slack.com/upload/v1/Ad4FoJN1mC67CgACFF", "file_id" => "F07KB41F4P7", "ok" => true } }
+
+  before do
+    EnvironmentStub.with("SLACK_EPB_BOT_TOKEN", "12345")
+    HttpStub.enable_webmock
+    WebMock.stub_request(:post, slack_external_url_body["upload_url"]).to_return(status: 200, headers: {}, body: { ok: true }.to_json)
+    allow(slack_web_client).to receive_messages(files_getUploadURLExternal: slack_external_url_body, files_completeUploadExternal: true)
+  end
+
+  after do
+    EnvironmentStub.remove(%w[SLACK_EPB_BOT_TOKEN])
+    WebMock.disable!
+  end
+
+  context "when the class in initialised" do
+    it "does not raise an error" do
+      expect { gateway }.not_to raise_error
+    end
+
+    describe "#upload_file" do
+      before do
+        gateway.upload_file(file_path:, message: "this is a test message")
+      end
+
+      it "posts to the Slack files.getUploadURLExternal with correct file" do
+        expect(slack_web_client).to have_received(:files_getUploadURLExternal).with(filename: "postcodes_test.csv", length: 5470)
+      end
+
+      it "posts to the Slack files_completeUploadExternal with the correct info" do
+        files = [{ id: slack_external_url_body["file_id"], title: "this is a test message" }].to_json
+        expect(slack_web_client).to have_received(:files_completeUploadExternal).with(files:, channel_id: "C02LETGH0CR", initial_comment: "this is a test message")
+      end
+    end
+
+    describe "#post_file" do
+      before do
+        gateway.post_file(url: slack_external_url_body["upload_url"], file_path:, filename: "postcodes_test.csv")
+      end
+
+      it "posts to the file the slack api" do
+        expect(WebMock).to have_requested(:post, slack_external_url_body["upload_url"]).with(headers: { "Content-Type" => %r{multipart/form-data} })
+      end
+
+      context "when the upload fails" do
+        it "raises an SlackMessageError error with a 200 where body: ok" do
+          WebMock.stub_request(:post, slack_external_url_body["upload_url"]).to_return(status: 200, headers: {}, body: { ok: false }.to_json)
+          expect { gateway.post_file(url: slack_external_url_body["upload_url"], file_path:, filename: "postcodes_test.csv") }.to raise_error Boundary::SlackMessageError
+        end
+
+        it "raises a slack error message status 500" do
+          WebMock.stub_request(:post, slack_external_url_body["upload_url"]).to_return(status: 500, headers: {})
+
+          expect { gateway.post_file(url: slack_external_url_body["upload_url"], file_path:, filename: "postcodes_test.csv") }.to raise_error Boundary::SlackMessageError
+        end
+      end
+    end
+  end
+end
