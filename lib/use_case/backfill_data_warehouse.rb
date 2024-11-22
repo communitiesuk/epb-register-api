dea999b8b7877462b8597c1daa2a7462930a05dc
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-11-22 15:25:37 +0000

    removes log lines from backfill use case

--- lib/use_case/backfill_data_warehouse.rb
+++ lib/use_case/backfill_data_warehouse.rb
@@ -12,12 +12,9 @@ module UseCase
       raise Boundary::NoData, "No assessments to export" if rrn_array.count.zero?
 
       puts "Exporting #{rrn_array.count} assessments out to the data warehouse queue..."
-      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
       rrn_array.map { |assessment_ids| assessment_ids }.each_slice(500) do |assessment_ids|
         @data_warehouse_queues_gateway.push_to_queue(:assessments, assessment_ids)
-
-        seconds_elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start_time
-        puts "Pushed #{rrn_array} assessments out to the data warehouse queue in #{seconds_elapsed}s."
       end
     end
   end
