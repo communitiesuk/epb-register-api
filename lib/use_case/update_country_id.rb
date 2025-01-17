14f8a58727273e91ca1b5d03faddfe2294e221b7
Alice Lui <54395483+al01010@users.noreply.github.com>
2025-01-17 16:17:43 +0000

    EPBR-8081: Add use case to update the country id of assessments

---
+++ lib/use_case/update_country_id.rb
@@ -0,0 +1,30 @@
+module UseCase
+  class UpdateCountryId
+    def initialize(assessments_gateway:, country_use_case:, add_country_id_from_address:, assessments_country_id_gateway:)
+      @assessments_gateway = assessments_gateway
+      @country_use_case = country_use_case
+      @add_country_id_from_address = add_country_id_from_address
+      @assessments_country_id_gateway = assessments_country_id_gateway
+    end
+
+    def execute(assessments_ids:)
+      assessments_ids.each do |assessment_id|
+        assessment_location = @assessments_gateway.fetch_location_by_assessment_id(assessment_id)
+        postcode = assessment_location["postcode"]
+        address_id = assessment_location["address_id"]
+        xml = assessment_location["xml"]
+        schema_type = assessment_location["schema_type"]
+        country_lookup = @country_use_case.execute(rrn: assessment_id, postcode:, address_id:)
+        begin
+          lodgement_domain = Domain::Lodgement.new(xml, schema_type)
+        rescue NoMethodError => e
+          puts "rrn: #{assessment_id} for #{e.message}"
+          next
+        end
+        @add_country_id_from_address.execute(country_domain: country_lookup, lodgement_domain:)
+        country_id = lodgement_domain.fetch_country_id
+        @assessments_country_id_gateway.insert(assessment_id:, country_id:, upsert: true)
+      end
+    end
+  end
+end
---
+++ spec/unit/use_case/update_country_id_spec.rb
@@ -0,0 +1,117 @@
+shared_context "when extracting the country" do
+  def get_country_for_assessment(assessment_id:)
+    ActiveRecord::Base.connection.exec_query("SELECT country_name FROM assessments_country_ids a join countries using(country_id) WHERE assessment_id='#{assessment_id}' ").map { |rows| rows["country_name"] }.first
+  end
+end
+
+describe UseCase::UpdateCountryId, :set_with_timecop do
+  include RSpecRegisterApiServiceMixin
+  subject(:use_case) do
+    described_class.new(
+      assessments_gateway: Gateway::AssessmentsGateway.new,
+      country_use_case: ApiFactory.get_country_for_candidate_backfill_use_case,
+      add_country_id_from_address: add_country_use_case,
+      assessments_country_id_gateway: Gateway::AssessmentsCountryIdGateway.new,
+    )
+  end
+
+  include_context "when extracting the country"
+
+  let!(:add_country_use_case) do
+    UseCase::AddCountryIdFromAddress.new(Gateway::CountryGateway.new)
+  end
+
+  let(:assessments_ids) { %w[0000-0000-0000-0000-0001 0000-0000-0000-0000-0000] }
+
+  before(:all) do
+    add_countries
+    insert_wales = <<-SQL
+            INSERT INTO countries(country_code, country_name, address_base_country_code)
+            VALUES ('WLS', 'Wales' ,'["W"]'::jsonb)
+    SQL
+    ActiveRecord::Base.connection.exec_query(insert_wales, "SQL")
+    add_address_base uprn: "100020003000", postcode: "NP16 5UB", country_code: "W"
+    add_address_base uprn: "100020004000", postcode: "SW1 0AA", country_code: "E"
+
+    assessments = [
+      {
+        rrn: "0000-0000-0000-0000-0001",
+        schema_type: "SAP-Schema-18.0.0",
+        uprn: "RRN-0000-0000-0000-0000-0001",
+        country_code: "WLS",
+        postcode: "NP16 5UB",
+      },
+      {
+        rrn: "0000-0000-0000-0000-0000",
+        schema_type: "SAP-Schema-18.0.0",
+        uprn: "RRN-0000-0000-0000-0000-0000",
+        country_code: "ENG",
+        postcode: "SW1 0AA",
+      },
+    ]
+    scheme_id = add_scheme_and_get_id
+    add_super_assessor(scheme_id:)
+    assessments.each do |assessment|
+      xml = assessment[:type].nil? ? (Nokogiri.XML Samples.xml assessment[:schema_type]) : (Nokogiri.XML Samples.xml assessment[:schema_type], assessment[:type])
+      xml.at("//*[local-name() = 'RRN']").content = assessment[:rrn]
+      xml.at("//*[local-name() = 'UPRN']").content = assessment[:uprn] if assessment[:uprn]
+      xml.at("Registration-Date").content = assessment[:registered_date] if assessment[:registered_date]
+      xml.at("//*[local-name() = 'Country-Code']").content = assessment[:country_code] if assessment[:country_code]
+      lodge_assessment(
+        assessment_body: xml.to_xml,
+        accepted_responses: [201],
+        auth_data: {
+          scheme_ids: [scheme_id],
+        },
+        migrated: true,
+        schema_name: assessment[:schema_type],
+      )
+      ActiveRecord::Base.connection.exec_query("UPDATE assessments SET postcode = '#{assessment[:postcode]}' WHERE assessment_id='#{assessment[:rrn]}'") if assessment[:postcode]
+    end
+  end
+
+  describe "#execute" do
+    it "does not return an error" do
+      expect { use_case.execute(assessments_ids:) }.not_to raise_error
+    end
+
+    context "when a SAP 18.0.0 assessment with WLS as the XML country code is updated from EAW" do
+      let(:assessment_id) { "0000-0000-0000-0000-0001" }
+
+      before do
+        eaw_country_id = ActiveRecord::Base.connection.exec_query("SELECT country_id from countries WHERE country_code = 'EAW'").map { |rows| rows["country_id"] }.first
+        update_eaw = <<-SQL
+          UPDATE assessments_country_ids
+          SET assessment_id = '#{assessment_id}', country_id = #{eaw_country_id}
+          WHERE assessment_id = '#{assessment_id}'
+        SQL
+        ActiveRecord::Base.connection.exec_query(update_eaw, "SQL")
+      end
+
+
+      it "updates to Wales" do
+        use_case.execute(assessments_ids:)
+        expect(get_country_for_assessment(assessment_id:)).to eq "Wales"
+      end
+    end
+
+    context "when a SAP 18.0.0 assessment with ENG as the XML country code is updated from UKN" do
+      let(:assessment_id) { "0000-0000-0000-0000-0000" }
+
+      before do
+        ukn_country_id = ActiveRecord::Base.connection.exec_query("SELECT country_id from countries WHERE country_code = 'UKN'").map { |rows| rows["country_id"] }.first
+        update_ukn = <<-SQL
+          UPDATE assessments_country_ids
+          SET assessment_id = '#{assessment_id}', country_id = #{ukn_country_id}
+          WHERE assessment_id = '#{assessment_id}'
+        SQL
+        ActiveRecord::Base.connection.exec_query(update_ukn, "SQL")
+      end
+
+      it "updates to England" do
+        use_case.execute(assessments_ids:)
+        expect(get_country_for_assessment(assessment_id:)).to eq "England"
+      end
+    end
+  end
+end
