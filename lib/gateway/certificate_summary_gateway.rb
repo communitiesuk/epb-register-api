3a480a396ff06d3507c7b40849a96c69912157f0
christinehorrocks <christine.horrocks@communities.gov.uk>
2025-02-25 15:49:22 +0000

    EPBR-8573 Create certificate summary gateway and spec

---
+++ lib/gateway/certificate_summary_gateway.rb
@@ -0,0 +1,43 @@
+module Gateway
+  class CertificateSummaryGateway
+    def fetch(assessment_id)
+      sql = <<-SQL
+      SELECT
+      a.created_at,
+      a.opt_out,
+      a.cancelled_at,
+      a.not_for_issue_at,
+      aai.address_id AS assessment_address_id,
+      ac.country_id,
+      a.scheme_assessor_id,
+      ass.first_name AS assessor_first_name,
+      ass.last_name AS assessor_last_name,
+      ass.telephone_number AS assessor_telephone_number,
+      ass.email AS assessor_email,
+      ass.registered_by AS scheme_id,
+      s.name AS scheme_name,
+      x.schema_type,
+      x.xml,
+      gda.green_deal_plan_id
+      FROM assessments a
+      LEFT OUTER JOIN green_deal_assessments gda ON a.assessment_id = gda.assessment_id
+      INNER JOIN assessors ass ON a.scheme_assessor_id = ass.scheme_assessor_id
+      INNER JOIN schemes s ON ass.registered_by = s.scheme_id
+      INNER JOIN assessments_xml x ON a.assessment_id = x.assessment_id
+      INNER JOIN assessments_address_id aai ON a.assessment_id = aai.assessment_id
+      LEFT JOIN assessments_country_ids ac ON a.assessment_id = ac.assessment_id
+      WHERE a.assessment_id = $1
+      SQL
+
+      bindings = [
+        ActiveRecord::Relation::QueryAttribute.new(
+          "assessment_id",
+          assessment_id,
+          ActiveRecord::Type::String.new,
+          ),
+      ]
+
+      ActiveRecord::Base.connection.exec_query(sql, "SQL", bindings).first
+    end
+  end
+end
---
+++ spec/unit/gateway/certificate_summary_gateway_spec.rb
@@ -0,0 +1,44 @@
+describe Gateway::CertificateSummaryGateway do
+  include RSpecRegisterApiServiceMixin
+
+  subject(:gateway) { described_class.new }
+
+  let(:scheme_id) { add_scheme_and_get_id }
+
+  before do
+    add_countries
+    add_super_assessor(scheme_id:)
+    domestic_rdsap_xml = Nokogiri.XML Samples.xml("RdSAP-Schema-20.0.0")
+    lodge_assessment(
+      assessment_body: domestic_rdsap_xml.to_xml,
+      accepted_responses: [201],
+      auth_data: {
+        scheme_ids: [scheme_id],
+      },
+      migrated: true,
+      )
+    load_green_deal_data
+  end
+
+  describe "#fetch" do
+    it "returns the expected data" do
+      result = gateway.fetch("0000-0000-0000-0000-0000")
+      pp result
+      expect(result.count).to eq(1)
+    end
+  end
+
+  describe "#fetch" do
+    it "returns the expected data where a green deal is present" do
+      add_assessment_with_green_deal(
+        type: "RdSAP",
+        assessment_id: "0000-0000-0000-0000-1111",
+        registration_date: "2024-10-10",
+        green_deal_plan_id: "ABC654321DEF",
+        )
+      result = gateway.fetch("0000-0000-0000-0000-1111")
+      pp result
+      expect(result.count).to eq(1)
+    end
+  end
+end
