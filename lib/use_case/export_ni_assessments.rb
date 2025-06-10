137b1a1968c528da2f403a62d4de64e2be0e0dac
christinehorrocks <christine.horrocks@communities.gov.uk>
2025-06-10 13:35:10 +0100

    EPBR-5008 Correct type error

--- lib/use_case/export_ni_assessments.rb
+++ lib/use_case/export_ni_assessments.rb
@@ -8,27 +8,27 @@ module UseCase
     def execute(type_of_assessment:, date_from: "1990-01-01", date_to: Time.now)
       assessments_array = []
 
-      if type_of_assessment =="SAP-RDSAP-RR"
+      if type_of_assessment == ["{RdSAP", "SAP", "RR}"]
         use_case = UseCase::ExportOpenDataDomesticrr.new
-        assessments_array = use_case.execute(date_from, task_id, date_to, true)
-      end
+        assessments_array = use_case.execute(date_from, 0, date_to, true)
+      else
+        assessments = @ni_export_gateway.fetch_assessments(type_of_assessment:, date_from:, date_to:)
 
-      assessments = @ni_export_gateway.fetch_assessments(type_of_assessment:, date_from:, date_to:)
+        assessments.each do |assessment|
+          xml_data = @xml_gateway.fetch(assessment["assessment_id"])
 
-      assessments.each do |assessment|
-        xml_data = @xml_gateway.fetch(assessment["assessment_id"])
+          view_model =
+            ViewModel::Factory.new.create(
+              xml_data[:xml],
+              xml_data[:schema_type],
+              assessment["assessment_id"],
+              )
 
-        view_model =
-          ViewModel::Factory.new.create(
-            xml_data[:xml],
-            xml_data[:schema_type],
-            assessment["assessment_id"],
-          )
-
-        view_model_data = type_of_assessment.any?{ |x| %w[CEPC DEC].include?(x) } ? view_model.to_report : view_model.to_hash_ni
-        combined_data = view_model_data.merge(assessment.symbolize_keys)
-        combined_data[:assessment_id] = Helper::RrnHelper.hash_rrn(assessment["assessment_id"])
-        assessments_array << combined_data
+          view_model_data = type_of_assessment.any?{ |x| %w[CEPC DEC].include?(x) } ? view_model.to_report : view_model.to_hash_ni
+          combined_data = view_model_data.merge(assessment.symbolize_keys)
+          combined_data[:assessment_id] = Helper::RrnHelper.hash_rrn(assessment["assessment_id"])
+          assessments_array << combined_data
+        end
       end
       assessments_array
     end
