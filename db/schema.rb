cb2b1a956927552d6752320ba0ce60d86727bc7c
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-11-06 09:30:44 +0000

    Removes 'create_schema "scotland"' from schema
    To fix error in run app codebuild stage in the CI pipeline.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_11_134216) do
-  create_schema "scotland"
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
