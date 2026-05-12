d1c981cfd35a4a4ff80905746408913b7a3c3e8c
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2026-05-12 14:34:53 +0100

    EPBR-9373 Refactors Scotland data controller
    Adds private methods to DRY out the code
    -error handling
    -date filtering

--- lib/controller/scotland_data_controller.rb
+++ lib/controller/scotland_data_controller.rb
@@ -32,65 +32,27 @@ module Controller
     }.freeze
 
     get "/api/scotland/v1/updates/new-reports", auth_token_has_all: %w[scotland_data:fetch] do
-      params = params_body DATE_RANGE_SCHEMA
+      filter_date_params
 
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
-
-      raise Boundary::InvalidDates if start_date > end_date
-      raise Boundary::InvalidArgument, "date range includes today" if end_date >= Date.today
-
-      current_page = params[:page] ? params[:page].to_i : 1
-
-      data = ApiFactory.fetch_new_reports_use_case.execute(start_date: start_date, end_date: end_date, current_page: current_page)
-      pagination = ApiFactory.get_pagination_for_new_reports.execute(start_date: start_date, end_date: end_date, current_page: current_page, url: request.url)
+      data = ApiFactory.fetch_new_reports_use_case.execute(start_date: @start_date, end_date: @end_date, current_page: @current_page)
+      pagination = ApiFactory.get_pagination_for_new_reports.execute(start_date: @start_date, end_date: @end_date, current_page: @current_page, url: request.url)
 
       json_api_response(code: 200, links: pagination, data: data)
     rescue StandardError => e
-      case e
-      when Boundary::InvalidDates
-        error_response 400, "INVALID_REQUEST", "A required argument is is invalid: #{e.message}"
-      when Boundary::InvalidArgument
-        error_response 400, "INVALID_REQUEST", e.message
-      when Boundary::Json::ValidationError
-        error_response 400, "INVALID_REQUEST", e.message
-      when Errors::OutOfPaginationRangeError
-        error_response 400, "PAGINATION_ERROR", e.message
-      else
-        server_error(e)
-      end
+      rescue_errors(e)
     end
 
     get "/api/scotland/v1/updates/assessments/status", auth_token_has_all: %w[scotland_data:fetch] do
-      params = params_body DATE_RANGE_SCHEMA
-
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
-
-      raise Boundary::InvalidDates if start_date > end_date
-      raise Boundary::InvalidArgument, "date range includes today" if end_date >= Date.today
-
-      current_page = params[:page] ? params[:page].to_i : 1
+      filter_date_params
 
       event_types = %w[scottish_opt_out scottish_opt_in scottish_cancelled]
 
-      data = ApiFactory.fetch_scottish_assessment_status_updates.execute(event_types:, start_date:, end_date: end_date, current_page: current_page)
-      pagination = ApiFactory.get_pagination_for_scottish_assessment_status_updates.execute(event_types:, start_date: start_date, end_date: end_date, current_page: current_page, url: request.url, count_method: :count_scottish_events)
+      data = ApiFactory.fetch_scottish_assessment_status_updates.execute(event_types:, start_date: @start_date, end_date: @end_date, current_page: @current_page)
+      pagination = ApiFactory.get_pagination_for_scottish_assessment_status_updates.execute(event_types:, start_date: @start_date, end_date: @end_date, current_page: @current_page, url: request.url, count_method: :count_scottish_events)
 
       json_api_response(code: 200, links: pagination, data: data)
     rescue StandardError => e
-      case e
-      when Boundary::InvalidDates
-        error_response 400, "INVALID_REQUEST", "A required argument is is invalid: #{e.message}"
-      when Boundary::InvalidArgument
-        error_response 400, "INVALID_REQUEST", e.message
-      when Boundary::Json::ValidationError
-        error_response 400, "INVALID_REQUEST", e.message
-      when Errors::OutOfPaginationRangeError
-        error_response 400, "PAGINATION_ERROR", e.message
-      else
-        server_error(e)
-      end
+      rescue_errors(e)
     end
 
     get "/api/scotland/v1/assessments/:assessmentId/meta-data",
@@ -136,63 +98,54 @@ module Controller
     end
 
     get "/api/scotland/v1/updates/assessors/status", auth_token_has_all: %w[scotland_data:fetch] do
-      params = params_body DATE_RANGE_SCHEMA
+      filter_date_params
 
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
-
-      raise Boundary::InvalidDates if start_date > end_date
-      raise Boundary::InvalidArgument, "date range includes today" if end_date >= Date.today
-
-      current_page = params[:page] ? params[:page].to_i : 1
-
-      data = ApiFactory.fetch_scottish_assessor_status_updates.execute(start_date:, end_date: end_date, current_page: current_page)
-      pagination = ApiFactory.get_pagination_for_scottish_assessor_status_updates.execute(start_date: start_date, end_date: end_date, current_page: current_page, url: request.url, count_method: :count_scottish_assessor_events)
+      data = ApiFactory.fetch_scottish_assessor_status_updates.execute(start_date: @start_date, end_date: @end_date, current_page: @current_page)
+      pagination = ApiFactory.get_pagination_for_scottish_assessor_status_updates.execute(start_date: @start_date, end_date: @end_date, current_page: @current_page, url: request.url, count_method: :count_scottish_assessor_events)
 
       json_api_response(code: 200, links: pagination, data: data)
     rescue StandardError => e
-      case e
-      when Boundary::InvalidDates
-        error_response 400, "INVALID_REQUEST", "A required argument is is invalid: #{e.message}"
-      when Boundary::InvalidArgument
-        error_response 400, "INVALID_REQUEST", e.message
-      when Boundary::Json::ValidationError
-        error_response 400, "INVALID_REQUEST", e.message
-      when Errors::OutOfPaginationRangeError
-        error_response 400, "PAGINATION_ERROR", e.message
-      else
-        server_error(e)
-      end
+      rescue_errors(e)
     end
 
     get "/api/scotland/v1/updates/new-assessors", auth_token_has_all: %w[scotland_data:fetch] do
-      params = params_body DATE_RANGE_SCHEMA
+      filter_date_params
 
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
-
-      raise Boundary::InvalidDates if start_date > end_date
-      raise Boundary::InvalidArgument, "date range includes today" if end_date >= Date.today
-
-      current_page = params[:page] ? params[:page].to_i : 1
-
-      data = ApiFactory.fetch_scottish_assessor_by_date.execute(start_date:, end_date:, current_page:)
-      pagination = ApiFactory.get_pagination_for_scottish_assessors_by_date.execute(start_date: start_date, end_date: end_date, current_page: current_page, url: request.url, count_method: :count_search_by_date)
+      data = ApiFactory.fetch_scottish_assessor_by_date.execute(start_date: @start_date, end_date: @end_date, current_page: @current_page)
+      pagination = ApiFactory.get_pagination_for_scottish_assessors_by_date.execute(start_date: @start_date, end_date: @end_date, current_page: @current_page, url: request.url, count_method: :count_search_by_date)
 
       json_api_response(code: 200, links: pagination, data: data)
     rescue StandardError => e
-      case e
+      rescue_errors(e)
+    end
+
+  private
+
+    def rescue_errors(error)
+      case error
       when Boundary::InvalidDates
-        error_response 400, "INVALID_REQUEST", "A required argument is is invalid: #{e.message}"
+        error_response 400, "INVALID_REQUEST", "A required argument is is invalid: #{error.message}"
       when Boundary::InvalidArgument
-        error_response 400, "INVALID_REQUEST", e.message
+        error_response 400, "INVALID_REQUEST", error.message
       when Boundary::Json::ValidationError
-        error_response 400, "INVALID_REQUEST", e.message
+        error_response 400, "INVALID_REQUEST", error.message
       when Errors::OutOfPaginationRangeError
-        error_response 400, "PAGINATION_ERROR", e.message
+        error_response 400, "PAGINATION_ERROR", error.message
       else
-        server_error(e)
+        server_error(error)
       end
+    end
+
+    def filter_date_params
+      @params = params_body DATE_RANGE_SCHEMA
+
+      @start_date = Date.parse(@params[:start_date])
+      @end_date = Date.parse(@params[:end_date])
+
+      raise Boundary::InvalidDates if @start_date > @end_date
+      raise Boundary::InvalidArgument, "date range includes today" if @end_date >= Date.today
+
+      @current_page = @params[:page] ? @params[:page].to_i : 1
     end
   end
 end
