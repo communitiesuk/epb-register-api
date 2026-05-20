c6cbceaef5f0ac5af933cdd1bca8868199e85807
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2026-05-20 09:16:12 +0100

    EPBR-9282 Updates reload_queues rake with new fetch method
    The existing fetch method on the audit_log gateway could not be used as it uses date format for filtering by timestamp. This commit add a new custom function  audit_logs.timestamp can filtered by utc datetime.

--- lib/tasks/oneoff/reload_queues.rake
+++ lib/tasks/oneoff/reload_queues.rake
@@ -1,10 +1,11 @@
 namespace :oneoff do
   desc "Fetch latest inserts and updated from the register and push onto queues"
   task :reload_queues do
-    number_hours_before = ENV["NUMBER_HOURS_BEFORE"].to_i || 1
+    number_hours_before = ENV["NUMBER_HOURS_BEFORE"].nil? ? 1 : ENV["NUMBER_HOURS_BEFORE"].to_i
+    utc_now = Time.now
+    start_date = (utc_now - number_hours_before.hours)
+    end_date = utc_now
 
-    start_date = Time.now - number_hours_before.hours
-    end_date = Time.now.to_s
     event_types =
       [{ type: "lodgement", queue: :assessments },
        { type: "address_id_updated", queue: :assessments_address_update },
@@ -12,11 +13,24 @@ namespace :oneoff do
        { type: "opt_out", queue: :opt_outs }]
 
     event_types.each do |i|
-      assessment_ids = ApiFactory.audit_logs_gateway.fetch_assessment_ids(event_type: i[:type], start_date:)
+      assessment_ids = Helper::ReloadQueues.fetch_assessment_ids(event_type: i[:type], start_date:)
       ApiFactory.data_warehouse_queues_gateway.push_to_queue(i[:queue], assessment_ids.join(" ")) unless assessment_ids.nil? || assessment_ids.empty?
     end
+
     ENV["DATE_FROM"] = start_date.to_s
-    ENV["DATE_TO"] = end_date
+    ENV["DATE_TO"] = end_date.to_s
     Rake::Task["oneoff:address_match_assessments"].invoke
+  end
+end
+
+class Helper::ReloadQueues
+  def self.fetch_assessment_ids(event_type:, start_date:)
+    sql = <<-SQL
+            SELECT entity_id
+            FROM audit_logs
+            WHERE  timestamp BETWEEN '#{start_date.strftime('%Y-%m-%d %H:%M:%S')}' AND now()
+            AND event_type = '#{event_type}'
+    SQL
+    ActiveRecord::Base.connection.exec_query(sql).map { |result| result["entity_id"] }
   end
 end
--- spec/tasks/reload_queues_spec.rb
+++ spec/tasks/reload_queues_spec.rb
@@ -3,12 +3,18 @@ describe "reload queues rake" do
   before do
     allow($stdout).to receive(:puts)
     Timecop.freeze(2026, 2, 22, 14, 32, 0)
-    allow(ApiFactory).to receive_messages(audit_logs_gateway: audit_logs_gateway, data_warehouse_queues_gateway: data_warehouse_queues_gateway)
+    allow(ApiFactory).to receive_messages(data_warehouse_queues_gateway: data_warehouse_queues_gateway)
     allow(data_warehouse_queues_gateway).to receive(:push_to_queue)
     allow(Helper::AddressMatchAssessment).to receive(:find_unmatched_assessments).and_return(unmatched_assessments)
     OauthStub.token
     WebMock.stub_request(:post, "http://test-addressing.gov.uk/match-address").to_return(status: 200, body: "", headers: {})
     EnvironmentStub.with("NUMBER_HOURS_BEFORE", "3")
+    real_gateway = Gateway::AuditLogsGateway.new
+    event_types.each do |event_type|
+      assessment_ids.each do |assessment_id|
+        real_gateway.add_audit_event(Domain::AuditEvent.new(entity_type: :assessment, entity_id: assessment_id, event_type: event_type[:type].to_sym))
+      end
+    end
   end
 
   after(:all) do
@@ -27,7 +33,7 @@ describe "reload queues rake" do
   end
 
   let(:start_date) do
-    Time.now - 3.hours
+    Time.now.utc - 3.hours
   end
 
   let(:assessment_ids) do
@@ -42,19 +48,11 @@ describe "reload queues rake" do
      { type: "opt_out", queue: :opt_outs }]
   end
 
-  let(:audit_logs_gateway) { instance_double(Gateway::AuditLogsGateway) }
   let(:data_warehouse_queues_gateway) { instance_double(Gateway::DataWarehouseQueuesGateway) }
 
   context "when calling the rake" do
     before do
-      allow(audit_logs_gateway).to receive(:fetch_assessment_ids).and_return(assessment_ids)
       rake.invoke
-    end
-
-    it "calls the gateway to extract data from the audit logs" do
-      event_types.each do |i|
-        expect(audit_logs_gateway).to have_received(:fetch_assessment_ids).with(event_type: i[:type], start_date: start_date).once
-      end
     end
 
     it "calls gateway to put that data into the correct queue" do
@@ -68,14 +66,13 @@ describe "reload queues rake" do
     end
 
     it "the address match rake receives the correct date range" do
-      expect(Helper::AddressMatchAssessment).to have_received(:find_unmatched_assessments).with(date_from: start_date.to_s, date_to: Time.now.to_s, is_scottish: false, skip_existing: nil)
+      expect(Helper::AddressMatchAssessment).to have_received(:find_unmatched_assessments).with(date_from: start_date, date_to: Time.now.to_s, is_scottish: false, skip_existing: nil)
     end
   end
 
   context "when no RRNs are found for an event type" do
     before do
-      allow(audit_logs_gateway).to receive(:fetch_assessment_ids).and_return(assessment_ids)
-      allow(audit_logs_gateway).to receive(:fetch_assessment_ids).with(event_type: "opt_out", start_date: start_date).and_return([])
+      ActiveRecord::Base.connection.execute("DELETE FROM audit_logs WHERE event_type = 'opt_out'")
       rake.invoke
     end
 
