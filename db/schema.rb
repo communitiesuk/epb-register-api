4534a44e2da5d2f536a1b7b4f76f371cd3b2da2b
Eleanor Mollett <eleanor.mollett@gmail.com>
2026-09-04 08:51:13 +0100

    EPBR-9029 Removing the address_base_tmp table that had snuck back into the schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -35,18 +35,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_03_102639) do
     t.index ["town"], name: "index_address_base_on_town"
   end
 
-  create_table "public.address_base_tmp", primary_key: "uprn", id: :string, force: :cascade do |t|
-    t.string "address_line1"
-    t.string "address_line2"
-    t.string "address_line3"
-    t.string "address_line4"
-    t.string "address_type", limit: 15
-    t.string "classification_code", limit: 6
-    t.string "country_code", limit: 1
-    t.string "postcode"
-    t.string "town"
-  end
-
   create_table "public.address_base_versions", primary_key: "version_number", id: :integer, default: nil, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "version_name", null: false
